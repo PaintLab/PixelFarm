c51d5a0b59e7446a8b8893f3e71bdd55c2ce3c4f
win <wintercoredev@gmail.com>
2014-11-06 20:09:43 +0700

    425

--- a_mini/projects/Mini/3_Samples/07_Shading/01_GradientSample.cs
+++ a_mini/projects/Mini/3_Samples/07_Shading/01_GradientSample.cs
@@ -36,17 +36,21 @@ namespace PixelFarm.Agg.Sample_Gradient
         {
             CanvasPainter painter = new CanvasPainter(graphics2D);
             painter.FillColor = ColorRGBA.Blue;
-            painter.FillRectangle(10, 10, 50, 50);
+            painter.FillRectangle(0, 70, 150, 120);
 
 
             //-------------------------------------------
 
-            Agg.Gradients.GvcRadial innerGradient = new Gradients.GvcRadial();
+            //Agg.Gradients.GvcRadial innerGradient = new Gradients.GvcRadial();
+            var innerGradient = new Gradients.GvcRadial();
             SpanInterpolatorLinear linerInterpolator = new SpanInterpolatorLinear(Affine.IdentityMatrix);
             LinearGradientColorsProvider linearGradFunc = new LinearGradientColorsProvider(ColorRGBA.Red, ColorRGBA.Yellow);
-            SpanGenGradient spanGenGradient = new SpanGenGradient(linerInterpolator, innerGradient, linearGradFunc, 100, 150);
+            SpanGenGradient spanGenGradient = new SpanGenGradient(linerInterpolator,
+                innerGradient, 
+                linearGradFunc,
+                0, 150);
 
-            SimpleRect srect = new SimpleRect(100, 10, 150, 50);
+            SimpleRect srect = new SimpleRect(0, 0, 150, 50);
             painter.Fill(srect.MakeVxs(), spanGenGradient);
 
         }
