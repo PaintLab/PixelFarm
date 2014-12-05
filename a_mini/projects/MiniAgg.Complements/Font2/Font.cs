1ca2149b2c4b21c6b2fad8dfd2443e86cfb8794b
win <wintercoredev@gmail.com>
2014-12-05 12:22:09 +0700

    651.1:NativeFont

---
+++ a_mini/projects/MiniAgg.Complements/Font2/Font.cs
@@ -0,0 +1,32 @@
+﻿//----------------------------------- 
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+using PixelFarm.Agg;
+namespace PixelFarm.Font2
+{
+
+    public abstract class Font : IDisposable
+    {
+
+        protected abstract void OnDispose();
+        public abstract FontGlyph GetGlyphByIndex(uint glyphIndex);
+        public abstract FontGlyph GetGlyph(char c);
+        public abstract FontFace FontFace { get; }
+        public abstract void GetGlyphPos(char[] buffer, int start, int len, ProperGlyph[] properGlyphs);
+
+        public void Dispose()
+        {
+            OnDispose();
+        }
+        
+        ~Font()
+        {
+            Dispose();
+        }
+    }
+
+   
+}
