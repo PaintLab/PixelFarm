a3ebc35199c2e44bc27cd61fb33d3eb6b063e66b
win <wintercoredev@gmail.com>
2015-01-01 19:23:26 +0700

    888: dispose canvas when not use
    here is temp fix, ugly code, need refactoring

--- a_mini/projects/Mini2/FormTestWinGLControl.cs
+++ a_mini/projects/Mini2/FormTestWinGLControl.cs
@@ -12,12 +12,13 @@ namespace Mini2
 
     public partial class FormTestWinGLControl : Form
     {
-        Button b1;
+        Button b1; 
         public FormTestWinGLControl()
         {
             InitializeComponent();
 
             this.derivedGLControl1.SetBounds(0, 30, 800, 600);
+             
             //------------------------------------------------------
             b1 = new Button();
             this.Controls.Add(b1);
@@ -26,19 +27,27 @@ namespace Mini2
             b1.Click += new EventHandler((o, s) => { this.derivedGLControl1.Refresh(); });
             this.Load += new EventHandler(FormTestWinGLControl_Load);
         }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.derivedGLControl1.CloseControl();
 
-
+            base.OnClosing(e);
+        }
         void FormTestWinGLControl_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Maximized;
             this.derivedGLControl1.ClearColor = new OpenTK.Graphics.Color4(255, 255, 255, 255);
             if (!this.DesignMode)
             {
-
                 //for 2d 
                 this.derivedGLControl1.InitSetup2d(Screen.PrimaryScreen.Bounds);
             }
-        } 
+        }
+
+        public void SetOnDestroyHandler(EventHandler handler)
+        {
+            this.derivedGLControl1.SetHandleDestroyHandler(handler);
+        }
         public void SetGLPaintHandler(EventHandler handler)
         {
             this.derivedGLControl1.SetGLPaintHandler(handler);
@@ -47,7 +56,9 @@ namespace Mini2
         {
             return this.derivedGLControl1;
         }
-        
+
+
+
     }
 
 
