d3492f74f5ac226084795f5b84197a3de5769045
win <wintercoredev@gmail.com>
2016-07-01 11:59:07 +0700

    1131: add Sameple02 to TestMiniPlatform

---
+++ a_mini/projects/PixelFarm/MiniAgg.HardwareGraphics/TestMiniPlatform/00_DemoBase/SampleBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenTK.Graphics.ES20;
+namespace OpenTkEssTest
+{
+    public abstract class SampleBase
+    {
+        public void InitGLProgram()
+        {
+            OnInitGLProgram(this, EventArgs.Empty);
+        }
+        public void Close()
+        {
+            DemoClosing();
+        }
+        public void Render()
+        {
+            OnGLRender(this, EventArgs.Empty);
+        }
+        protected abstract void OnInitGLProgram(object sender, EventArgs e);
+        protected abstract void DemoClosing();
+        protected virtual void OnGLRender(object sender, EventArgs args) { }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        protected void SwapBuffer() { }
+    }
+}
