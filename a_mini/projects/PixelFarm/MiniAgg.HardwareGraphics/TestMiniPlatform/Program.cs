7418db84a457ffeeec46ab4154734fe7098740c2
win <wintercoredev@gmail.com>
2016-07-01 09:53:43 +0700

    1121: TestMiniPlatform

---
+++ a_mini/projects/PixelFarm/MiniAgg.HardwareGraphics/TestMiniPlatform/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestGlfw
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            GLFWProgram.Start();
+        }
+    }
+    
+}
