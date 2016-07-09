0569ebed8dff88ca0ed25405cb6901a50c16927b
win <wintercoredev@gmail.com>
2016-07-09 20:13:49 +0700

    1889: SimpleFontAtlas

---
+++ a_mini/projects/PixelFarm/MiniAgg.Complements/FontAtlas/SimpleFontAtlas.cs
@@ -0,0 +1,14 @@
+﻿//MIT,2016, WinterDev
+//----------------------------------- 
+
+using System;
+using System.Collections.Generic;
+using PixelFarm.Agg;
+namespace PixelFarm.Drawing.Fonts
+{
+    class SimpleFontAtlas
+    {
+        ActualImage fontActualImage;
+
+    }
+}
