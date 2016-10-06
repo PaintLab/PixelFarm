b8a503fa463b6091a46d3ae12b24b5c4d21da9f6
win <wintercoredev@gmail.com>
2016-10-06 20:11:40 +0700

    1154: use WinGdiFontPrinter for small font

--- a_mini/projects/PixelFarm/PixelFarm.Drawing.NativePlatform/PixelFarm.Drawing.GLES2/Implementation/GLCanvasPainter.cs
+++ a_mini/projects/PixelFarm/PixelFarm.Drawing.NativePlatform/PixelFarm.Drawing.GLES2/Implementation/GLCanvasPainter.cs
@@ -5,16 +5,19 @@ namespace PixelFarm.DrawingGL
 {
     public class GLCanvasPainter : GLCanvasPainterBase
     {
-        //WinGdiFontPrinter _winGdiPrinter;
+        WinGdiFontPrinter _win32GdiPrinter;
         public GLCanvasPainter(CanvasGL2d canvas, int w, int h)
             : base(canvas, w, h)
         {
-            // _win32GdiPrinter = new WinGdiFontPrinter(w, h);
+            _win32GdiPrinter = new WinGdiFontPrinter(w, h);
         }
-        // public override void DrawString(string text, double x, double y)
-        //        {
-        //            _win32GdiPrinter.DrawString(_canvas, text, (float)x, (float)y);
-        //            //base.DrawString(text, x, y);
-        //        }
+        public override void DrawString(string text, double x, double y)
+        {
+            //TODO: review here
+            //for small font size we use gdi+
+            _win32GdiPrinter.DrawString(_canvas, text, (float)x, (float)y);
+            //for large font size we use msdf font
+            //base.DrawString(text, x, y);
+        }
     }
 }
