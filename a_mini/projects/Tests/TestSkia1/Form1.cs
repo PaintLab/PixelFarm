cb8ceb846eeef2b093824305c9b6e22b62adac35
win <wintercoredev@gmail.com>
2016-11-24 00:14:39 +0700

    1221: test Skia

---
+++ a_mini/projects/Tests/TestSkia1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using SkiaSharp;
+
+namespace TestSkia1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            glControl.Visible = false;
+            canvas.Visible = true;
+
+            glControl.PaintSurface += GlControl_PaintSurface;
+            canvas.PaintSurface += Canvas_PaintSurface;
+        }
+
+        private void Canvas_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            var skCanvas = e.Surface.Canvas;
+            skCanvas.Clear(new SkiaSharp.SKColor(255, 255, 255)); 
+
+            using (var paint = new SKPaint())
+            {
+                paint.TextSize = 64.0f;
+                paint.IsAntialias = true;
+                paint.Color = (SKColor)0xFF4281A4;
+                paint.IsStroke = false; 
+                skCanvas.DrawText("SkiaSharp", 400 / 2f, 64.0f, paint);
+            } 
+
+        }
+        private void GlControl_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            var skCanvas = e.Surface.Canvas;
+
+        }
+    }
+}
