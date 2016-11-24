1a19cd68302bb0823d5fa3d15bf35d71ee85f3a8
win <wintercoredev@gmail.com>
2016-11-24 09:13:44 +0700

    1222: developing ...

---
+++ a_mini/projects/PixelFarm/PixelFarm.Drawing.Skia/0_Start/System.Drawing.cs
@@ -0,0 +1,244 @@
+﻿//2016 MIT, WinterDev
+
+using System;
+using System.Collections.Generic;
+using PixelFarm.Agg;
+using PixelFarm.Agg.Transform;
+
+namespace System.Drawing
+{
+    public class Graphics
+    {
+
+        //TODO: implement IDISPOSE
+        Bitmap bufferBmp;
+        SkiaSharp.SKBitmap bmp;
+        internal SkiaSharp.SKCanvas canvas;
+        SkiaSharp.SKPaint penPainter;
+        SkiaSharp.SKPaint brushPainter;
+
+        int w;
+        int h;
+        CompositingMode compositeMode;
+        PixelFarm.Drawing.SmoothingMode smoothingMode;
+        public Graphics(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+            bmp = new SkiaSharp.SKBitmap(w, h);
+            canvas = new SkiaSharp.SKCanvas(bmp);
+            //
+            penPainter = new SkiaSharp.SKPaint();
+            penPainter.IsStroke = true;
+            //
+            brushPainter = new SkiaSharp.SKPaint();
+            brushPainter.IsStroke = false;
+
+        }
+        private Graphics()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+        public static Graphics FromImage(System.Drawing.Bitmap bmp)
+        {
+            Graphics g = new Drawing.Graphics();
+            g.bufferBmp = bmp;
+            g.bmp = bmp.internalBmp;
+            g.w = bmp.Width;
+            g.h = bmp.Height;
+            return g;
+        }
+        public void ScaleTransform(float x, float y)
+        {
+            canvas.Scale(x, y);
+        }
+        public void TranslateTransform(float dx, float dy)
+        {
+            canvas.Translate(dx, dy);
+        }
+        public void ResetTransform()
+        {
+            canvas.ResetMatrix();
+        }
+        public CompositingMode CompositingMode
+        {
+            get { return compositeMode; }
+            set
+            {
+                this.compositeMode = value;
+            }
+        }
+        public PixelFarm.Drawing.SmoothingMode SmoothMode
+        {
+            get { return smoothingMode; }
+            set
+            {
+                smoothingMode = value;
+            }
+        }
+        public void Clear(PixelFarm.Drawing.Color color)
+        {
+            canvas.Clear(new SkiaSharp.SKColor(color.R, color.G, color.B, color.A));
+        }
+        public void FillPath(SkiaSharp.SKPath path, PixelFarm.Drawing.Color c)
+        {
+            brushPainter.Color = new SkiaSharp.SKColor(c.R, c.G, c.B, c.A);
+            canvas.DrawPath(path, brushPainter);
+        }
+        public void DrawPath(SkiaSharp.SKPath path, PixelFarm.Drawing.Color c)
+        {
+            penPainter.Color = new SkiaSharp.SKColor(c.R, c.G, c.B, c.A);
+            canvas.DrawPath(path, penPainter);
+        }
+        public void ClipRect(SkiaSharp.SKRect rect)
+        {
+            canvas.ClipRect(rect);
+        }
+        public void SetClip(SkiaSharp.SKRect rect)
+        {
+            canvas.ClipRect(rect);
+        }
+        PixelFarm.Drawing.Color _penColor;
+        public PixelFarm.Drawing.Color PenColor
+        {
+            get
+            {
+                return _penColor;
+            }
+            set
+            {
+                _penColor = value;
+                penPainter.Color = new SkiaSharp.SKColor(value.R, value.G, value.B, value.A);
+            }
+        }
+        float _penWidth;
+        public float PenWidth
+        {
+            get { return _penWidth; }
+            set
+            {
+                _penWidth = value;
+                penPainter.StrokeWidth = value;
+            }
+        }
+        public void FillPolygon(PixelFarm.Drawing.Color color, PixelFarm.Drawing.PointF[] points)
+        {
+            using (var polygon = CreatePolygon(points))
+            {
+                var prevColor = brushPainter.Color;//save
+                brushPainter.Color = new SkiaSharp.SKColor(color.R, color.G, color.B, color.A);
+                canvas.DrawPath(polygon, brushPainter);
+                brushPainter.Color = prevColor;
+            } 
+        }
+        public void DrawPolygon(PixelFarm.Drawing.Color color, PixelFarm.Drawing.PointF[] points)
+        {
+            using (var polygon = CreatePolygon(points))
+            {
+                var prevColor = penPainter.Color;    //save              
+                penPainter.Color = new SkiaSharp.SKColor(color.R, color.G, color.B, color.A);
+                canvas.DrawPath(polygon, penPainter);
+                penPainter.Color = prevColor; //reset
+            }
+        }
+        static SkiaSharp.SKPath CreatePolygon(PixelFarm.Drawing.PointF[] points)
+        {
+            SkiaSharp.SKPath p = new SkiaSharp.SKPath();
+            int j = points.Length;
+            PixelFarm.Drawing.PointF p0 = new PixelFarm.Drawing.PointF();
+            for (int i = 0; i < j; ++i)
+            {
+                if (i == 0)
+                {
+                    p0 = points[0];
+                    p.MoveTo(p0.X, p0.Y);
+                }
+                else if (i == j - 1)
+                {
+                    //last one
+                    var point = points[i];
+                    p.LineTo(point.X, point.Y);
+                    p.LineTo(p0.X, p0.Y);
+                    p.Close();
+                    break;
+                }
+                else
+                {
+                    var point = points[i];
+                    p.LineTo(point.X, point.Y);
+                }
+            }
+            return p;
+        }
+    }
+    public class Bitmap
+    {
+        internal SkiaSharp.SKBitmap internalBmp;
+        int w;
+        int h;
+        public Bitmap(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+            internalBmp = new SkiaSharp.SKBitmap(w, h);
+        }
+        public int Width
+        {
+            get { return w; }
+        }
+        public int Height
+        {
+            get { return h; }
+        }
+
+    }
+    public class Pen
+    {
+
+        PixelFarm.Drawing.Color innerColor;
+        public Pen(PixelFarm.Drawing.Color color)
+        {
+            this.innerColor = color;
+        }
+        public float Width
+        {
+            get;
+            set;
+        }
+        public PixelFarm.Drawing.Color Color
+        {
+            get { return innerColor; }
+            set
+            {
+                innerColor = value;
+            }
+        }
+
+    }
+    public class SolidBrush
+    {
+        PixelFarm.Drawing.Color color;
+        public SolidBrush(PixelFarm.Drawing.Color color)
+        {
+            this.color = color;
+        }
+        public PixelFarm.Drawing.Color Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+            }
+        }
+    }
+
+    public enum CompositingMode
+    {
+
+    }
+
+}
---
+++ a_mini/projects/PixelFarm/PixelFarm.Drawing.Skia/0_Start/WinGdiPlusPlatform.cs
@@ -0,0 +1,80 @@
+﻿//BSD, 2014-2016, WinterDev 
+using System;
+using Win32;
+using System.Runtime.InteropServices;
+using PixelFarm.Drawing.Text;
+using PixelFarm.Drawing.Fonts;
+namespace PixelFarm.Drawing.WinGdi
+{
+    public class WinGdiPlusPlatform : GraphicsPlatform
+    { 
+        static InstalledFontCollection s_installFontCollection = new InstalledFontCollection();
+        static WinGdiPlusPlatform()
+        {
+            var installFontsWin32 = new PixelFarm.Drawing.Win32.InstallFontsProviderWin32();
+            s_installFontCollection.LoadInstalledFont(installFontsWin32.GetInstalledFontIter());
+            WinGdiFontFace.SetInstalledFontCollection(s_installFontCollection); 
+            PixelFarm.Agg.AggBuffMx.SetNaiveBufferImpl(new Win32AggBuffMx());
+           
+            //3. set default encoing
+            WinGdiTextService.SetDefaultEncoding(System.Text.Encoding.ASCII);
+        }
+        public WinGdiPlusPlatform()
+        {
+
+
+        }
+
+        public override Canvas CreateCanvas(int left, int top, int width, int height, CanvasInitParameters canvasInitPars = new CanvasInitParameters())
+        {
+            return new MyGdiPlusCanvas(0, 0, left, top, width, height);
+        }
+
+        public static void SetFontEncoding(System.Text.Encoding encoding)
+        {
+            WinGdiTextService.SetDefaultEncoding(encoding);
+        }
+        public static void SetFontNotFoundHandler(FontNotFoundHandler fontNotFoundHandler)
+        {
+            s_installFontCollection.SetFontNotFoundHandler(fontNotFoundHandler);
+        }
+    }
+
+
+
+    class Win32AggBuffMx : PixelFarm.Agg.AggBuffMx
+    {
+
+        protected override void InnerMemCopy(byte[] dest_buffer, int dest_startAt, byte[] src_buffer, int src_StartAt, int len)
+        {
+            unsafe
+            {
+                fixed (byte* head_dest = &dest_buffer[dest_startAt])
+                fixed (byte* head_src = &src_buffer[src_StartAt])
+                {
+                    memcpy(head_dest, head_src, len);
+                }
+            }
+        }
+        protected override void InnerMemSet(byte[] dest, int startAt, byte value, int count)
+        {
+            unsafe
+            {
+                fixed (byte* head = &dest[0])
+                {
+                    memset(head, 0, 100);
+                }
+            }
+        }
+        //this is platform specific ***
+        [DllImport("msvcrt.dll", EntryPoint = "memset", CallingConvention = CallingConvention.Cdecl)]
+        static unsafe extern void memset(byte* dest, byte c, int byteCount);
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl)]
+        static unsafe extern void memcpy(byte* dest, byte* src, int byteCount);
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl)]
+        static unsafe extern int memcmp(byte* dest, byte* src, int byteCount);
+    }
+
+
+
+}
