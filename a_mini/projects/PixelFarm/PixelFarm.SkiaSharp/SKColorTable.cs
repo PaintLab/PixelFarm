82789539276766f279a5e1f8f1933a3c262af8ca
win <wintercoredev@gmail.com>
2016-11-25 01:57:12 +0700

    1236: modified SkiaSharp

--- a_mini/projects/PixelFarm/PixelFarm.SkiaSharp/SKColorTable.cs
+++ a_mini/projects/PixelFarm/PixelFarm.SkiaSharp/SKColorTable.cs
@@ -12,89 +12,115 @@ using System.Runtime.InteropServices;
 
 namespace SkiaSharp
 {
-	public class SKColorTable : SKObject
-	{
-		public const int MaxLength = 256;
+    public class SKColorTable : SKObject
+    {
+        public const int MaxLength = 256;
 
-		protected override void Dispose (bool disposing)
-		{
-			if (Handle != IntPtr.Zero && OwnsHandle) {
-				SkiaApi.sk_colortable_unref (Handle);
-			}
+        protected override void Dispose(bool disposing)
+        {
+            if (Handle != IntPtr.Zero && OwnsHandle)
+            {
+                SkiaApi.sk_colortable_unref(Handle);
+            }
 
-			base.Dispose (disposing);
-		}
+            base.Dispose(disposing);
+        }
 
-		[Preserve]
-		internal SKColorTable (IntPtr x, bool owns)
-			: base (x, owns)
-		{
-		}
+        [Preserve]
+        internal SKColorTable(IntPtr x, bool owns)
+            : base(x, owns)
+        {
+        }
 
-		public SKColorTable ()
-			: this (new SKColor [MaxLength])
-		{
-		}
+        public SKColorTable()
+            : this(new SKColor[MaxLength])
+        {
+        }
 
-		public SKColorTable (int count)
-			: this (new SKColor [count])
-		{
-		}
+        public SKColorTable(int count)
+            : this(new SKColor[count])
+        {
+        }
 
-		public SKColorTable (SKColor[] colors)
-			: this (colors, colors.Length)
-		{
-		}
+        public SKColorTable(SKColor[] colors)
+            : this(colors, colors.Length)
+        {
+        }
 
-		public SKColorTable (SKColor[] colors, int count)
-			: this (SkiaApi.sk_colortable_new (colors, count), true)
-		{
-			if (Handle == IntPtr.Zero) {
-				throw new InvalidOperationException ("Unable to create a new SKColorTable instance.");
-			}
-		}
+        public SKColorTable(SKColor[] colors, int count)
+            : this(SkiaApi.sk_colortable_new(colors, count), true)
+        {
+            if (Handle == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Unable to create a new SKColorTable instance.");
+            }
+        }
 
-		public int Count => SkiaApi.sk_colortable_count (Handle);
+        public int Count => SkiaApi.sk_colortable_count(Handle);
 
-		public SKColor[] Colors
-		{
-			get
-			{
-				var count = Count;
-				var colors = new SKColor[count];
-				var type = typeof(SKColor);
-				var size = Marshal.SizeOf (type);
-				var pointer = ReadColors ();
-				for (var i = 0; i < count; i++) {
-					colors[i] = (SKColor)Marshal.PtrToStructure (pointer + (i * size), type);
-				}
-				return colors;
-			}
-		}
+        public SKColor[] Colors
+        {
+            get
+            {
+                var count = Count;
+                var colors = new SKColor[count];
+                var type = typeof(SKColor);
+                var size = Marshal.SizeOf(type);
+                var pointer = ReadColors();
+                for (var i = 0; i < count; i++)
+                {
+                    //colors[i] = (SKColor)Marshal.PtrToStructure(pointer + (i * size), type);
+#if NET20
 
-		public SKColor this [int index]
-		{
-			get
-			{
-				var count = Count;
-				var pointer = ReadColors ();
+                    unsafe
+                    {
+                        byte* newptr = (byte*)pointer;
+                        newptr += (i * size);
+                        colors[i] = (SKColor)Marshal.PtrToStructure(new IntPtr(newptr), type);
+                    }
+#else
+                    colors[i] = (SKColor)Marshal.PtrToStructure(pointer + (i * size), type);
+#endif
+                }
+                return colors;
+            }
+        }
 
-				if (index < 0 || index >= count || pointer == IntPtr.Zero) {
-					throw new ArgumentOutOfRangeException (nameof (index));
-				}
+        public SKColor this[int index]
+        {
+            get
+            {
+                var count = Count;
+                var pointer = ReadColors();
 
-				var type = typeof(SKColor);
-				var size = Marshal.SizeOf (type);
-				return (SKColor)Marshal.PtrToStructure (pointer + (index * size), type);
-			}
-		}
+                if (index < 0 || index >= count || pointer == IntPtr.Zero)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
 
-		public IntPtr ReadColors ()
-		{
-			IntPtr colors;
-			SkiaApi.sk_colortable_read_colors (Handle, out colors);
-			return colors;
-		}
-	}
+                var type = typeof(SKColor);
+                var size = Marshal.SizeOf(type);
+
+#if NET20
+
+                unsafe
+                {
+                    byte* newptr = (byte*)pointer;
+                    newptr += (index * size);
+                    return (SKColor)Marshal.PtrToStructure(new IntPtr(newptr), type);
+                }
+#else
+                //return (SKColor)Marshal.PtrToStructure (pointer + (index * size), type);
+#endif
+            }
+        }
+
+        public IntPtr ReadColors()
+        {
+            IntPtr colors;
+            SkiaApi.sk_colortable_read_colors(Handle, out colors);
+            return colors;
+        }
+    }
 }
 
