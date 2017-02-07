67e46ba15a58d1a4016fd907cf27ceef06a15f7f
win <wintercoredev@gmail.com>
2017-02-07 21:22:19 +0700

    1273: fix project files

--- a_mini/projects/Tests/TestSamples/00_DemoBase/SampleBase.cs
+++ a_mini/projects/Tests/TestSamples/00_DemoBase/SampleBase.cs
@@ -24,20 +24,21 @@ namespace OpenTkEssTest
         public int Height { get; set; }
 
         protected void SwapBuffer() { }
-        public PixelFarm.DrawingGL.CanvasGL2d CreateCanvasGL2d(int w,int h)
+        public PixelFarm.DrawingGL.CanvasGL2d CreateCanvasGL2d(int w, int h)
         {
             return PixelFarm.Drawing.GLES2.GLES2Platform.CreateCanvasGL2d(w, h);
         }
         protected static PixelFarm.DrawingGL.GLBitmap LoadTexture(string imgFileName)
         {
-            //1. create native image
-            var nativeImg = new PixelFarm.Drawing.Imaging.NativeImage(imgFileName); 
-            //2. create glbmp from native img
-            var glbmp = new PixelFarm.DrawingGL.GLBitmap(nativeImg, false);
-            //we load image from myft's image module
-            //its already big-endian
-            glbmp.IsBigEndianPixel = true;
-            return glbmp;
+            throw new NotSupportedException();
+            ////1. create native image
+            //var nativeImg = new PixelFarm.Drawing.Imaging.NativeImage(imgFileName);
+            ////2. create glbmp from native img
+            //var glbmp = new PixelFarm.DrawingGL.GLBitmap(nativeImg, false);
+            ////we load image from myft's image module
+            ////its already big-endian
+            //glbmp.IsBigEndianPixel = true;
+            //return glbmp;
         }
         protected bool EnableAnimationTimer
         {
