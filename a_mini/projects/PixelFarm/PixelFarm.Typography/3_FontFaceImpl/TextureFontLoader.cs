e9de9620dc5dcb3ba7a52f85cde8bf75c577cc0c
win <wintercoredev@gmail.com>
2017-02-27 15:14:02 +0700

    1380: remove unused code

--- a_mini/projects/PixelFarm/PixelFarm.Typography/3_FontFaceImpl/TextureFontLoader.cs
+++ a_mini/projects/PixelFarm/PixelFarm.Typography/3_FontFaceImpl/TextureFontLoader.cs
@@ -23,8 +23,6 @@ namespace PixelFarm.Drawing.Fonts
             SimpleFontAtlasBuilder atlas1 = CreateSampleMsdfTextureFont(fontfile, 14, 0, 255);
             GlyphImage glyphImg2 = atlas1.BuildSingleImage();
             fontAtlas = atlas1.CreateSimpleFontAtlas();
-            //GlyphImage glyphImg = new GlyphImage(glyphImg2.Width, glyphImg2.Height);
-            //glyphImg.SetImageBuffer(glyphImg2.GetImageBuffer(), glyphImg2.IsBigEndian);
             fontAtlas.TotalGlyph = glyphImg2;
 
             //string xmlFontFileInfo = "";
@@ -38,61 +36,64 @@ namespace PixelFarm.Drawing.Fonts
             var textureFontFace = new TextureFontFace(openFont, fontAtlas);
             return textureFontFace;
         }
+
         static SimpleFontAtlasBuilder CreateSampleMsdfTextureFont(string fontfile,
             float sizeInPoint,
             ushort startGlyphIndex, ushort endGlyphIndex)
         {
-            //sample
-            var reader = new OpenFontReader();
 
+            //read type face from file
+            Typeface typeface;
             using (var fs = new FileStream(fontfile, FileMode.Open, FileAccess.Read))
             {
+                var reader = new OpenFontReader();
                 //1. read typeface from font file
-                Typeface typeface = reader.Read(fs);
-                //sample: create sample msdf texture 
-                //-------------------------------------------------------------
-                var builder = new MyGlyphPathBuilder(typeface);
-                //builder.UseTrueTypeInterpreter = this.chkTrueTypeHint.Checked;
-                //builder.UseVerticalHinting = this.chkVerticalHinting.Checked;
-                //-------------------------------------------------------------
-                var atlasBuilder = new SimpleFontAtlasBuilder();
-                var msdfBuilder = new MsdfGlyphGen();
-
-                for (ushort n = startGlyphIndex; n <= endGlyphIndex; ++n)
-                {
-                    //build glyph
-                    builder.BuildFromGlyphIndex(n, sizeInPoint);
-
-                    var msdfGlyphGen = new MsdfGlyphGen();
-                    var actualImg = msdfGlyphGen.CreateMsdfImage(
-                        builder.GetOutputPoints(),
-                        builder.GetOutputContours(),
-                        builder.GetPixelScale());
-                    atlasBuilder.AddGlyph((int)n, actualImg);
-
-                    //using (Bitmap bmp = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
-                    //{
-                    //    var bmpdata = bmp.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-                    //    System.Runtime.InteropServices.Marshal.Copy(buffer, 0, bmpdata.Scan0, buffer.Length);
-                    //    bmp.UnlockBits(bmpdata);
-                    //    bmp.Save("d:\\WImageTest\\a001_xn2_" + n + ".png");
-                    //}
-                }
-
-                return atlasBuilder;
-                //var glyphImg2 = atlasBuilder.BuildSingleImage();
-                //using (Bitmap bmp = new Bitmap(glyphImg2.Width, glyphImg2.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
-                //{
-                //    var bmpdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, glyphImg2.Width, glyphImg2.Height),
-                //        System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-                //    int[] intBuffer = glyphImg2.GetImageBuffer();
-
-                //    System.Runtime.InteropServices.Marshal.Copy(intBuffer, 0, bmpdata.Scan0, intBuffer.Length);
-                //    bmp.UnlockBits(bmpdata);
-                //    bmp.Save("d:\\WImageTest\\a_total.png");
-                //}
-                //atlasBuilder.SaveFontInfo("d:\\WImageTest\\a_info.xml");
+                typeface = reader.Read(fs);
             }
+            //sample: create sample msdf texture 
+            //-------------------------------------------------------------
+            var builder = new MyGlyphPathBuilder(typeface);
+            //builder.UseTrueTypeInterpreter = this.chkTrueTypeHint.Checked;
+            //builder.UseVerticalHinting = this.chkVerticalHinting.Checked;
+            //-------------------------------------------------------------
+            var atlasBuilder = new SimpleFontAtlasBuilder();
+            var msdfBuilder = new MsdfGlyphGen();
+
+            for (ushort n = startGlyphIndex; n <= endGlyphIndex; ++n)
+            {
+                //build glyph
+                builder.BuildFromGlyphIndex(n, sizeInPoint);
+
+                var msdfGlyphGen = new MsdfGlyphGen();
+                var actualImg = msdfGlyphGen.CreateMsdfImage(
+                    builder.GetOutputPoints(),
+                    builder.GetOutputContours(),
+                    builder.GetPixelScale());
+                atlasBuilder.AddGlyph((int)n, actualImg);
+
+                //using (Bitmap bmp = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+                //{
+                //    var bmpdata = bmp.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+                //    System.Runtime.InteropServices.Marshal.Copy(buffer, 0, bmpdata.Scan0, buffer.Length);
+                //    bmp.UnlockBits(bmpdata);
+                //    bmp.Save("d:\\WImageTest\\a001_xn2_" + n + ".png");
+                //}
+            }
+
+            return atlasBuilder;
+            //var glyphImg2 = atlasBuilder.BuildSingleImage();
+            //using (Bitmap bmp = new Bitmap(glyphImg2.Width, glyphImg2.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            //{
+            //    var bmpdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, glyphImg2.Width, glyphImg2.Height),
+            //        System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+            //    int[] intBuffer = glyphImg2.GetImageBuffer();
+
+            //    System.Runtime.InteropServices.Marshal.Copy(intBuffer, 0, bmpdata.Scan0, intBuffer.Length);
+            //    bmp.UnlockBits(bmpdata);
+            //    bmp.Save("d:\\WImageTest\\a_total.png");
+            //}
+            //atlasBuilder.SaveFontInfo("d:\\WImageTest\\a_info.xml");
+
         }
     }
 
