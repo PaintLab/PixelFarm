80df6f4a68e4c243d5587a5589e244e12354dfa5
win <wintercoredev@gmail.com>
2017-03-02 22:55:44 +0700

    1407.3:  FormGLTest

--- a_mini/projects/Tests/Mini/7_GLES2Test/FormGLTest.cs
+++ a_mini/projects/Tests/Mini/7_GLES2Test/FormGLTest.cs
@@ -1,8 +1,6 @@
 ﻿//MIT, 2014-2017, WinterDev
-using System;
 
 using System.Windows.Forms;
-using PixelFarm.DrawingGL;
 using OpenTK;
 
 namespace Mini
@@ -10,27 +8,11 @@ namespace Mini
     partial class FormGLTest : Form
     {
         MyGLControl miniGLControl;
-        IntPtr hh1;
-        CanvasGL2d canvas2d;
-        DemoBase exampleBase;
-        GLCanvasPainter canvasPainter;
         public FormGLTest()
         {
             InitializeComponent();
         }
-        public void LoadExample(ExampleAndDesc exAndDesc)
-        {
-            DemoBase exBase = Activator.CreateInstance(exAndDesc.Type) as DemoBase;
-            if (exBase == null)
-            {
-                return;
-            }
 
-            this.Text = exAndDesc.ToString();
-            //----------------------------------------------------------------------------
-            this.exampleBase = exBase;
-            exampleBase.Init();
-        }
         public MyGLControl InitMiniGLControl(int w, int h)
         {
             if (miniGLControl == null)
@@ -38,27 +20,10 @@ namespace Mini
                 miniGLControl = new MyGLControl();
                 miniGLControl.Width = w;
                 miniGLControl.Height = h;
-                this.Controls.Add(miniGLControl); 
+                this.Controls.Add(miniGLControl);
             }
             return miniGLControl;
         }
 
-        void HandleGLPaint(object sender, System.EventArgs e)
-        {
-            canvas2d.SmoothMode = CanvasSmoothMode.Smooth;
-            canvas2d.StrokeColor = PixelFarm.Drawing.Color.Black;
-            canvas2d.ClearColorBuffer();
-            //example
-            canvasPainter.FillColor = PixelFarm.Drawing.Color.Black;
-            canvasPainter.FillRectLBWH(20, 20, 150, 150);
-            //load bmp image 
-            //------------------------------------------------------------------------- 
-            if (exampleBase != null)
-            {
-                exampleBase.Draw(canvasPainter);
-            }
-            miniGLControl.SwapBuffers();
-        }
-        
     }
 }
