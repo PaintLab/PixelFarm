538ad0c51dfc896aa1114f48cacae59ec059c48d
win <wintercoredev@gmail.com>
2017-03-02 11:53:25 +0700

    1403.7: arrange code

--- a_mini/projects/Tests/TestMiniPlatform4/GLFWProgram.cs
+++ a_mini/projects/Tests/TestMiniPlatform4/GLFWProgram.cs
@@ -4,57 +4,21 @@ using Pencil.Gaming;
 using PixelFarm;
 using PixelFarm.Drawing;
 using PixelFarm.DrawingGL;
-
 using PixelFarm.Forms;
 using OpenTK.Graphics.ES20;
 using SkiaSharp;
 
 namespace TestGlfw
 {
-    class MyNativeRGBA32BitsImage : IDisposable
-    {
-        int width;
-        int height;
-        int bitDepth;
-        int stride;
-        IntPtr unmanagedMem;
-        public MyNativeRGBA32BitsImage(int width, int height)
-        {
-            //width and height must >0 
-            this.width = width;
-            this.height = height;
-            this.bitDepth = 32;
-            this.stride = width * (32 / 8);
-            unmanagedMem = System.Runtime.InteropServices.Marshal.AllocHGlobal(stride * height);
-            //this.pixelBuffer = new byte[stride * height];
-        }
-        public IntPtr Scan0
-        {
-            get { return this.unmanagedMem; }
-        }
-        public int Stride
-        {
-            get { return this.stride; }
-        }
-        public void Dispose()
-        {
-            if (unmanagedMem != IntPtr.Zero)
-            {
-                System.Runtime.InteropServices.Marshal.FreeHGlobal(unmanagedMem);
-                unmanagedMem = IntPtr.Zero;
-            }
-        }
-    }
+    //-------------------------------------------------------------------------
+    //WITHOUT WinForms.
+    //This demonstrate how to draw with 1) Skia  or 2) Glfw
+    //-------------------------------------------------------------------------
 
-    class FormRenderUpdateEventArgs : EventArgs
-    {
-        public GlFwForm form;
-    }
     class GLFWProgram
     {
         static bool needUpdateContent = false;
-        static MyNativeRGBA32BitsImage myImg;
-        static int textureId;
+        static MyNativeRGBA32BitsImage myImg; 
         static GLBitmap glBmp;
 
         static void UpdateViewContent(FormRenderUpdateEventArgs formRenderUpdateEventArgs)
@@ -209,5 +173,45 @@ namespace TestGlfw
 
             Glfw.Terminate();
         }
+    }
+
+    class MyNativeRGBA32BitsImage : IDisposable
+    {
+        int width;
+        int height;
+        int bitDepth;
+        int stride;
+        IntPtr unmanagedMem;
+        public MyNativeRGBA32BitsImage(int width, int height)
+        {
+            //width and height must >0 
+            this.width = width;
+            this.height = height;
+            this.bitDepth = 32;
+            this.stride = width * (32 / 8);
+            unmanagedMem = System.Runtime.InteropServices.Marshal.AllocHGlobal(stride * height);
+            //this.pixelBuffer = new byte[stride * height];
+        }
+        public IntPtr Scan0
+        {
+            get { return this.unmanagedMem; }
+        }
+        public int Stride
+        {
+            get { return this.stride; }
+        }
+        public void Dispose()
+        {
+            if (unmanagedMem != IntPtr.Zero)
+            {
+                System.Runtime.InteropServices.Marshal.FreeHGlobal(unmanagedMem);
+                unmanagedMem = IntPtr.Zero;
+            }
+        }
+    }
+
+    class FormRenderUpdateEventArgs : EventArgs
+    {
+        public GlFwForm form;
     }
 }
