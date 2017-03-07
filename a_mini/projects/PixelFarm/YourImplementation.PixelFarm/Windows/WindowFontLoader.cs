703d2eb5e30827dbfb4a9321b294cce705e0a97c
win <wintercoredev@gmail.com>
2017-03-07 17:50:48 +0700

    1156: SetFontNotFoundHandler()

--- a_mini/projects/PixelFarm/YourImplementation.PixelFarm/Windows/WindowFontLoader.cs
+++ a_mini/projects/PixelFarm/YourImplementation.PixelFarm/Windows/WindowFontLoader.cs
@@ -12,13 +12,35 @@ namespace PixelFarm.Drawing
             //iterate
             var installFontsWin32 = new InstallFontsProviderWin32();
             installFontCollection.LoadInstalledFont(installFontsWin32.GetInstalledFontIter());
-            //TODO: review font not found handler here
+
+            SetFontNotFoundHandler(
+              (fontCollection, fontName, style) =>
+              {
+                    //TODO: implement font not found mapping here
+                    //_fontsMapping["monospace"] = "Courier New";
+                    //_fontsMapping["Helvetica"] = "Arial";
+                    fontName = fontName.ToUpper();
+                  switch (fontName)
+                  {
+                      case "MONOSPACE":
+                          return fontCollection.GetFont("Courier New", style);
+                      case "HELVETICA":
+                          return fontCollection.GetFont("Arial", style);
+                      case "TAHOMA":
+                            //default font must found
+                            //if not throw err 
+                            //this prevent infinit loop
+                            throw new System.NotSupportedException();
+                      default:
+                          return fontCollection.GetFont("tahoma", style);
+                  }
+              });
         }
         public InstalledFont GetFont(string fontName, InstalledFontStyle style)
         {
             return installFontCollection.GetFont(fontName, style);
         }
-        public void SetFontHotFoundHandler(FontNotFoundHandler fontNotFoundHandler)
+        public void SetFontNotFoundHandler(FontNotFoundHandler fontNotFoundHandler)
         {
             installFontCollection.SetFontNotFoundHandler(fontNotFoundHandler);
         }
