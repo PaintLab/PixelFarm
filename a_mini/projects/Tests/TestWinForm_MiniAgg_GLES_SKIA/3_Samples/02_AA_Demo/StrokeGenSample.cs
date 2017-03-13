510a880fc951c1c7c5cb66a78ef34fe882764766
win <wintercoredev@gmail.com>
2017-03-13 07:10:25 +0700

    1207: AggStrokeSample

---
+++ a_mini/projects/Tests/TestWinForm_MiniAgg_GLES_SKIA/3_Samples/02_AA_Demo/StrokeGenSample.cs
@@ -0,0 +1,67 @@
+﻿//BSD, 2014-2017, WinterDev
+
+//MatterHackers: BSD
+// Much of the ui to the drawing functions still needs to be C#'ed and cleaned up.  A lot of
+// it still follows the originall agg function names.  I have been cleaning these up over time
+// and intend to do much more refactoring of these things over the long term.
+
+using PixelFarm.Agg.VertexSource;
+using PixelFarm.VectorMath;
+using PixelFarm.Agg.Transform;
+
+using Mini;
+
+namespace PixelFarm.Agg.Sample_Draw
+{
+    [Info(OrderCode = "02")]
+    [Info("StrokeGenSample")]
+    public class StrokeGenSample : DemoBase
+    {
+        public StrokeGenSample()
+        {
+        }
+        [DemoConfig]
+        public LineJoin LineJoin
+        {
+            get;
+            set;
+        }
+        [DemoConfig]
+        public LineCap LineCap
+        {
+            get;
+            set;
+        }
+
+        public override void Draw(CanvasPainter p)
+        {
+            var aggPainter = p as PixelFarm.Agg.AggCanvasPainter;
+            if (aggPainter == null)
+            {
+                return;
+            }
+            Draw(aggPainter);
+        }
+        void Draw(PixelFarm.Agg.AggCanvasPainter aggPainter)
+        {
+
+            aggPainter.Clear(PixelFarm.Drawing.Color.White);
+            //--------------------------
+            aggPainter.StrokeColor = PixelFarm.Drawing.Color.Black;
+            aggPainter.StrokeWidth = 30.0f;
+
+            //
+            VertexStore vxs = new VertexStore();
+            PathWriter writer = new PathWriter(vxs);
+
+            writer.MoveTo(20, 0);
+            writer.LineTo(100, 100);
+            writer.LineTo(20, 200);
+
+            aggPainter.LineJoin = this.LineJoin;
+            aggPainter.LineCap = this.LineCap;
+            //
+            aggPainter.Draw(vxs);
+        }
+    }
+}
