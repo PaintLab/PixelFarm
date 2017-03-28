86635156d3e769baa760ec24107eba079e3f25a5
win <wintercoredev@gmail.com>
2017-03-28 18:49:58 +0700

    9286: TessTool

--- a_mini/projects/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLCanvas/TessListener.cs
+++ a_mini/projects/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLCanvas/TessListener.cs
@@ -145,6 +145,63 @@ namespace PixelFarm.DrawingGL
         }
     }
 
+
+    public class TessTool
+    {
+        internal readonly Tesselate.Tesselator tess;
+        internal readonly TessListener2 tessListener;
+        List<Vertex> vertexts = new List<Vertex>();
+        public TessTool(Tesselate.Tesselator tess)
+        {
+            this.tess = tess;
+            this.tessListener = new TessListener2();
+            tessListener.Connect(tess, true);
+        }
+        public float[] TessPolygon(float[] vertex2dCoords, out int areaCount)
+        {
+            vertexts.Clear();//reset
+            //
+            int ncoords = vertex2dCoords.Length / 2;
+            if (ncoords == 0) { areaCount = 0; return null; }
+
+            int nn = 0;
+            for (int i = 0; i < ncoords; ++i)
+            {
+                vertexts.Add(new Vertex(vertex2dCoords[nn++], vertex2dCoords[nn++]));
+            }
+            //-----------------------
+            tessListener.Reset(vertexts);
+            //-----------------------
+            tess.BeginPolygon();
+            tess.BeginContour();
+            int j = vertexts.Count;
+            for (int i = 0; i < j; ++i)
+            {
+                Vertex v = vertexts[i];
+                tess.AddVertex(v.m_X, v.m_Y, 0, i);
+            }
+            tess.EndContour();
+            tess.EndPolygon();
+            //-----------------------
+            List<Vertex> vertextList = tessListener.resultVertexList;
+            //-----------------------------   
+            //switch how to fill polygon
+            j = vertextList.Count;
+            float[] vtx = new float[j * 2];
+            int n = 0;
+            for (int p = 0; p < j; ++p)
+            {
+                var v = vertextList[p];
+                vtx[n] = (float)v.m_X;
+                vtx[n + 1] = (float)v.m_Y;
+                n += 2;
+            }
+            //triangle list
+            areaCount = j;
+            return vtx;
+        }
+    }
+
     //public class TessListener
     //{
 
