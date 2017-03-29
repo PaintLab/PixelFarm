2f12285fecf362fd1d00584d4f0e0dfbf687941d
win <wintercoredev@gmail.com>
2017-03-29 09:02:59 +0700

    9293: move samples

--- a_mini/projects/PixelFarm/Typography/Demo/Windows/GlyphTess.WinForms/Form1.cs
+++ a_mini/projects/PixelFarm/Typography/Demo/Windows/GlyphTess.WinForms/Form1.cs
@@ -1,21 +1,24 @@
 ﻿//MIT, 2017, WinterDev
-using System; 
-using System.Drawing; 
+using System;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+
 //
-using PixelFarm.DrawingGL;
-using Typography.OpenFont; 
+using Typography.OpenFont;
+//
 using DrawingGL;
 using DrawingGL.Text;
+//
 
-namespace BuildTextureFonts
+
+namespace Test_WinForm_TessGlyph
 {
     public partial class FormTess : Form
     {
         Graphics g;
         float[] glyphPoints2;
-        int[] endContours;
+        int[] contourEnds;
 
         TessTool tessTool = new TessTool();
         public FormTess()
@@ -24,19 +27,17 @@ namespace BuildTextureFonts
         }
         private void FormTess_Load(object sender, EventArgs e)
         {
-            g = CreateGraphics();
+            g = this.pnlGlyph.CreateGraphics();
 
             string testFont = "d:\\WImageTest\\DroidSans.ttf";
             using (FileStream fs = new FileStream(testFont, FileMode.Open, FileAccess.Read))
             {
                 OpenFontReader reader = new OpenFontReader();
                 Typeface typeface = reader.Read(fs);
-                Glyph glyph = typeface.GetGlyphByIndex(typeface.LookupIndex('G'));
-                //--
-                GlyphPointF[] glyphPoints = glyph.GlyphPoints;
+
                 //--
                 var builder = new Typography.Rendering.GlyphPathBuilder(typeface);
-                builder.BuildFromGlyphIndex(typeface.LookupIndex('b'), 256);
+                builder.BuildFromGlyphIndex(typeface.LookupIndex('a'), 256);
 
                 var txToPath = new GlyphTranslatorToPath();
                 var writablePath = new WritablePath();
@@ -44,7 +45,7 @@ namespace BuildTextureFonts
                 builder.ReadShapes(txToPath);
                 //from contour to  
                 var curveFlattener = new SimpleCurveFlattener();
-                float[] flattenPoints = curveFlattener.Flatten(writablePath._points, out endContours);
+                float[] flattenPoints = curveFlattener.Flatten(writablePath._points, out contourEnds);
                 glyphPoints2 = flattenPoints;
                 ////--------------------------------------
                 ////raw glyph points
@@ -64,59 +65,75 @@ namespace BuildTextureFonts
             }
         }
 
-        float[] GetPolygonData()
+        float[] GetPolygonData(out int[] endContours)
         {
+            endContours = this.contourEnds;
             return glyphPoints2;
 
-            return new float[]
-            {
-                    10,10,
-                    200,10,
-                    100,100,
-                    150,200,
-                    20,200,
-                    50,100
-            };
+            ////--
+            ////for test
+
+            //return new float[]
+            //{
+            //        10,10,
+            //        200,10,
+            //        100,100,
+            //        150,200,
+            //        20,200,
+            //        50,100
+            //};
         }
-        private void button1_Click(object sender, EventArgs e)
+        void DrawOutput()
         {
+            //-----------
+            //for GDI+ only
+            bool drawInvert = chkInvert.Checked;
+            int viewHeight = this.pnlGlyph.Height;
+            if (drawInvert)
+            {
+                g.ScaleTransform(1, -1);
+                g.TranslateTransform(0, -viewHeight);
+            }
+            //----------- 
             //show tess
             g.Clear(Color.White);
+            int[] contourEndIndices;
+            float[] polygon1 = GetPolygonData(out contourEndIndices);
 
-            float[] polygon1 = GetPolygonData();
 
-            int nn = polygon1.Length;
             using (Pen pen1 = new Pen(Color.LightGray, 6))
             {
+                int nn = polygon1.Length;
                 int a = 0;
                 PointF p0;
                 PointF p1;
-                for (int m = 2; m < nn;)
+
+                int contourCount = contourEndIndices.Length;
+                int startAt = 3;
+                for (int cnt_index = 0; cnt_index < contourCount; ++cnt_index)
                 {
+                    int endAt = contourEndIndices[cnt_index];
+                    for (int m = startAt; m <= endAt;)
+                    {
+                        p0 = new PointF(polygon1[m - 3], polygon1[m - 2]);
+                        p1 = new PointF(polygon1[m - 1], polygon1[m]);
+                        g.DrawLine(pen1, p0, p1);
+                        g.DrawString(a.ToString(), this.Font, Brushes.Black, p0);
+                        m += 2;
+                        a++;
+                    }
+                    //close coutour 
 
-                    p0 = new PointF(polygon1[m - 2], polygon1[m - 1]);
-                    p1 = new PointF(polygon1[m], polygon1[m + 1]);
-
+                    p0 = new PointF(polygon1[endAt - 1], polygon1[endAt]);
+                    p1 = new PointF(polygon1[startAt - 3], polygon1[startAt - 2]);
                     g.DrawLine(pen1, p0, p1);
-
-                    //-----
                     g.DrawString(a.ToString(), this.Font, Brushes.Black, p0);
-
-
-                    m += 2;
-                    a++;
+                    //
+                    startAt = (endAt + 1) + 3;
                 }
-                //-------
-                //close polygon 
-                p0 = new PointF(polygon1[nn - 2], polygon1[nn - 1]);
-                p1 = new PointF(polygon1[0], polygon1[1]);
-                g.DrawLine(pen1, p0, p1);
-                g.DrawString(a.ToString(), this.Font, Brushes.Black, p0);
-
-                //-------
             }
             int areaCount;
-            float[] tessData = tessTool.TessPolygon(polygon1, endContours, out areaCount);
+            float[] tessData = tessTool.TessPolygon(polygon1, contourEnds, out areaCount);
             //draw tess 
             int j = tessData.Length;
             for (int i = 0; i < j;)
@@ -131,6 +148,20 @@ namespace BuildTextureFonts
 
                 i += 6;
             }
-        } 
+
+            //-----------
+            //for GDI+ only
+            g.ResetTransform();
+            //-----------
+        }
+        private void cmdDrawGlyph_Click(object sender, EventArgs e)
+        {
+            DrawOutput();
+        }
+
+        private void chkInvert_CheckedChanged(object sender, EventArgs e)
+        {
+            DrawOutput();
+        }
     }
 }
