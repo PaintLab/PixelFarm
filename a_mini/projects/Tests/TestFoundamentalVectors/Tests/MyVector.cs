7e91416631acb74fc60a4646a36665cad87ea622
win <wintercoredev@gmail.com>
2017-04-12 16:46:05 +0700

    9383.2: move code

---
+++ a_mini/projects/Tests/TestFoundamentalVectors/Tests/MyVector.cs
@@ -0,0 +1,129 @@
+﻿//MIT, 2017, WinterDev
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TestFoundamentalVectors
+{
+    public struct Vector
+    {
+        double _x, _y;
+        public Vector(double x, double y)
+        {
+            _x = x; _y = y;
+        }
+        public Vector(PointF pt)
+        {
+            _x = pt.X;
+            _y = pt.Y;
+        }
+        public Vector(PointF st, PointF end)
+        {
+            _x = end.X - st.X;
+            _y = end.Y - st.Y;
+        }
+
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public double Magnitude
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.X, -v.Y);
+        }
+
+        public static Vector operator *(double c, Vector v)
+        {
+            return new Vector(c * v.X, c * v.Y);
+        }
+
+        public static Vector operator *(Vector v, double c)
+        {
+            return new Vector(c * v.X, c * v.Y);
+        }
+
+        public static Vector operator /(Vector v, double c)
+        {
+            return new Vector(v.X / c, v.Y / c);
+        }
+
+        // A * B =|A|.|B|.sin(angle AOB)
+        public double CrossProduct(Vector v)
+        {
+            return _x * v.Y - v.X * _y;
+        }
+
+        // A. B=|A|.|B|.cos(angle AOB)
+        public double DotProduct(Vector v)
+        {
+            return _x * v.X + _y * v.Y;
+        }
+
+        public static bool IsClockwise(PointF pt1, PointF pt2, PointF pt3)
+        {
+            Vector V21 = new Vector(pt2, pt1);
+            Vector v23 = new Vector(pt2, pt3);
+            return V21.CrossProduct(v23) < 0; // sin(angle pt1 pt2 pt3) > 0, 0<angle pt1 pt2 pt3 <180
+        }
+
+        public static bool IsCCW(PointF pt1, PointF pt2, PointF pt3)
+        {
+            Vector V21 = new Vector(pt2, pt1);
+            Vector v23 = new Vector(pt2, pt3);
+            return V21.CrossProduct(v23) > 0;  // sin(angle pt2 pt1 pt3) < 0, 180<angle pt2 pt1 pt3 <360
+        }
+
+        public static double DistancePointLine(PointF pt, PointF lnA, PointF lnB)
+        {
+            Vector v1 = new Vector(lnA, lnB);
+            Vector v2 = new Vector(lnA, pt);
+            v1 /= v1.Magnitude;
+            return Math.Abs(v2.CrossProduct(v1));
+        }
+        public PointF ToPointF()
+        {
+            return new PointF((float)_x, (float)_y);
+        }
+        public void Rotate(int Degree)
+        {
+            double radian = Degree * Math.PI / 180.0;
+            double sin = Math.Sin(radian);
+            double cos = Math.Cos(radian);
+            double nx = _x * cos - _y * sin;
+            double ny = _x * sin + _y * cos;
+            _x = nx;
+            _y = ny;
+        }
+        public Vector NewLength(double newLength)
+        {
+            //radian
+            double atan = Math.Atan2(_y, _x);
+            return new Vector(Math.Cos(atan) * newLength,
+                        Math.Sin(atan) * newLength);
+        }
+    }
+
+}
