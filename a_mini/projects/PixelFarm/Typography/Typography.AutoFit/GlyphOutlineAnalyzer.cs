192a4ba370382b19eadaca41a454e756d555f556
win <wintercoredev@gmail.com>
2017-04-19 15:18:06 +0700

    9459: Generate output from GlyphDynamicOutline

--- a_mini/projects/PixelFarm/Typography/Typography.AutoFit/GlyphOutlineAnalyzer.cs
+++ a_mini/projects/PixelFarm/Typography/Typography.AutoFit/GlyphOutlineAnalyzer.cs
@@ -9,7 +9,7 @@ namespace Typography.Rendering
 
     public class GlyphOutlineAnalyzer
     {
-        GlyphPartFlattener _glyhFlattener = new GlyphPartFlattener();
+        GlyphPartFlattener _glyphFlattener = new GlyphPartFlattener();
         GlyphContourBuilder _glyphToCountor = new GlyphContourBuilder();
         public GlyphOutlineAnalyzer()
         {
@@ -32,10 +32,10 @@ namespace Typography.Rendering
             //
             //3. flatten each contour with the flattener
             int j = contours.Count;
-            _glyhFlattener.NSteps = 2;
+            _glyphFlattener.NSteps = 2;
             for (int i = 0; i < j; ++i)
             {
-                contours[i].Flatten(_glyhFlattener);
+                contours[i].Flatten(_glyphFlattener);
             }
             //4. after flatten, the we can create fit outline
             if (j > 0)
@@ -106,8 +106,6 @@ namespace Typography.Rendering
 
             return new GlyphDynamicOutline(intermediateOutline);
         }
-
-
 
 
         /// <summary>
@@ -275,129 +273,7 @@ namespace Typography.Rendering
             }
 
         }
-#endif
-
-
-        //public struct Vector
-        //{
-        //    double _x, _y;
-        //    public Vector(double x, double y)
-        //    {
-        //        _x = x; _y = y;
-        //    }
-        //    public Vector(Vector pt)
-        //    {
-        //        _x = pt.X;
-        //        _y = pt.Y;
-        //    }
-        //    public Vector(Vector st, Vector end)
-        //    {
-        //        _x = end.X - st.X;
-        //        _y = end.Y - st.Y;
-        //    }
-
-        //    public double X
-        //    {
-        //        get { return _x; }
-        //        set { _x = value; }
-        //    }
-
-        //    public double Y
-        //    {
-        //        get { return _y; }
-        //        set { _y = value; }
-        //    }
-
-        //    public double Magnitude
-        //    {
-        //        get { return Math.Sqrt(X * X + Y * Y); }
-        //    }
-
-        //    public static Vector operator +(Vector v1, Vector v2)
-        //    {
-        //        return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-        //    }
-
-        //    public static Vector operator -(Vector v1, Vector v2)
-        //    {
-        //        return new Vector(v1.X - v2.X, v1.Y - v2.Y);
-        //    }
-
-        //    public static Vector operator -(Vector v)
-        //    {
-        //        return new Vector(-v.X, -v.Y);
-        //    }
-
-        //    public static Vector operator *(double c, Vector v)
-        //    {
-        //        return new Vector(c * v.X, c * v.Y);
-        //    }
-
-        //    public static Vector operator *(Vector v, double c)
-        //    {
-        //        return new Vector(c * v.X, c * v.Y);
-        //    }
-
-        //    public static Vector operator /(Vector v, double c)
-        //    {
-        //        return new Vector(v.X / c, v.Y / c);
-        //    }
-
-        //    // A * B =|A|.|B|.sin(angle AOB)
-        //    public double CrossProduct(Vector v)
-        //    {
-        //        return _x * v.Y - v.X * _y;
-        //    }
-
-        //    // A. B=|A|.|B|.cos(angle AOB)
-        //    public double DotProduct(Vector v)
-        //    {
-        //        return _x * v.X + _y * v.Y;
-        //    }
-
-        //    public static bool IsClockwise(Vector pt1, Vector pt2, Vector pt3)
-        //    {
-        //        Vector V21 = new Vector(pt2, pt1);
-        //        Vector v23 = new Vector(pt2, pt3);
-        //        return V21.CrossProduct(v23) < 0; // sin(angle pt1 pt2 pt3) > 0, 0<angle pt1 pt2 pt3 <180
-        //    }
-
-        //    public static bool IsCCW(Vector pt1, Vector pt2, Vector pt3)
-        //    {
-        //        Vector V21 = new Vector(pt2, pt1);
-        //        Vector v23 = new Vector(pt2, pt3);
-        //        return V21.CrossProduct(v23) > 0;  // sin(angle pt2 pt1 pt3) < 0, 180<angle pt2 pt1 pt3 <360
-        //    }
-
-        //    public static double DistancePointLine(Vector pt, Vector lnA, Vector lnB)
-        //    {
-        //        Vector v1 = new Vector(lnA, lnB);
-        //        Vector v2 = new Vector(lnA, pt);
-        //        v1 /= v1.Magnitude;
-        //        return Math.Abs(v2.CrossProduct(v1));
-        //    }
-
-        //    public void Rotate(int Degree)
-        //    {
-        //        double radian = Degree * Math.PI / 180.0;
-        //        double sin = Math.Sin(radian);
-        //        double cos = Math.Cos(radian);
-        //        double nx = _x * cos - _y * sin;
-        //        double ny = _x * sin + _y * cos;
-        //        _x = nx;
-        //        _y = ny;
-        //    }
-        //    public Vector NewLength(double newLength)
-        //    {
-        //        //radian
-        //        double atan = Math.Atan2(_y, _x);
-        //        return new Vector(Math.Cos(atan) * newLength,
-        //                    Math.Sin(atan) * newLength);
-        //    }
-        //}
-
-
-
+#endif 
 
     }
 }
