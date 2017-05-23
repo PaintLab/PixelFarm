1d2800afd8e61e1cdee34cf02d7b154d29468a17
win <wintercoredev@gmail.com>
2017-05-23 20:54:52 +0700

    9732: TessListener

--- a_mini/projects/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLCanvas/TessListener.cs
+++ a_mini/projects/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLCanvas/TessListener.cs
@@ -37,6 +37,9 @@ namespace PixelFarm.DrawingGL
         List<Vertex> inputVertextList;
         List<Vertex> tempVertextList = new List<Vertex>();
         public List<Vertex> resultVertexList = new List<Vertex>();
+        public List<int> resultIndexList = new List<int>();
+
+        public Tesselator.TriangleListType triangleListType;
         public TessListener2()
         {
             //empty not use
@@ -45,6 +48,9 @@ namespace PixelFarm.DrawingGL
         }
         public void BeginCallBack(Tesselator.TriangleListType type)
         {
+            this.triangleListType = type;
+
+            //what type of triangle list
             //Console.WriteLine("begin: " + type.ToString());
             //Assert.IsTrue(GetNextOutputAsString() == "B");
             //switch (type)
@@ -78,12 +84,13 @@ namespace PixelFarm.DrawingGL
             //Assert.AreEqual(GetNextOutputAsInt(), index); 
             if (index < 0)
             {
-                //use data from temp store
+                //use data from temp store***
                 resultVertexList.Add(this.tempVertextList[-index]);
                 //Console.WriteLine("temp_v_cb:" + index + ":(" + tempVertextList[-index] + ")");
             }
             else
             {
+                resultIndexList.Add(index);
                 resultVertexList.Add(this.inputVertextList[index]);
                 // Console.WriteLine("v_cb:" + index + ":(" + inputVertextList[index] + ")");
             }
@@ -139,9 +146,11 @@ namespace PixelFarm.DrawingGL
         }
         public void Reset(List<Vertex> vertextList)
         {
+            this.triangleListType = Tesselator.TriangleListType.LineLoop;//?
             this.tempVertextList.Clear();
             this.resultVertexList.Clear();
             this.inputVertextList = vertextList;
+            resultIndexList.Clear();
         }
     }
 
@@ -158,6 +167,8 @@ namespace PixelFarm.DrawingGL
             this.tessListener = new TessListener2();
             tessListener.Connect(tess, true);
         }
+
+
         public float[] TessPolygon(float[] vertex2dCoords, int[] contourEndPoints, out int areaCount)
         {
             vertexts.Clear();//reset
@@ -174,7 +185,6 @@ namespace PixelFarm.DrawingGL
             tessListener.Reset(vertexts);
             //-----------------------
             tess.BeginPolygon();
-
             int nContourCount = contourEndPoints.Length;
             int beginAt = 0;
             for (int m = 0; m < nContourCount; ++m)
@@ -189,8 +199,6 @@ namespace PixelFarm.DrawingGL
                 beginAt = thisContourEndAt + 1;
                 tess.EndContour();
             }
-
-
             tess.EndPolygon();
             //-----------------------
             List<Vertex> vertextList = tessListener.resultVertexList;
@@ -210,128 +218,43 @@ namespace PixelFarm.DrawingGL
             areaCount = j;
             return vtx;
         }
+
+        public int[] TessPolygon2(float[] vertex2dCoords, int[] contourEndPoints, out int areaCount)
+        {
+            vertexts.Clear();//reset
+            //
+            int ncoords = vertex2dCoords.Length / 2;
+            if (ncoords == 0) { areaCount = 0; return null; }
+
+            int nn = 0;
+            for (int i = 0; i < ncoords; ++i)
+            {
+                vertexts.Add(new Vertex(vertex2dCoords[nn++], vertex2dCoords[nn++]));
+            }
+            //-----------------------
+            tessListener.Reset(vertexts);
+            //-----------------------
+            tess.BeginPolygon();
+            int nContourCount = contourEndPoints.Length;
+            int beginAt = 0;
+            for (int m = 0; m < nContourCount; ++m)
+            {
+                int thisContourEndAt = (contourEndPoints[m] + 1) / 2;
+                tess.BeginContour();
+                for (int i = beginAt; i < thisContourEndAt; ++i)
+                {
+                    Vertex v = vertexts[i];
+                    tess.AddVertex(v.m_X, v.m_Y, 0, i);
+                }
+                beginAt = thisContourEndAt + 1;
+                tess.EndContour();
+            }
+            tess.EndPolygon();
+            //-----------------------
+            List<Vertex> vertextList = tessListener.resultVertexList;
+            areaCount = vertextList.Count;
+            return tessListener.resultIndexList.ToArray();
+
+        }
     }
-
-    //public class TessListener
-    //{
-
-    //    List<Vertex> inputVertextList;
-    //    List<Vertex> tempVertextList = new List<Vertex>();
-    //    public List<Vertex> resultVertexList = new List<Vertex>();
-    //    public TessListener()
-    //    {
-    //        //empty not use
-    //        //not use first item in temp
-    //        tempVertextList.Add(new Vertex(0, 0));
-    //    }
-    //    public void BeginCallBack(Tesselator.TriangleListType type)
-    //    {
-
-    //        //Console.WriteLine("begin: " + type.ToString());
-    //        //Assert.IsTrue(GetNextOutputAsString() == "B");
-    //        //switch (type)
-    //        //{
-    //        //    case Tesselator.TriangleListType.Triangles:
-    //        //        Assert.IsTrue(GetNextOutputAsString() == "TRI");
-    //        //        break;
-
-    //        //    case Tesselator.TriangleListType.TriangleFan:
-    //        //        Assert.IsTrue(GetNextOutputAsString() == "FAN");
-    //        //        break;
-
-    //        //    case Tesselator.TriangleListType.TriangleStrip:
-    //        //        Assert.IsTrue(GetNextOutputAsString() == "STRIP");
-    //        //        break;
-
-    //        //    default:
-    //        //        throw new Exception("unknown TriangleListType '" + type.ToString() + "'.");
-    //        //}
-    //    }
-
-    //    public void EndCallBack()
-    //    {
-    //        //Assert.IsTrue(GetNextOutputAsString() == "E");
-    //        //Console.WriteLine("end");
-    //    }
-
-    //    public void VertexCallBack(int index)
-    //    {
-    //        //Assert.IsTrue(GetNextOutputAsString() == "V");
-    //        //Assert.AreEqual(GetNextOutputAsInt(), index); 
-    //        if (index < 0)
-    //        {
-    //            //use data from temp store
-    //            resultVertexList.Add(this.tempVertextList[-index]);
-    //            //Console.WriteLine("temp_v_cb:" + index + ":(" + tempVertextList[-index] + ")");
-    //        }
-    //        else
-    //        {
-    //            resultVertexList.Add(this.inputVertextList[index]);
-    //            // Console.WriteLine("v_cb:" + index + ":(" + inputVertextList[index] + ")");
-    //        }
-
-
-    //    }
-
-    //    public void EdgeFlagCallBack(bool IsEdge)
-    //    {
-    //        //Console.WriteLine("edge: " + IsEdge);
-    //        //Assert.IsTrue(GetNextOutputAsString() == "F");
-    //        //Assert.AreEqual(GetNextOutputAsBool(), IsEdge);
-    //    }
-
-    //    public void CombineCallBack(double v0, double v1, double v2, int[] data4,
-    //        double[] weight4, out int outData)
-    //    {
-    //        //double error = .001;
-    //        //Assert.IsTrue(GetNextOutputAsString() == "C");
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), v0, error);
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), v1, error);
-    //        //Assert.AreEqual(GetNextOutputAsInt(), data4[0]);
-    //        //Assert.AreEqual(GetNextOutputAsInt(), data4[1]);
-    //        //Assert.AreEqual(GetNextOutputAsInt(), data4[2]);
-    //        //Assert.AreEqual(GetNextOutputAsInt(), data4[3]);
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), weight4[0], error);
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), weight4[1], error);
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), weight4[2], error);
-    //        //Assert.AreEqual(GetNextOutputAsDouble(), weight4[3], error); 
-    //        //here , outData = index of newly add vertext
-
-
-    //        //----------------------------------------------------------------------
-    //        //*** new vertext is added into user vertext list ***            
-    //        //use negative to note that this vertext is from temporary source 
-
-    //        //other implementation:
-    //        // append to end of input list is ok if the input list can grow up ***
-    //        //----------------------------------------------------------------------
-
-    //        outData = -this.tempVertextList.Count;
-    //        //----------------------------------------
-    //        tempVertextList.Add(new Vertex(v0, v1));
-    //        //----------------------------------------
-    //    }
-    //    public void Connect(Tesselate.Tesselator tesselator, Tesselator.WindingRuleType windingRule, bool setEdgeFlag)
-    //    {
-
-
-    //        tesselator.callBegin = new Tesselate.Tesselator.CallBeginDelegate(BeginCallBack);
-    //        tesselator.callEnd = new Tesselate.Tesselator.CallEndDelegate(EndCallBack);
-    //        tesselator.callVertex = new Tesselate.Tesselator.CallVertexDelegate(VertexCallBack);
-    //        tesselator.callCombine = new Tesselate.Tesselator.CallCombineDelegate(CombineCallBack);
-    //        tesselator.windingRule = windingRule;
-
-    //        if (setEdgeFlag)
-    //        {
-    //            tesselator.callEdgeFlag += new Tesselate.Tesselator.CallEdgeFlagDelegate(EdgeFlagCallBack);
-    //        }
-    //    }
-    //    public void Reset(List<Vertex> vertextList)
-    //    {
-    //        this.tempVertextList.Clear();
-    //        this.resultVertexList.Clear();
-    //        this.inputVertextList = vertextList;
-    //    }
-
-    //}
 }
