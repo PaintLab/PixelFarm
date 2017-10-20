1362fb20e5a8c5a6bd95dea1718d3353d5477847
win <wintercoredev@gmail.com>
2017-10-20 18:08:00 +0700

    9507: CPAL's _colorRBGABuffer

--- a_mini/projects/PixelFarm/Typography/Typography.OpenFont/Tables.AdvancedLayout/CPAL.cs
+++ a_mini/projects/PixelFarm/Typography/Typography.OpenFont/Tables.AdvancedLayout/CPAL.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2017 Sam Hocevar <sam@hocevar.net>
+﻿// Copyright © 2017 Sam Hocevar <sam@hocevar.net>, WinterDev
 // Apache2
 
 using System;
@@ -11,6 +11,7 @@ namespace Typography.OpenFont.Tables
     {
         public override string Name { get { return "CPAL"; } }
 
+        byte[] _colorRBGABuffer;
         // Read the CPAL table
         // https://www.microsoft.com/typography/otspec/cpal.htm
         protected override void ReadContentFrom(BinaryReader reader)
@@ -20,18 +21,24 @@ namespace Typography.OpenFont.Tables
             ushort version = reader.ReadUInt16();
             ushort entryCount = reader.ReadUInt16(); // XXX: unused?
             ushort paletteCount = reader.ReadUInt16();
-            Colors = new byte[reader.ReadUInt16()][];
+            ushort colorCount = reader.ReadUInt16();
             uint colorsOffset = reader.ReadUInt32();
 
             Palettes = Utils.ReadUInt16Array(reader, paletteCount);
 
             reader.BaseStream.Seek(offset + colorsOffset, SeekOrigin.Begin);
-            for (int i = 0; i < Colors.Length; ++i)
-                Colors[i] = reader.ReadBytes(4);
+            _colorRBGABuffer = reader.ReadBytes(4 * colorCount);
         }
-
         public ushort[] Palettes { get; private set; }
-        public byte[][] Colors { get; private set; }
+        public void GetColor(int colorIndex, out byte r, out byte g, out byte b, out byte a)
+        {
+            byte[] colorRBGABuffer = _colorRBGABuffer;
+            int startAt = colorIndex * 4;//rgba
+            r = colorRBGABuffer[startAt];
+            g = colorRBGABuffer[startAt + 1];
+            b = colorRBGABuffer[startAt + 2];
+            a = colorRBGABuffer[startAt + 3];
+        } 
     }
 }
 
