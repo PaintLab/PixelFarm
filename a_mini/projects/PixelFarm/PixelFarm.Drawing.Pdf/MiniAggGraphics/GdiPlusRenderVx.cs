1b84c71c476006f6c588686488c4ea83c3b8814c
win <wintercoredev@gmail.com>
2017-11-27 06:11:38 +0700

    9649: start pdfsharp bridge
    start with the same code as Gdi+ canvas

---
+++ a_mini/projects/PixelFarm/PixelFarm.Drawing.Pdf/MiniAggGraphics/GdiPlusRenderVx.cs
@@ -0,0 +1,22 @@
+﻿//MIT, 2016-2017, WinterDev
+
+using PixelFarm.Agg;
+namespace PixelFarm.Drawing.WinGdi
+{
+    class WinGdiRenderVx : RenderVx
+    {
+        internal VertexStoreSnap snap;
+        internal System.Drawing.Drawing2D.GraphicsPath path;
+        public WinGdiRenderVx(VertexStoreSnap snap)
+        {
+            this.snap = snap;
+        }
+    }
+    class WinGdiRenderVxFormattedString : RenderVxFormattedString
+    {
+        public WinGdiRenderVxFormattedString(string str)
+        {
+            this.OriginalString = str;
+        }
+    }
+}
