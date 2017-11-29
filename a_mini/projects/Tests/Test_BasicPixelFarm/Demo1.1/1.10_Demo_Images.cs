3d5890c940680ef003e81d7fe973fa968dcece2b
win <wintercoredev@gmail.com>
2017-11-29 08:42:11 +0700

    9976_Demo_Images

---
+++ a_mini/projects/Tests/Test_BasicPixelFarm/Demo1.1/1.10_Demo_Images.cs
@@ -0,0 +1,27 @@
+﻿//Apache2, 2014-2017, WinterDev
+
+using System.Collections.Generic;
+using PixelFarm.Drawing;
+using LayoutFarm.UI;
+namespace LayoutFarm
+{
+    [DemoNote("1.10 MultipleImages")]
+    class Demo_MultipleImages : DemoBase
+    {
+        protected override void OnStartDemo(SampleViewport viewport)
+        {
+            ImageBinder imgBinder = new ClientImageBinder(null);
+            imgBinder.SetImage(LoadBitmap("../../Data/imgs/favorites32.png"));
+
+            for (int i = 0; i < 100; ++i)
+            {
+				//share 1 img binder with multiple img boxes
+                var imgBox = new CustomWidgets.ImageBox(imgBinder.Image.Width, imgBinder.Image.Height);
+                imgBox.ImageBinder = imgBinder;
+                imgBox.SetLocation(i * 10, i * 10);
+                viewport.AddContent(imgBox);
+            }
+
+        }
+    }
+}
