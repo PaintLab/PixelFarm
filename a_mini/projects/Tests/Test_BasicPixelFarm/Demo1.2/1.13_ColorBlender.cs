3118387549210122b7ec75a67a7807d25240908b
win <wintercoredev@gmail.com>
2017-11-30 04:12:34 +0700

    9985: init example
    from Wiesław Šoltés ( @wieslawsoltes)'s ColorBlender, MIT license,
    see original at https://github.com/wieslawsoltes/ColorBlender

---
+++ a_mini/projects/Tests/Test_BasicPixelFarm/Demo1.2/1.13_ColorBlender.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// (from https://github.com/wieslawsoltes/ColorBlender)
+//MIT, 2017, WinterDev
+
+using ColorBlender;
+using ColorBlender.Algorithms;
+
+namespace LayoutFarm
+{
+    [DemoNote("1.13 ColorBlenderExample")]
+    class DemoColorBlender : DemoBase
+    {
+        ColorMatch colorMatch;
+        protected override void OnStartDemo(SampleViewport viewport)
+        {
+
+            
+            //-------------------------------------
+            {
+                //horizontal scrollbar
+                var scbar = new LayoutFarm.CustomWidgets.ScrollBar(200, 15);
+                scbar.ScrollBarType = CustomWidgets.ScrollBarType.Horizontal;
+                scbar.SetLocation(80, 10);
+                scbar.MinValue = 0;
+                scbar.MaxValue = 255*10;
+                scbar.SmallChange = 1;
+                viewport.AddContent(scbar);
+            }
+            {
+                //horizontal scrollbar
+                var scbar = new LayoutFarm.CustomWidgets.ScrollBar(200, 15);
+                scbar.ScrollBarType = CustomWidgets.ScrollBarType.Horizontal;
+                scbar.SetLocation(80, 40);
+                scbar.MinValue = 0;
+                scbar.MaxValue = 255 * 10;
+                scbar.SmallChange = 1;
+                viewport.AddContent(scbar);
+            }
+
+            {
+                //horizontal scrollbar
+                var scbar = new LayoutFarm.CustomWidgets.ScrollBar(200, 15);
+                scbar.ScrollBarType = CustomWidgets.ScrollBarType.Horizontal;
+                scbar.SetLocation(80, 80);
+                scbar.MinValue = 0;
+                scbar.MaxValue = 255 * 10;
+                scbar.SmallChange = 1;
+                viewport.AddContent(scbar);
+            }
+            //-------------------------------------
+
+
+
+            colorMatch = new ColorMatch(213, 46, 49);
+            RGB rgbValue = colorMatch.CurrentRGB;
+
+            int x = 300;
+            int y = 20;
+
+            var cmd_R = new LayoutFarm.CustomWidgets.SimpleBox(30, 30);
+            cmd_R.BackColor = new PixelFarm.Drawing.Color((byte)rgbValue.R, 0, 0);
+            cmd_R.SetLocation(x, y);
+
+            var cmd_G = new LayoutFarm.CustomWidgets.SimpleBox(30, 30);
+            cmd_G.BackColor = new PixelFarm.Drawing.Color(0, (byte)rgbValue.G, 0);
+            cmd_G.SetLocation(x + 30, y + 0);
+
+            //
+            var cmd_B = new LayoutFarm.CustomWidgets.SimpleBox(30, 30);
+            cmd_B.BackColor = new PixelFarm.Drawing.Color(0, 0, (byte)rgbValue.B);
+            cmd_B.SetLocation(x + 60, y + 0);
+
+            var cmd_RGB = new LayoutFarm.CustomWidgets.SimpleBox(30, 30);
+            cmd_RGB.BackColor = new PixelFarm.Drawing.Color((byte)rgbValue.R,
+                (byte)rgbValue.G, (byte)rgbValue.B);
+            cmd_RGB.SetLocation(x + 90, y + 0);
+
+
+
+            viewport.AddContent(cmd_R);
+            viewport.AddContent(cmd_G);
+            viewport.AddContent(cmd_B);
+            viewport.AddContent(cmd_RGB);
+        }
+
+    }
+}
---
+++ a_mini/projects/Tests/Test_BasicPixelFarm/Demo1.2/ColorBlender/RGB.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// (from https://github.com/wieslawsoltes/ColorBlender)
+
+using System;
+
+namespace ColorBlender
+{
+    public class RGB
+    {
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+
+        public RGB()
+        {
+        }
+
+        public RGB(double r, double g, double b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+        public RGB(RGB rg)
+        {
+            this.R = rg.R;
+            this.G = rg.G;
+            this.B = rg.B;
+        }
+
+        public RGB(HSV hs)
+        {
+            RGB rg = hs.ToRGB();
+            this.R = rg.R;
+            this.G = rg.G;
+            this.B = rg.B;
+        }
+
+        public HSV ToHSV()
+        {
+            HSV hs = new HSV();
+            RGB rg = new RGB(this.R, this.G, this.B);
+
+            var m = rg.R;
+            if (rg.G < m) { m = rg.G; }
+            if (rg.B < m) { m = rg.B; }
+            var v = rg.R;
+            if (rg.G > v) { v = rg.G; }
+            if (rg.B > v) { v = rg.B; }
+            var value = 100 * v / 255;
+            var delta = v - m;
+            if (v == 0.0) { hs.S = 0; } else { hs.S = 100 * delta / v; }
+
+            if (hs.S == 0) { hs.H = 0; }
+            else
+            {
+                if (rg.R == v) { hs.H = 60.0 * (rg.G - rg.B) / delta; }
+                else if (rg.G == v) { hs.H = 120.0 + 60.0 * (rg.B - rg.R) / delta; }
+                else if (rg.B == v) { hs.H = 240.0 + 60.0 * (rg.R - rg.G) / delta; }
+                if (hs.H < 0.0) { hs.H = hs.H + 360.0; }
+            }
+
+            hs.H = Math.Round(hs.H);
+            hs.S = Math.Round(hs.S);
+            hs.V = Math.Round(value);
+
+            return hs;
+        }
+    }
+}
