16f7c643c6508e5a646872020f56da8bb926e37b
win <wintercoredev@gmail.com>
2017-12-07 19:24:53 +0700

    9721.7: TestBedStartup

--- a_mini/projects/Tests/Test_BasicPixelFarm/Program.cs
+++ a_mini/projects/Tests/Test_BasicPixelFarm/Program.cs
@@ -11,25 +11,13 @@ namespace TestGraphicPackage2
         static void Main()
         {
 
-#if GL_ENABLE
-            OpenTK.Toolkit.Init();
-#endif
+            YourImplementation.TestBedStartup.StartSetup(); 
             //-------------------------------
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false); 
-            //you can use your font loader
-            YourImplementation.BootStrapWinGdi.SetupDefaultValues();
-
-#if GL_ENABLE
-            YourImplementation.BootStrapOpenGLES2.SetupDefaultValues();
-#endif
-            //default text breaker, this bridge between 
-            LayoutFarm.Composers.Default.TextBreaker = new LayoutFarm.Composers.MyManagedTextBreaker();
-
             ////------------------------------- 
             formDemoList = new LayoutFarm.Dev.FormDemoList();
-            formDemoList.LoadDemoList(typeof(Program));
-
+            formDemoList.LoadDemoList(typeof(Program)); 
             Application.Run(formDemoList);
         }
 
---
+++ a_mini/projects/Tests/WinFormTestBed/YourImplStartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace YourImplementation
+{
+    public static class TestBedStartup
+    {
+        public static void StartSetup()
+        {
+#if GL_ENABLE
+            OpenTK.Toolkit.Init();
+#endif
+            //you can use your font loader
+            YourImplementation.BootStrapWinGdi.SetupDefaultValues();
+
+#if GL_ENABLE
+            YourImplementation.BootStrapOpenGLES2.SetupDefaultValues();
+#endif
+            //default text breaker, this bridge between 
+            LayoutFarm.Composers.Default.TextBreaker = new LayoutFarm.Composers.MyManagedTextBreaker();
+
+        }
+    }
+}
