1123427cb965f27f804500a78aa521abb743b6c0
win <wintercoredev@gmail.com>
2017-12-11 22:24:58 +0700

    9788.1: move code

--- a_mini/projects/PixelFarm/Typography/Typography.OpenFont/Tables.AdvancedLayout/ScriptLang.cs
+++ a_mini/projects/PixelFarm/Typography/Typography.OpenFont/Tables.AdvancedLayout/ScriptLang.cs
@@ -18,7 +18,18 @@ namespace Typography.OpenFont
             return this.fullname;
         }
     }
-
+    public static class UnicodeLangBitsExtension
+    {
+        public static UnicodeRangeInfo ToUnicodeRangeInfo(this UnicodeLangBits unicodeLangBits)
+        {
+            long bits = (long)unicodeLangBits;
+            int bitpos = (int)(bits >> 32);
+            int lower32 = (int)(bits & 0xFFFFFFFF);
+            return new UnicodeRangeInfo(bitpos,
+                lower32 >> 16,
+                lower32 & 0xFFFF);
+        }
+    }
 
     //unicode range 
     public struct UnicodeRangeInfo
