eec55b0b24908bca09cc02a4f018a189cc416f76
win <wintercoredev@gmail.com>
2017-12-13 20:47:02 +0700

    9801: add some ISegmentExtensions

--- a_mini/projects/PixelFarm/PixelFarm.TreeCollection/ISegment.cs
+++ a_mini/projects/PixelFarm/PixelFarm.TreeCollection/ISegment.cs
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 
 //from namespace MonoDevelop.Core.Text
 namespace PixelFarm.TreeCollection
@@ -50,5 +51,93 @@ namespace PixelFarm.TreeCollection
         /// <remarks>EndOffset = Offset + Length;</remarks>
         int EndOffset { get; }
     }
+    /// <summary>
+	/// Extension methods for <see cref="ISegment"/>.
+	/// </summary>
+	public static class ISegmentExtensions
+    {
+        /// <summary>
+        /// Gets whether <paramref name="segment"/> fully contains the specified segment.
+        /// </summary>
+        /// <remarks>
+        /// Use <c>segment.Contains(offset, 0)</c> to detect whether a segment (end inclusive) contains offset;
+        /// use <c>segment.Contains(offset, 1)</c> to detect whether a segment (end exclusive) contains offset.
+        /// </remarks>
+        public static bool Contains(this ISegment segment, int offset, int length)
+        {
+            if (segment == null)
+                throw new ArgumentNullException("segment");
+            return segment.Offset <= offset && offset + length <= segment.EndOffset;
+        }
 
+        /// <summary>
+        /// Gets whether <paramref name="segment"/> fully contains the specified segment.
+        /// </summary>
+        public static bool Contains(this ISegment segment, ISegment span)
+        {
+            if (segment == null)
+                throw new ArgumentNullException("segment");
+            if (span == null)
+                throw new ArgumentNullException("span");
+            return segment.Offset <= span.Offset && span.EndOffset <= segment.EndOffset;
+        }
+
+        /// <summary>
+        /// Gets whether the offset is within the <paramref name="segment"/>.
+        /// </summary>
+        public static bool Contains(this ISegment segment, int offset)
+        {
+            if (segment == null)
+                throw new ArgumentNullException("segment");
+            return unchecked((uint)(offset - segment.Offset) < (uint)segment.Length);
+        }
+
+        /// <summary>
+        /// Gets whether the offset is within the <paramref name="segment"/>.
+        /// </summary>
+        public static bool IsInside(this ISegment segment, int offset)
+        {
+            if (segment == null)
+                throw new ArgumentNullException("segment");
+            return unchecked((uint)(offset - segment.Offset) <= (uint)segment.Length);
+        }
+
+        /// <summary>
+        /// Determines whether <paramref name="other"/> overlaps this span. Two spans are considered to overlap 
+        /// if they have positions in common and neither is empty. Empty spans do not overlap with any 
+        /// other span.
+        /// </summary>
+        public static bool OverlapsWith(this ISegment segment, ISegment other)
+        {
+            int overlapStart = Math.Max(segment.Offset, other.Offset);
+            int overlapEnd = Math.Min(segment.EndOffset, other.EndOffset);
+            return overlapStart < overlapEnd;
+        }
+
+        //public static ISegment AdjustSegment(this ISegment segment, TextChangeEventArgs args)
+        //{
+        //    if (segment == null)
+        //        throw new ArgumentNullException("segment");
+        //    var newStartOffset = args.GetNewOffset(segment.Offset);
+        //    var newEndOffset = args.GetNewOffset(segment.EndOffset);
+        //    return new TextSegment(newStartOffset, newEndOffset - newStartOffset);
+        //}
+
+        //public static IEnumerable<ISegment> AdjustSegments(this IEnumerable<ISegment> segments, TextChangeEventArgs args)
+        //{
+        //    if (segments == null)
+        //        throw new ArgumentNullException("segments");
+        //    foreach (var segment in segments)
+        //    {
+        //        yield return segment.AdjustSegment(args);
+        //    }
+        //}
+
+        public static bool IsInvalid(this ISegment segment)
+        {
+            if (segment == null)
+                throw new ArgumentNullException(nameof(segment));
+            return segment.Offset < 0;
+        }
+    }
 }
