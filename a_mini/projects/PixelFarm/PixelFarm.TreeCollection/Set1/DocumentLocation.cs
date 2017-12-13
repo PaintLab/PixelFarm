762110ee764417dedf3733c37bc0a1286f7bf6be
win <wintercoredev@gmail.com>
2017-12-13 21:48:42 +0700

    9804: move code

---
+++ a_mini/projects/PixelFarm/PixelFarm.TreeCollection/Set1/DocumentLocation.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PixelFarm.TreeCollection
+{
+
+    public struct DocumentLocation : IComparable<DocumentLocation>, IEquatable<DocumentLocation>
+    {
+        /// <summary>
+        /// Represents no text location (0, 0).
+        /// </summary>
+        public static readonly DocumentLocation Empty = new DocumentLocation(0, 0);
+
+        /// <summary>
+        /// Constant of the minimum line.
+        /// </summary>
+        public const int MinLine = 1;
+
+        /// <summary>
+        /// Constant of the minimum column.
+        /// </summary>
+        public const int MinColumn = 1;
+
+        /// <summary>
+        /// Creates a TextLocation instance.
+        /// </summary>
+        public DocumentLocation(int line, int column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        int column, line;
+
+        /// <summary>
+        /// Gets the line number.
+        /// </summary>
+        public int Line
+        {
+            get { return line; }
+        }
+
+        /// <summary>
+        /// Gets the column number.
+        /// </summary>
+        public int Column
+        {
+            get { return column; }
+        }
+
+        /// <summary>
+        /// Gets whether the TextLocation instance is empty.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return column < MinLine && line < MinColumn;
+            }
+        }
+
+        /// <summary>
+        /// Gets a string representation for debugging purposes.
+        /// </summary>
+        public override string ToString()
+        {
+            return string.Format(System.Globalization.CultureInfo.InvariantCulture, "(Line {1}, Col {0})", this.column, this.line);
+        }
+
+        /// <summary>
+        /// Gets a hash code.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return unchecked(column << 20 ^ line);
+        }
+
+        /// <summary>
+        /// Equality test.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DocumentLocation)) return false;
+            return (DocumentLocation)obj == this;
+        }
+
+        /// <summary>
+        /// Equality test.
+        /// </summary>
+        public bool Equals(DocumentLocation other)
+        {
+            return this == other;
+        }
+
+        /// <summary>
+        /// Equality test.
+        /// </summary>
+        public static bool operator ==(DocumentLocation left, DocumentLocation right)
+        {
+            return left.column == right.column && left.line == right.line;
+        }
+
+        /// <summary>
+        /// Inequality test.
+        /// </summary>
+        public static bool operator !=(DocumentLocation left, DocumentLocation right)
+        {
+            return left.column != right.column || left.line != right.line;
+        }
+
+        /// <summary>
+        /// Compares two text locations.
+        /// </summary>
+        public static bool operator <(DocumentLocation left, DocumentLocation right)
+        {
+            if (left.line < right.line)
+                return true;
+            else if (left.line == right.line)
+                return left.column < right.column;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Compares two text locations.
+        /// </summary>
+        public static bool operator >(DocumentLocation left, DocumentLocation right)
+        {
+            if (left.line > right.line)
+                return true;
+            else if (left.line == right.line)
+                return left.column > right.column;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Compares two text locations.
+        /// </summary>
+        public static bool operator <=(DocumentLocation left, DocumentLocation right)
+        {
+            return !(left > right);
+        }
+
+        /// <summary>
+        /// Compares two text locations.
+        /// </summary>
+        public static bool operator >=(DocumentLocation left, DocumentLocation right)
+        {
+            return !(left < right);
+        }
+
+        //public static implicit operator Microsoft.CodeAnalysis.Text.LinePosition(DocumentLocation location)
+        //{
+        //    return new Microsoft.CodeAnalysis.Text.LinePosition(location.Line - 1, location.Column - 1);
+        //}
+
+        //public static implicit operator DocumentLocation(Microsoft.CodeAnalysis.Text.LinePosition location)
+        //{
+        //    return new DocumentLocation(location.Line + 1, location.Character + 1);
+        //}
+
+        /// <summary>
+        /// Compares two text locations.
+        /// </summary>
+        public int CompareTo(DocumentLocation other)
+        {
+            if (this == other)
+                return 0;
+            if (this < other)
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+
+}
--- a_mini/projects/Tests/Test_TreeCollection/Form1.cs
+++ a_mini/projects/Tests/Test_TreeCollection/Form1.cs
@@ -37,5 +37,10 @@ namespace Test_TreeCollection
             tests.TestAddBug();
             tests.TestRemoveBug();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
