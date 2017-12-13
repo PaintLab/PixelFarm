8b42623ad1905c936b6555b0e2bed2cc24ce7611
win <wintercoredev@gmail.com>
2017-12-13 21:24:56 +0700

    9803: TextSegment

--- a_mini/projects/PixelFarm/PixelFarm.TreeCollection/ISegment.cs
+++ a_mini/projects/PixelFarm/PixelFarm.TreeCollection/ISegment.cs
@@ -51,6 +51,324 @@ namespace PixelFarm.TreeCollection
         /// <remarks>EndOffset = Offset + Length;</remarks>
         int EndOffset { get; }
     }
+
+
+    /// <summary>
+	/// An (Offset, Length) pair representing a text span.
+	/// </summary>
+	public struct TextSegment : IEquatable<TextSegment>, ISegment
+    {
+        public static readonly TextSegment Invalid = new TextSegment(-1, 0);
+
+        readonly int offset;
+
+        /// <summary>
+        ///  Gets the start offset of the segment. 
+        /// </summary>
+        /// <value>
+        /// The offset.
+        /// </value>
+        public int Offset
+        {
+            get
+            {
+                return offset;
+            }
+        }
+
+        readonly int length;
+
+        /// <summary>
+        /// Gets the length of the segment. 
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        /// <summary>
+        /// Gets the end offset of the segment. 
+        /// </summary>
+        /// <remarks>
+        /// EndOffset = Offset + Length;
+        /// </remarks>
+        /// <value>
+        /// The end offset.
+        /// </value>
+        public int EndOffset
+        {
+            get
+            {
+                return Offset + Length;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is empty.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsEmpty
+        {
+            get
+            {
+                return Length == 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is invalid.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is invalid; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsInvalid
+        {
+            get
+            {
+                return Offset < 0;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextSegment"/> struct.
+        /// </summary>
+        /// <param name='offset'>
+        /// The offset of the segment.
+        /// </param>
+        /// <param name='length'>
+        /// The length of the segment.
+        /// </param>
+        public TextSegment(int offset, int length)
+        {
+            this.offset = offset;
+            this.length = length;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextSegment"/> struct.
+        /// </summary>
+        public TextSegment(ISegment segment) : this(segment.Offset, segment.Length)
+        {
+        }
+
+        public static bool operator ==(TextSegment left, TextSegment right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(TextSegment left, TextSegment right)
+        {
+            return !Equals(left, right);
+        }
+
+        public static bool Equals(TextSegment left, TextSegment right)
+        {
+            return left.Offset == right.Offset && left.Length == right.Length;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is inside the specified offset. 
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if this instance is inside the specified offset (upper bound inclusive); otherwise, <c>false</c>.
+        /// </returns>
+        /// <param name='offset'>
+        /// The offset offset.
+        /// </param>
+        public bool IsInside(int offset)
+        {
+            return Offset <= offset && offset <= EndOffset;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="TextSegment"/> is equal to the current <see cref="TextSegment"/>.
+        /// </summary>
+        /// <param name='other'>
+        /// The <see cref="TextSegment"/> to compare with the current <see cref="TextSegment"/>.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="TextSegment"/> is equal to the current
+        /// <see cref="TextSegment"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Equals(TextSegment other)
+        {
+            return Equals(this, other);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to the current <see cref="TextSegment"/>.
+        /// </summary>
+        /// <param name='obj'>
+        /// The <see cref="System.Object"/> to compare with the current <see cref="TextSegment"/>.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="System.Object"/> is equal to the current
+        /// <see cref="TextSegment"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            var otherSegment = obj as ISegment;
+            if (otherSegment == null)
+                return false;
+            return Offset == otherSegment.Offset && length == otherSegment.Length;
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a <see cref="TextSegment"/> object.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return Offset ^ Length;
+        }
+
+        public static TextSegment FromBounds(int startOffset, int endOffset)
+        {
+            if (startOffset > endOffset)
+                throw new ArgumentOutOfRangeException("endOffset", "endOffset < startOffset");
+            return new TextSegment(startOffset, endOffset - startOffset);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents the current <see cref="TextSegment"/>.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents the current <see cref="TextSegment"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("[TextSegment: Offset={0}, Length={1}]", Offset, Length);
+        }
+    }
+
+    /// <summary>
+    /// An abstract implementation of the ISegment (Offset, Length) pair representing a text span.
+    /// </summary>
+    public abstract class AbstractSegment : ISegment
+    {
+        readonly int offset;
+
+        /// <summary>
+        ///  Gets the start offset of the segment. 
+        /// </summary>
+        /// <value>
+        /// The offset.
+        /// </value>
+        public int Offset
+        {
+            get
+            {
+                return offset;
+            }
+        }
+
+        readonly int length;
+
+        /// <summary>
+        /// Gets the length of the segment. 
+        /// </summary>
+        /// <value>
+        /// The length.
+        /// </value>
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        /// <summary>
+        /// Gets the end offset of the segment. 
+        /// </summary>
+        /// <remarks>
+        /// EndOffset = Offset + Length;
+        /// </remarks>
+        /// <value>
+        /// The end offset.
+        /// </value>
+        public int EndOffset
+        {
+            get
+            {
+                return Offset + Length;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is empty.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is empty; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsEmpty
+        {
+            get
+            {
+                return Length == 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is invalid.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is invalid; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsInvalid
+        {
+            get
+            {
+                return Offset < 0;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextSegment"/> struct.
+        /// </summary>
+        /// <param name='offset'>
+        /// The offset of the segment.
+        /// </param>
+        /// <param name='length'>
+        /// The length of the segment.
+        /// </param>
+        protected AbstractSegment(int offset, int length)
+        {
+            if (length < 0)
+                throw new System.ArgumentOutOfRangeException(nameof(length), "was " + length);
+            this.offset = offset;
+            this.length = length;
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextSegment"/> struct.
+        /// </summary>
+        protected AbstractSegment(ISegment segment)
+        {
+            if (segment == null)
+                throw new ArgumentNullException("segment");
+            this.offset = segment.Offset;
+            this.length = segment.Length;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[AbstractSegment: Offset={0}, Length={1}]", Offset, Length);
+        }
+    }
+
+
     /// <summary>
 	/// Extension methods for <see cref="ISegment"/>.
 	/// </summary>
