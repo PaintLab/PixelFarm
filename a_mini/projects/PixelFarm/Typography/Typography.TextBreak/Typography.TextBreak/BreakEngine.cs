68af2598e23f80b09fc83d22ce6c187283832155
win <wintercoredev@gmail.com>
2017-12-14 08:24:54 +0700

    9807: temp comment unused code

--- a_mini/projects/PixelFarm/Typography/Typography.TextBreak/Typography.TextBreak/BreakEngine.cs
+++ a_mini/projects/PixelFarm/Typography/Typography.TextBreak/Typography.TextBreak/BreakEngine.cs
@@ -26,42 +26,44 @@ namespace Typography.TextBreak
         Number,
         Punc
     }
-    public abstract class TextBreaker
-    {
-        public abstract void DoBreak(char[] input, int start, int len, OnBreak onbreak);
-        public TextBreakKind BreakKind
-        {
-            get;
-            set;
-        }
-        public void DoBreak(char[] charBuff, OnBreak onbreak)
-        {
-            IsCanceled = false;//reset 
-            //to end
-            DoBreak(charBuff, 0, charBuff.Length, onbreak);
-        }
+
+
+    //public abstract class TextBreaker
+    //{
+    //    public abstract void DoBreak(char[] input, int start, int len, OnBreak onbreak);
+    //    public TextBreakKind BreakKind
+    //    {
+    //        get;
+    //        set;
+    //    }
+    //    public void DoBreak(char[] charBuff, OnBreak onbreak)
+    //    {
+    //        IsCanceled = false;//reset 
+    //        //to end
+    //        DoBreak(charBuff, 0, charBuff.Length, onbreak);
+    //    }
        
-        protected bool IsCanceled { get; set; }
-        /// <summary>
-        /// cancel current breaking task
-        /// </summary>
-        public void Cancel() { IsCanceled = true; }
-    }
-    public struct SplitBound
-    {
-        public readonly int startIndex;
-        public readonly int length;
-        public SplitBound(int startIndex, int length)
-        {
-            this.startIndex = startIndex;
-            this.length = length;
-        }
-#if DEBUG
-        public override string ToString()
-        {
-            return startIndex + ":" + length;
-        }
-#endif
-    }
+    //    protected bool IsCanceled { get; set; }
+    //    /// <summary>
+    //    /// cancel current breaking task
+    //    /// </summary>
+    //    public void Cancel() { IsCanceled = true; }
+    //}
+//    public struct SplitBound
+//    {
+//        public readonly int startIndex;
+//        public readonly int length;
+//        public SplitBound(int startIndex, int length)
+//        {
+//            this.startIndex = startIndex;
+//            this.length = length;
+//        }
+//#if DEBUG
+//        public override string ToString()
+//        {
+//            return startIndex + ":" + length;
+//        }
+//#endif
+//    }
 
 }
