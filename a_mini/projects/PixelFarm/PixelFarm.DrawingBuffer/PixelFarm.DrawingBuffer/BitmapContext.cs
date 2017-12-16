38d32fba8b5625eab029ae78b6cba6d3195ad32e
win <wintercoredev@gmail.com>
2017-12-16 19:43:40 +0700

    9866.1

--- a_mini/projects/PixelFarm/PixelFarm.DrawingBuffer/PixelFarm.DrawingBuffer/BitmapContext.cs
+++ a_mini/projects/PixelFarm/PixelFarm.DrawingBuffer/PixelFarm.DrawingBuffer/BitmapContext.cs
@@ -128,4 +128,34 @@ namespace PixelFarm.DrawingBuffer
             Array.Clear(pixels, 0, pixels.Length);
         }
     }
+
+
+    /// <summary>
+    /// Provides the WriteableBitmap context pixel data
+    /// </summary>
+    public static partial class WriteableBitmapContextExtensions
+    {
+        /// <summary>
+        /// Gets a BitmapContext within which to perform nested IO operations on the bitmap
+        /// </summary>
+        /// <remarks>For WPF the BitmapContext will lock the bitmap. Call Dispose on the context to unlock</remarks>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        public static BitmapContext GetBitmapContext(this BitmapBuffer bmp)
+        {
+            return new BitmapContext(bmp);
+        }
+
+        /// <summary>
+        /// Gets a BitmapContext within which to perform nested IO operations on the bitmap
+        /// </summary>
+        /// <remarks>For WPF the BitmapContext will lock the bitmap. Call Dispose on the context to unlock</remarks>
+        /// <param name="bmp">The bitmap.</param>
+        /// <param name="mode">The ReadWriteMode. If set to ReadOnly, the bitmap will not be invalidated on dispose of the context, else it will</param>
+        /// <returns></returns>
+        public static BitmapContext GetBitmapContext(this BitmapBuffer bmp, ReadWriteMode mode)
+        {
+            return new BitmapContext(bmp, mode);
+        }
+    }
 }
