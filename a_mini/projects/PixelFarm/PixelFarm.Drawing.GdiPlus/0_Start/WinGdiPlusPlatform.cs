d1a49c6fd9cda582aa2e8a15ff097479252fd796
win <wintercoredev@gmail.com>
2017-12-18 08:20:38 +0700

    9911: move code

--- a_mini/projects/PixelFarm/PixelFarm.Drawing.GdiPlus/0_Start/WinGdiPlusPlatform.cs
+++ a_mini/projects/PixelFarm/PixelFarm.Drawing.GdiPlus/0_Start/WinGdiPlusPlatform.cs
@@ -1,5 +1,5 @@
 ﻿//BSD, 2014-2017, WinterDev 
-
+ 
 using Typography.TextServices;
 namespace PixelFarm.Drawing.WinGdi
 {
@@ -7,10 +7,9 @@ namespace PixelFarm.Drawing.WinGdi
     {
 
         static WinGdiPlusPlatform()
-        {
-            PixelFarm.Agg.AggBuffMx.SetNaiveBufferImpl(new Win32AggBuffMx());           
+        { 
         }
-
+     
         public static void SetFontEncoding(System.Text.Encoding encoding)
         {
             WinGdiTextService.SetDefaultEncoding(encoding);
@@ -24,35 +23,7 @@ namespace PixelFarm.Drawing.WinGdi
             return new Gdi32IFonts();
         }
     }
-
-
-
-    class Win32AggBuffMx : PixelFarm.Agg.AggBuffMx
-    {
-
-        protected override void InnerMemCopy(byte[] dest_buffer, int dest_startAt, byte[] src_buffer, int src_StartAt, int len)
-        {
-            unsafe
-            {
-                fixed (byte* head_dest = &dest_buffer[dest_startAt])
-                fixed (byte* head_src = &src_buffer[src_StartAt])
-                {
-                    Win32.MyWin32.memcpy(head_dest, head_src, len);
-                }
-            }
-        }
-        protected override void InnerMemSet(byte[] dest, int startAt, byte value, int count)
-        {
-            unsafe
-            {
-                fixed (byte* head = &dest[startAt])
-                {
-                    Win32.MyWin32.memset(head, value, count);
-                }
-            }
-        }
-
-    }
+     
 
 
 
