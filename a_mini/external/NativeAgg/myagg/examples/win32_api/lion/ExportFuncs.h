ecd44f918108bcc6c296c9d3847f10b152ff2345
win <wintercoredev@gmail.com>
2017-12-21 18:41:49 +0700

    move folders 2

--- a_mini/external/NativeAgg/myagg/examples/win32_api/lion/ExportFuncs.h
+++
@@ -1,9 +0,0 @@
-#define MY_DLL_EXPORT __declspec(dllexport)  
- 
-typedef void (*del02)(int oIndex,int methodName);
-
-MY_DLL_EXPORT int LibGetVersion();
-MY_DLL_EXPORT int RegisterManagedCallBack(del02 callback,int callBackKind);
-MY_DLL_EXPORT int TestCallBack();  
-MY_DLL_EXPORT int CallServices(int serviceNumber); 
-
--- a_mini/external/myfonts/myft/msdfgen/msdfgen_main.h
+++
@@ -1,2 +0,0 @@
-//int msdfgen_main(int argc, const char * const *argv);
-int msdfgen_main(int argc, char **argv);
