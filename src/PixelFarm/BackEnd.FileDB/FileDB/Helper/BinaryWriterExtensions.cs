36827cce83153ccd8ef05ba65bc733a6e5e95afa
win <wintercoredev@gmail.com>
2018-01-14 20:40:42 +0700

    205: add FileDB for simple font cache

---
+++ src/PixelFarm/BackEnd.FileDB/FileDB/Helper/BinaryWriterExtensions.cs
@@ -0,0 +1,68 @@
+﻿//MIT, 2015, Mauricio David
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Numeria.IO
+{
+    internal static class BinaryWriterExtensions
+    {
+        private const int MAX_TRY_LOCK_FILE = 50; // Max try to lock the data file
+        private const int DELAY_TRY_LOCK_FILE = 50; // in miliseconds
+
+        public static void Write(this BinaryWriter writer, Guid guid)
+        {
+            writer.Write(guid.ToByteArray());
+        }
+
+        public static void Write(this BinaryWriter writer, DateTime dateTime)
+        {
+            writer.Write(dateTime.Ticks);
+        }
+        /// <summary>
+        /// move to specific positioin from the begining of stream
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public static long SetWritePos(this BinaryWriter writer, long position)
+        {
+            return writer.BaseStream.Seek(position, SeekOrigin.Begin);
+        }
+
+        public static void Lock(this BinaryWriter writer, long position, long length)
+        {
+            var fileStream = writer.BaseStream as FileStream;
+
+            TryLockFile(fileStream, position, length, 0);
+        }
+
+        private static void TryLockFile(FileStream fileStream, long position, long length, int tryCount)
+        {
+            try
+            {
+                fileStream.Lock(position, length);
+            }
+            catch (IOException ex)
+            {
+                if (ex.IsLockException())
+                {
+                    if (tryCount >= DELAY_TRY_LOCK_FILE)
+                        throw new FileDBException("Database file is in lock for a long time");
+
+                    Thread.Sleep(tryCount * DELAY_TRY_LOCK_FILE);
+
+                    TryLockFile(fileStream, position, length, ++tryCount);
+                }
+                else
+                    throw ex;
+            }
+        }
+
+        public static void Unlock(this BinaryWriter writer, long position, long length)
+        {
+            var fileStream = writer.BaseStream as FileStream;
+            fileStream.Unlock(position, length);
+        }
+    }
+}
---
+++ src/PixelFarm/BackEnd.FileDB/FileDB/Structure/BasePage.cs
@@ -0,0 +1,24 @@
+﻿//MIT, 2015, Mauricio David
+namespace Numeria.IO
+{
+    internal enum PageType
+    {
+        /// <summary>
+        /// Data = 1
+        /// </summary>
+        Data = 1, 
+        /// <summary>
+        /// Index = 2
+        /// </summary>
+        Index = 2
+    }
+
+    internal abstract class BasePage
+    {
+        public const int PAGE_SIZE = 4096;
+
+        public uint PageID { get; set; }
+        public abstract PageType Type { get; }
+        public uint NextPageID { get; set; }
+    }
+}
