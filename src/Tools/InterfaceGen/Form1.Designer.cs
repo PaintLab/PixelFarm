c86363985935c272e844fdfec30f59316e717186
win <wintercoredev@gmail.com>
2018-01-25 18:33:16 +0700

    254: add interface gen

--- src/Tools/InterfaceGen/Form1.Designer.cs
+++ src/Tools/InterfaceGen/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.cmd3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -73,11 +74,22 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 430);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 78);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(628, 520);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.cmd3);
@@ -93,6 +105,7 @@
         private System.Windows.Forms.Button cmd3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- src/Tools/InterfaceGen/Form1.cs
+++ src/Tools/InterfaceGen/Form1.cs
@@ -80,15 +80,21 @@ namespace InterfaceGen
             byte[] dataBuffer = null;
             using (System.Drawing.Bitmap bmp = new Bitmap("d:\\WImageTest\\smaa_textureArea.png"))
             {
+                bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
                 var bmpdata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly,
                       System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
                 dataBuffer = new byte[bmpdata.Stride * bmpdata.Height];
                 System.Runtime.InteropServices.Marshal.Copy(bmpdata.Scan0, dataBuffer, 0, dataBuffer.Length);
                 bmp.UnlockBits(bmpdata);
+
+                //--
+                bmp.Save("d:\\WImageTest\\smaa_text2.png");
             }
 
             string base64 = Convert.ToBase64String(dataBuffer);
+           
             //int j = dataBuffer.Length;
             //StringBuilder stbuilder = new StringBuilder();
 
@@ -283,57 +289,92 @@ namespace InterfaceGen
 
 
 
-            int stride = 160 * 2;
+            int stride = 160 * 3;
             byte[] rgbBuffer = new byte[stride * 560];
 
 
             int readIndex = 0;
             int writeIndex = 0;
-           
-             
-            int writeRowNo = 560 - 1;
 
-            for (int row = 0; row < 560; ++row)
+
+
+            //invert image
+            bool invertOutput = false;
+
+
+            if (invertOutput)
             {
-                writeIndex = writeRowNo * stride;
-                for (int col = 0; col < 160; ++col)
+                int writeRowNo = 560 - 1;
+                for (int row = 0; row < 560; ++row)
                 {
-                    rgbBuffer[writeIndex] = AreaTex.areaTexBytes[readIndex];
-                    rgbBuffer[writeIndex + 1] = AreaTex.areaTexBytes[readIndex + 1];
-                    readIndex += 2;
-                    writeIndex += 2;
+                    writeIndex = writeRowNo * stride;
+                    for (int col = 0; col < 160; ++col)
+                    {
+                        rgbBuffer[writeIndex] = AreaTex.areaTexBytes[readIndex];
+                        rgbBuffer[writeIndex + 1] = AreaTex.areaTexBytes[readIndex + 1];
 
+                        readIndex += 2;
+                        writeIndex += 3;
+                    }
+                    writeRowNo--;
                 }
-                writeRowNo--;
+            }
+            else
+            {
+                int writeRowNo = 0;
+                for (int row = 0; row < 560; ++row)
+                {
+                    writeIndex = writeRowNo * stride;
+                    for (int col = 0; col < 160; ++col)
+                    {
+                        //BGRA
+                        rgbBuffer[writeIndex + 1] = AreaTex.areaTexBytes[readIndex + 1];
+                        rgbBuffer[writeIndex + 2] = AreaTex.areaTexBytes[readIndex];
+
+                        readIndex += 2;
+                        writeIndex += 3;
+                    }
+                    writeRowNo++;
+                }
+                SaveOutputImageAsRGB24(rgbBuffer, "d:\\WImageTest\\area_nz1.png");
             }
 
-            //for (int i = 0; i < pixelCount; ++i)
-            //{
-            //    rgbBuffer[writeIndex] = AreaTex.areaTexBytes[readIndex];
-            //    rgbBuffer[writeIndex + 1] = AreaTex.areaTexBytes[readIndex + 1];
-            //    writeIndex += 2;
-            //    readIndex += 2;
-            //}
 
             ////finish
             ////string base64Str = Convert.ToBase64String(AreaTex.areaTexBytes);
             string base64StrOfRGB = Convert.ToBase64String(rgbBuffer);
 
+
+        }
+        void SaveOutputImageAsRGB24(byte[] rgbBuffer, string filename)
+        {
             using (System.Drawing.Bitmap bmp = new Bitmap(160, 560, System.Drawing.Imaging.PixelFormat.Format24bppRgb))
             {
-
                 var bmpdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
                 System.Runtime.InteropServices.Marshal.Copy(
                   rgbBuffer, 0,
                   bmpdata.Scan0, rgbBuffer.Length);
                 bmp.UnlockBits(bmpdata);
-
-
-                bmp.Save("d:\\WImageTest\\AreaTex_n3.png");
+                bmp.Save(filename);
             }
-
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tt_01 = "d:\\WImageTest\\tt_01.png";
+            using (System.Drawing.Bitmap bmp = new Bitmap(tt_01))
+            {
+                var bmpdata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+                int buffLen = bmpdata.Stride * bmpdata.Height;
 
+                byte[] buffer = new byte[buffLen];
+                System.Runtime.InteropServices.Marshal.Copy(bmpdata.Scan0, buffer, 0, buffLen);
+
+
+                bmp.UnlockBits(bmpdata);
+
+                string base64StrOfRGB = Convert.ToBase64String(buffer);
+            }
+        }
     }
 }
