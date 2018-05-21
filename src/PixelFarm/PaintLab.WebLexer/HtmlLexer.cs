4dc80fa1845748359138d14baf95f3082eee179b
win <wintercoredev@gmail.com>
2018-05-21 10:39:41 +0700

    301: rearrange PaintLab.WebEssential
    Css, Svg

--- src/PixelFarm/PaintLab.WebLexer/HtmlLexer.cs
+++ src/PixelFarm/PaintLab.WebLexer/HtmlLexer.cs
@@ -27,30 +27,30 @@ namespace LayoutFarm.WebDom.Parser
         CommentContent
     }
 
-    enum HtmlLexState
-    {
-        Init,
-        AfterOpenAngle
-    }
+    //enum HtmlLexState
+    //{
+    //    Init,
+    //    AfterOpenAngle
+    //}
 
-    public delegate void HtmlLexerEventHandler(HtmlLexerEvent lexEvent, int startIndex, int len);
-    public abstract partial class HtmlLexer
-    {
-        public event HtmlLexerEventHandler LexStateChanged;
-        protected void RaiseStateChanged(HtmlLexerEvent lexEvent, int startIndex, int len)
-        {
-            LexStateChanged(lexEvent, startIndex, len);
-        }
-        public virtual void Analyze(TextSnapshot textSnapshot) { }
-        public virtual void BeginLex()
-        {
-        }
-        public virtual void EndLex()
-        {
-        }
-        public static HtmlLexer CreateLexer()
-        {
-            return new MyHtmlLexer();
-        }
-    }
+    //public delegate void HtmlLexerEventHandler(HtmlLexerEvent lexEvent, int startIndex, int len);
+    //public abstract partial class HtmlLexer
+    //{
+    //    public event HtmlLexerEventHandler LexStateChanged;
+    //    protected void RaiseStateChanged(HtmlLexerEvent lexEvent, int startIndex, int len)
+    //    {
+    //        LexStateChanged(lexEvent, startIndex, len);
+    //    }
+    //    public virtual void Analyze(TextSnapshot textSnapshot) { }
+    //    public virtual void BeginLex()
+    //    {
+    //    }
+    //    public virtual void EndLex()
+    //    {
+    //    }
+    //    public static HtmlLexer CreateLexer()
+    //    {
+    //        return new MyHtmlLexer();
+    //    }
+    //}
 }
