4fccadac87b055ea94ba8428433f47f54b006a04
win <wintercoredev@gmail.com>
2018-05-29 17:04:19 +0700

    346: remove abstract ,unused class TextBreaker

--- src/PixelFarm/Typography/Typography.TextBreak/Typography.TextBreak/BreakEngine.cs
+++ src/PixelFarm/Typography/Typography.TextBreak/Typography.TextBreak/BreakEngine.cs
@@ -3,12 +3,7 @@
 
 namespace Typography.TextBreak
 {
-    public enum TextBreakKind
-    {
-        Word,
-        Sentence,
-    }
-
+   
     public delegate void OnBreak(BreakBounds breakBounds);
 
     public class BreakBounds
@@ -26,27 +21,7 @@ namespace Typography.TextBreak
         Number,
         Punc
     }
-    public abstract class TextBreaker
-    {
-        public abstract void DoBreak(char[] input, int start, int len, OnBreak onbreak);
-        public TextBreakKind BreakKind
-        {
-            get;
-            set;
-        }
-        public void DoBreak(char[] charBuff, OnBreak onbreak)
-        {
-            IsCanceled = false;//reset 
-            //to end
-            DoBreak(charBuff, 0, charBuff.Length, onbreak);
-        }
-       
-        protected bool IsCanceled { get; set; }
-        /// <summary>
-        /// cancel current breaking task
-        /// </summary>
-        public void Cancel() { IsCanceled = true; }
-    }
+  
     public struct SplitBound
     {
         public readonly int startIndex;
