b37daf74cc721d3db3a736115eb6268123dd7b83
win <wintercoredev@gmail.com>
2018-06-01 17:50:04 +0700

    362: VectorToolBox

---
+++ src/PixelFarm/PixelFarm.DrawingCanvas/5_Agg_DrawElements/VectorToolBox.cs
@@ -0,0 +1,63 @@
+﻿//BSD, 2014-2018, WinterDev
+
+using System.Collections.Generic;
+namespace PixelFarm.Drawing
+{
+    public static class VectorToolBox
+    {
+        [System.ThreadStatic]
+        static Stack<VertexStore> s_vxsPool = new Stack<VertexStore>();
+        public static void GetFreeVxs(out VertexStore vxs1)
+        {
+            vxs1 = GetFreeVxs();
+        }
+        public static void GetFreeVxs(out VertexStore vxs1, out VertexStore vxs2)
+        {
+            vxs1 = GetFreeVxs();
+            vxs2 = GetFreeVxs();
+        }
+        public static void ReleaseVxs(ref VertexStore vxs1, ref VertexStore vxs2)
+        {
+            ReleaseVxs(ref vxs1);
+            ReleaseVxs(ref vxs2);
+        }
+        public static void ReleaseVxs(ref VertexStore vxs1)
+        {
+            vxs1.Clear();
+            s_vxsPool.Push(vxs1);
+            vxs1 = null;
+        }
+        static VertexStore GetFreeVxs()
+        {
+            if (s_vxsPool.Count > 0)
+            {
+                return s_vxsPool.Pop();
+            }
+            else
+            {
+                return new VertexStore();
+            }
+        }
+
+
+
+        static Stack<Agg.Stroke> s_strokePool = new Stack<Agg.Stroke>();
+        public static void GetFreeStroke(out Agg.Stroke stroke, int w)
+        {
+            if (s_strokePool.Count > 0)
+            {
+                stroke = s_strokePool.Pop();
+                stroke.Width = w;
+            }
+            else
+            {
+                stroke = new Agg.Stroke(w);
+            }
+        }
+        public static void ReleaseStroke(ref Agg.Stroke stroke)
+        {
+            s_strokePool.Push(stroke);
+            stroke = null;
+        }
+    }
+}
