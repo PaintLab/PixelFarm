c5912fd26d2c55ed6f0f6141161a87e140fdcae9
win <wintercoredev@gmail.com>
2018-06-02 00:10:27 +0700

    365: IEventListener

--- src/PixelFarm/PaintLab.PlatformBridge/Events/IEventListener.cs
+++ src/PixelFarm/PaintLab.PlatformBridge/Events/IEventListener.cs
@@ -2,63 +2,12 @@
 
 namespace LayoutFarm.UI
 {
-    public interface IUIEventListener
-    {
-        //--------------------------------------------------------------------------
-        void ListenKeyPress(UIKeyEventArgs args);
-        void ListenKeyDown(UIKeyEventArgs e);
-        void ListenKeyUp(UIKeyEventArgs e);
-        bool ListenProcessDialogKey(UIKeyEventArgs args);
-        //--------------------------------------------------------------------------
-        void ListenMouseDown(UIMouseEventArgs e);
-        void ListenMouseMove(UIMouseEventArgs e);
-        void ListenMouseUp(UIMouseEventArgs e);
-        void ListenMouseLeave(UIMouseEventArgs e);
-        void ListenMouseWheel(UIMouseEventArgs e);
-        void ListenLostMouseFocus(UIMouseEventArgs e);
-        //-------------------------------------------------------------------------- 
-        void ListenMouseClick(UIMouseEventArgs e);
-        void ListenMouseDoubleClick(UIMouseEventArgs e);
-        //--------------------------------------------------------------------------
-        void ListenGotKeyboardFocus(UIFocusEventArgs e);
-        void ListenLostKeyboardFocus(UIFocusEventArgs e);
-        //--------------------------------------------------------------------------  
-        void ListenInterComponentMsg(object sender, int msgcode, string msg);
-        void ListenGuestTalk(UIGuestTalkEventArgs e);
-        //-------------------------------------------------------------------------- 
-        void HandleContentLayout();
-        void HandleContentUpdate();
-        void HandleElementUpdate();
-        //--------------------------------------------------------------------------
-        bool BypassAllMouseEvents { get; }
-        bool AutoStopMouseEventPropagation { get; }
-        void GetGlobalLocation(out int x, out int y);
-        //--------------------------------------------------------------------------  
-    }
-
-    //public interface IUIElement
-    //{
-    //    bool AttachEventListener(IEventListener eventListener);
-    //}
-    //public interface IUIRootElement
-    //{
-    //    IEventListener CreateEventListener();
-    //    IUIElement CreateElement(string elemName);
-    //    void AddContent(IUIElement uiElement);
-    //}
-
+    
     /// <summary>
     /// can listen to some event
     /// </summary>
     public interface IEventListener
     {
-        event UIEventHandler<UIMouseEventArgs> MouseDown;
-        event UIEventHandler<UIMouseEventArgs> MouseUp;
-        event UIEventHandler<UIMouseEventArgs> MouseMove;
-        //
-        event UIEventHandler<UIKeyEventArgs> KeyDown;
-        event UIEventHandler<UIKeyEventArgs> KeyPress;
-        event UIEventHandler<UIKeyEventArgs> KeyUp;
 
 
         //--------------------------------------------------------------------------
@@ -85,7 +34,18 @@ namespace LayoutFarm.UI
         //-------------------------------------------------------------------------- 
 
     }
+    public interface IUIEventListener : IEventListener
+    {
+        //-------------------------------------------------------------------------- 
+        void HandleContentLayout();
+        void HandleContentUpdate();
+        void HandleElementUpdate();
+        //--------------------------------------------------------------------------
+        bool BypassAllMouseEvents { get; }
+        bool AutoStopMouseEventPropagation { get; }
+        void GetGlobalLocation(out int x, out int y);
+        //--------------------------------------------------------------------------  
+    }
 
-     
     public delegate void UIEventHandler<T>(T e) where T : System.EventArgs;
 }
