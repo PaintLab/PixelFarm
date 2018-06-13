644725f9c8ed821bfee651f623d58113cceb5e2f
win <wintercoredev@gmail.com>
2018-06-13 21:28:32 +0700

    render img

--- external/SoftEngineGdiPlus/Form1.Designer.cs
+++ external/SoftEngineGdiPlus/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.chkWireFrame = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,11 +62,22 @@
             this.chkWireFrame.UseVisualStyleBackColor = true;
             this.chkWireFrame.CheckedChanged += new System.EventHandler(this.chkWireFrame_CheckedChanged);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Render2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1132, 921);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.chkWireFrame);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox chkWireFrame;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- external/SoftEngineGdiPlus/Form1.cs
+++ external/SoftEngineGdiPlus/Form1.cs
@@ -17,6 +17,11 @@ namespace SoftEngine
         Mesh mesh = new Mesh("Cube", 8, 12);
         Camera mera = new Camera();
         Texture _texture;
+
+
+        //
+        Texture _simpleTexture2;
+        Mesh _myrectMesh;
         public Form1()
         {
             InitializeComponent();
@@ -57,6 +62,38 @@ namespace SoftEngine
             mera.Position = new Vector3d(0, 0, 10.0f);
             mera.Target = Vector3d.Zero;
             _meshes = device.LoadJSONFileAsync("monkey.babylon");
+
+            //----------
+
+            //---------------------
+            //another scene 
+            _simpleTexture2 = new Texture("favorites32.png", 32, 32);
+            //create a simple rect 
+            _myrectMesh = new Mesh("test", 4, 2);
+            //_myrectMesh.Vertices[0] = new Vertex() { Coordinates = new Vector3d(-0.5, 0, 1), Normal = new Vector3d(-0.5, 0, -1), TextureCoordinates = new Vector2d(0, 1) };
+            //_myrectMesh.Vertices[1] = new Vertex() { Coordinates = new Vector3d(-0.5, 0.5, 1), Normal = new Vector3d(-0.5, 0.5, -1), TextureCoordinates = new Vector2d(0, 0) };
+            //_myrectMesh.Vertices[2] = new Vertex() { Coordinates = new Vector3d(0.5, 0.5, 1), Normal = new Vector3d(0.5, 0.5, -1), TextureCoordinates = new Vector2d(1, 0) };
+            //_myrectMesh.Vertices[3] = new Vertex() { Coordinates = new Vector3d(0.5, 0, 1), Normal = new Vector3d(0.5, 0, -1), TextureCoordinates = new Vector2d(1, 1) };
+
+            //_myrectMesh.Vertices[0] = new Vertex() { Coordinates = new Vector3d(-1, 0, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(0, 0) };
+            //_myrectMesh.Vertices[1] = new Vertex() { Coordinates = new Vector3d(-1, 1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(0, 1) };
+            //_myrectMesh.Vertices[2] = new Vertex() { Coordinates = new Vector3d(0, 1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(1, 1) };
+            //_myrectMesh.Vertices[3] = new Vertex() { Coordinates = new Vector3d(0, 0, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(1, 0) };
+
+
+            _myrectMesh.Vertices[0] = new Vertex() { Coordinates = new Vector3d(-1, 1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(0, 1) };
+            _myrectMesh.Vertices[1] = new Vertex() { Coordinates = new Vector3d(-1, -1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(0, 0) };
+            _myrectMesh.Vertices[2] = new Vertex() { Coordinates = new Vector3d(0, 1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(1, 1) };
+            _myrectMesh.Vertices[3] = new Vertex() { Coordinates = new Vector3d(0, -1, 1), Normal = new Vector3d(0, 0, 1), TextureCoordinates = new Vector2d(1, 0) };
+
+
+
+            //each face is defined by a set of coords A,B,C
+            _myrectMesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
+            _myrectMesh.Faces[1] = new Face { A = 2, B = 1, C = 3 };
+            //
+            _myrectMesh.ComputeFaceNormals();
+            _myrectMesh.Texture = _simpleTexture2;
         }
         Mesh[] _meshes;
         System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
@@ -104,6 +141,44 @@ namespace SoftEngine
         private void chkWireFrame_CheckedChanged(object sender, EventArgs e)
         {
             UpdateOutput();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //test drawing a simple rect image on the screen
+
+
+            sw.Reset();
+            sw.Start();
+
+            //-------------------------
+            device.Clear(0, 0, 0, 255);
+
+            //device.RenderWireFrame(mera, _myrectMesh);
+
+
+            if (this.chkWireFrame.Checked)
+            {
+                device.RenderWireFrame(mera, _myrectMesh);
+            }
+            else
+            {
+                device.Render(mera, _myrectMesh);
+            }
+            //-------------------------
+
+            var bmpdata = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            device.Present(bmpdata.Scan0);
+            bmp.UnlockBits(bmpdata);
+
+
+            this._graphics.Clear(Color.White);
+            this._graphics.DrawImage(bmp, 0, 0);
+            sw.Stop();
+
+            //
+            long ms = sw.ElapsedMilliseconds;
+            this.Text = "interval: " + ms + "ms, " + (1000f / ms) + " fps";
         }
     }
 }
