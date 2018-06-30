1996998aad13c6dd8a85ee8d55d3541af4df1438
win <wintercoredev@gmail.com>
2018-06-30 15:08:22 +0700

    659: build pass

--- src/PixelFarm/Typography/Typography.TextBreak/Typography.TextBreak/DictionaryBasedBreakingEngine/CjBreakingEngine.cs
+++
@@ -1,5 +0,0 @@
-﻿//MIT, 2016-present, WinterDev
-// some code from icu-project
-// © 2016 and later: Unicode, Inc. and others.
-// License & terms of use: http://www.unicode.org/copyright.html#License
-
