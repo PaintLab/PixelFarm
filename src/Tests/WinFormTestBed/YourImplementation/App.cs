e84fb136699b2142bb3c1e43e0fa5d7cea26c984
win <wintercoredev@gmail.com>
2018-07-16 00:29:18 +0700

    712.1

--- src/Tests/WinFormTestBed/YourImplementation/App.cs
+++ src/Tests/WinFormTestBed/YourImplementation/App.cs
@@ -1,6 +1,5 @@
 ﻿//Apache2, 2014-present, WinterDev
-using System;
-using PixelFarm.Drawing;
+
 
 namespace LayoutFarm
 {
@@ -17,12 +16,6 @@ namespace LayoutFarm
         public virtual string Desciption
         {
             get { return ""; }
-        }
-        public static Image LoadBitmap(string filename)
-        {
-            System.Drawing.Bitmap gdiBmp = new System.Drawing.Bitmap(filename);
-            GdiPlusBitmap bmp = new GdiPlusBitmap(gdiBmp.Width, gdiBmp.Height, gdiBmp);
-            return bmp;
         }
     }
 
