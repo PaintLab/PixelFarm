e67b258bdf83267dcbdb4e149f01fd50fa0a938e
win <wintercoredev@gmail.com>
2018-07-28 13:11:09 +0700

    770.1

--- src/PixelFarm/PaintLab.Svg/SvgParserExtensions.cs
+++ src/PixelFarm/PaintLab.Svg/SvgParserExtensions.cs
@@ -129,7 +129,7 @@ namespace PixelFarm.CpuBlit
             }
             if (spec.HasStrokeWidth)
             {
-                cmds.Add(new VgCmdStrokeWidth(spec.StrokeWidth.Number)); 
+                cmds.Add(new VgCmdStrokeWidth(spec.StrokeWidth.Number));
             }
             if (spec.Transform != null)
             {
@@ -349,49 +349,4 @@ namespace PixelFarm.CpuBlit
             }
         }
     }
-
-    //public static class SvgParserExt
-    //{
-    //    //public static SvgRenderVx GetResultAsRenderVx(this PaintLab.Svg.SvgParser parser)
-    //    //{
-
-    //    //    ////resolve all elements...
-    //    //    //int j = _renderVxList.Count;
-
-    //    //    ////TODO: review here
-    //    //    ////temp fix 
-    //    //    //for (int i = 0; i < j; ++i)
-    //    //    //{
-    //    //    //    SvgPart svgpart = _renderVxList[i];
-    //    //    //    if (svgpart.SvgClipPath != null)
-    //    //    //    {
-    //    //    //        //var clipPathRef = svgpart.SvgClipPath as SvgClipPathReference;
-    //    //    //        //if (clipPathRef.ResolvedClip == null)
-    //    //    //        //{
-    //    //    //        //    //resolve this clip
-    //    //    //        //    SvgClipPath clipPath = FindSvgClipPathById(clipPathRef.RefName);
-    //    //    //        //    if (clipPath != null)
-    //    //    //        //    {
-    //    //    //        //        SvgClipPath foundClip = FindSvgClipPathById(clipPathRef.RefName);
-    //    //    //        //    }
-    //    //    //        //    else
-    //    //    //        //    {
-
-    //    //    //        //    }
-    //    //    //        //}
-    //    //    //    }
-    //    //    //}
-
-    //    //    //var result = new SvgRenderVx(_renderVxList.ToArray());
-    //    //    ////result.SetSvgDefs(_defsList.ToArray());
-    //    //    //return result;
-    //    //    return null;
-    //    //}
-
-
-    //    ////   ------------------------------------
-
-
-    //}
-
 }
