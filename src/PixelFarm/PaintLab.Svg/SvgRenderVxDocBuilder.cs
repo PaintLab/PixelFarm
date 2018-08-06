fb784481f8f98789ed99ed62449f61fda37a1056
win <wintercoredev@gmail.com>
2018-08-06 08:17:00 +0700

    829: SetController

--- src/PixelFarm/PaintLab.Svg/SvgRenderVxDocBuilder.cs
+++ src/PixelFarm/PaintLab.Svg/SvgRenderVxDocBuilder.cs
@@ -855,19 +855,20 @@ namespace PaintLab.Svg
                     renderE = new SvgRenderElement(WellknownSvgElementName.Svg, null);
                     break;
                 case WellknownSvgElementName.Rect:
-                    renderE = CreateRect(parentNode, (SvgRectSpec)elem.ElemSpec);
+                    renderE = CreateRect(parentNode, (SvgRectSpec)elem.ElemSpec); 
                     break;
                 case WellknownSvgElementName.Image:
                     renderE = CreateImage(parentNode, (SvgImageSpec)elem.ElemSpec);
+                    
                     break;
                 case WellknownSvgElementName.Polyline:
-                    renderE = CreatePolyline(parentNode, (SvgPolylineSpec)elem.ElemSpec);
+                    renderE = CreatePolyline(parentNode, (SvgPolylineSpec)elem.ElemSpec); 
                     break;
                 case WellknownSvgElementName.Polygon:
-                    renderE = CreatePolygon(parentNode, (SvgPolygonSpec)elem.ElemSpec);
+                    renderE = CreatePolygon(parentNode, (SvgPolygonSpec)elem.ElemSpec); 
                     break;
                 case WellknownSvgElementName.Ellipse:
-                    renderE = CreateEllipse(parentNode, (SvgEllipseSpec)elem.ElemSpec);
+                    renderE = CreateEllipse(parentNode, (SvgEllipseSpec)elem.ElemSpec); 
                     break;
                 case WellknownSvgElementName.Circle:
                     renderE = CreateCircle(parentNode, (SvgCircleSpec)elem.ElemSpec);
@@ -883,6 +884,7 @@ namespace PaintLab.Svg
                     break;
             }
 
+            renderE.SetController(elem);
             parentNode.AddChildElement(renderE);
             int childCount = elem.ChildCount;
             for (int i = 0; i < childCount; ++i)
