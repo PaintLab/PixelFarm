595b7f154a0d54f9a64dbb0473964ff16b35980e
win <wintercoredev@gmail.com>
2018-10-13 18:48:26 +0700

    1103: init load  opentk
    copy from
    https://github.com/PaintLab/opentk/commit/33e25b0c78e43a77c7e6d9d7f5e0ff64c1266856

---
+++ src/PixelFarm/BackEnd.MiniOpenTK/examples/BasicTriangle/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using OpenTK;
+
+using OpenTK.Graphics.ES30;
+namespace BasicTriangle
+{
+    sealed class Program : GameWindow
+    {
+
+        static Program()
+        {
+            OpenTK.Platform.Factory.GetCustomPlatformFactory = () => OpenTK.Platform.Egl.EglAngle.NewFactory();
+
+            Toolkit.Init(new ToolkitOptions
+            {
+                Backend = PlatformBackend.PreferNative,
+
+            });
+
+            OpenTK.Graphics.PlatformAddressPortal.GetAddressDelegate = OpenTK.Platform.Utilities.CreateGetAddress();
+        }
+
+        //A simple vertex shader possible.Just passes through the position vector.
+        //const string VertexShaderSource = @"
+        //    #version 330
+
+        //    layout(location = 0) in vec4 position;
+
+        //    void main(void)
+        //    {
+        //        gl_Position = position;
+        //    }
+        //";
+
+        //// A simple fragment shader. Just a constant red color.
+        //const string FragmentShaderSource = @"
+        //    #version 330
+
+        //    out vec4 outputColor;
+
+        //    void main(void)
+        //    {
+        //        outputColor = vec4(1.0, 0.0, 0.0, 1.0);
+        //    }
+        //";
+
+        //string VertexShaderSource = @"      
+
+        //    attribute vec2 a_position;
+        //    attribute vec4 a_color;
+
+        //    varying vec4 v_color;
+
+        //    void main()
+        //    {   
+
+        //        gl_Position = vec4(a_position[0],a_position[1],0,1);  
+        //        v_color = a_color;
+        //    }
+        //    ";
+        ////fragment source
+        //string FragmentShaderSource = @"
+        //        precision mediump float;
+        //        varying vec4 v_color;                 
+        //        void main()
+        //        { 
+        //            gl_FragColor = v_color;
+        //        }
+        //    ";
+
+        string VertexShaderSource =
+            "#version 300 es\n" +
+            @"in vec2 a_position;
+              in vec4 a_color; 
+              out vec4 v_color;
+              void main()
+              {    
+                gl_Position = vec4(a_position[0],a_position[1],0,1);  
+                v_color = a_color;
+             }
+            ";
+        //fragment source
+        string FragmentShaderSource =
+            "#version 300 es\n" +
+            @"  precision mediump float;
+                in vec4 v_color;        
+                out vec4 color;
+                void main()
+                { 
+                    color = v_color;
+                }
+            ";
+        // Points of a triangle in normalized device coordinates.
+        readonly float[] Points = new float[] {
+            // X, Y, Z, W
+            -0.5f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, 0.0f, 1.0f,
+            0.0f, 0.5f, 0.0f, 1.0f };
+
+        int VertexShader;
+        int FragmentShader;
+        int ShaderProgram;
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // Load the source of the vertex shader and compile it.
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+            GL.CompileShader(VertexShader);
+
+            // Load the source of the fragment shader and compile it.
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+            GL.CompileShader(FragmentShader);
+
+            // Create the shader program, attach the vertex and fragment shaders and link the program.
+            ShaderProgram = GL.CreateProgram();
+            GL.AttachShader(ShaderProgram, VertexShader);
+            GL.AttachShader(ShaderProgram, FragmentShader);
+            GL.LinkProgram(ShaderProgram);
+            // Retrive the position location from the program.
+            var positionLocation = GL.GetAttribLocation(ShaderProgram, "a_position");
+
+
+            // Create the vertex buffer object (VBO) for the vertex data.
+            VertexBufferObject = GL.GenBuffer();
+            // Bind the VBO and copy the vertex data into it.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, Points.Length * sizeof(float), Points, BufferUsageHint.StaticDraw);
+
+
+            // Create the vertex array object (VAO) for the program.
+            //VertexArrayObject = GL.GenVertexArray();
+            GL.GenVertexArrays(1, out VertexArrayObject);
+            // Bind the VAO and setup the position attribute.
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(positionLocation);
+
+            // Set the clear color to blue
+            GL.ClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            // Unbind all the resources by binding the targets to 0/null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+            GL.DeleteProgram(ShaderProgram);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+
+            base.OnUnload(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            // Resize the viewport to match the window size.
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            // Clear the color buffer.
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // Bind the VBO
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            // Bind the VAO
+            GL.BindVertexArray(VertexArrayObject);
+            // Use/Bind the program
+            GL.UseProgram(ShaderProgram);
+            // This draws the triangle.
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            // Swap the front/back buffers so what we just rendered to the back buffer is displayed in the window.
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            var program = new Program();
+            program.Run();
+        }
+    }
+}
---
+++ src/PixelFarm/BackEnd.MiniOpenTK/src/OpenTK/0_MyExtension/ES20mini/Helper.cs
@@ -0,0 +1,568 @@
+﻿//
+// The Open Toolkit Library License
+//
+// Copyright (c) 2006 - 2013 the Open Toolkit library.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+#if !MINIMAL
+using System.Drawing;
+#endif
+using System.Runtime.InteropServices;
+
+
+namespace OpenTK.Graphics.ES20
+{
+    /// <summary>
+    /// Provides access to OpenGL ES 2.0 methods.
+    /// </summary>
+    public sealed partial class GL : GraphicsBindingsBase
+    {
+#if IPHONE
+        private const string Library = "/System/Library/Frameworks/OpenGLES.framework/OpenGLES";
+#else
+        private const string Library = "libGLESv2.dll";
+#endif
+        private static readonly object sync_root = new object();
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public GL()
+        {
+
+        }
+        public override void LoadEntryPoints()
+        {
+            GLDelInit.LoadAll();
+        }
+        /// <summary>
+        /// Returns a synchronization token unique for the GL class.
+        /// </summary>
+        protected override object SyncRoot
+        {
+            get { return sync_root; }
+        }
+
+        //-----------------------------------------------------------------
+        //
+        public static int GenBuffer()
+        {
+            int newBufferId;
+            GL.GenBuffers(1, out newBufferId);
+            return newBufferId;
+        }
+        public static void DeleteBuffer(int bufferId)
+        {
+            unsafe
+            {
+                int* arr = stackalloc int[1];
+                arr[0] = bufferId;
+                GL.DeleteBuffers(1, arr);
+            }
+        }
+
+        public static void BufferData(BufferTarget target, int byteLen, float[] points, BufferUsageHint usageHint)
+        {
+            unsafe
+            {
+                fixed (float* h = points)
+                {
+                    GL.BufferData(target, (IntPtr)byteLen, (IntPtr)h, usageHint);
+                }
+            }
+        }
+
+        //-----------------------------------------------------------------
+#pragma warning disable 3019
+#pragma warning disable 1591
+#pragma warning disable 1572
+#pragma warning disable 1573
+
+        // Note: Mono 1.9.1 truncates StringBuilder results (for 'out string' parameters).
+        // We work around this issue by doubling the StringBuilder capacity.
+
+        public static void ClearColor(Color color)
+        {
+            GL.ClearColor(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
+        }
+
+        public static void ClearColor(Color4 color)
+        {
+            GL.ClearColor(color.R, color.G, color.B, color.A);
+        }
+
+        public static void BlendColor(Color color)
+        {
+            GL.BlendColor(color.R / 255.0f, color.G / 255.0f, color.B / 255.0f, color.A / 255.0f);
+        }
+
+        public static void BlendColor(Color4 color)
+        {
+            GL.BlendColor(color.R, color.G, color.B, color.A);
+        }
+
+        [CLSCompliant(false)]
+        public static void Uniform2(int location, ref Vector2 vector)
+        {
+            GL.Uniform2(location, vector.X, vector.Y);
+        }
+
+        [CLSCompliant(false)]
+        public static void Uniform3(int location, ref Vector3 vector)
+        {
+            GL.Uniform3(location, vector.X, vector.Y, vector.Z);
+        }
+
+        [CLSCompliant(false)]
+        public static void Uniform4(int location, ref Vector4 vector)
+        {
+            GL.Uniform4(location, vector.X, vector.Y, vector.Z, vector.W);
+        }
+
+        public static void Uniform2(int location, Vector2 vector)
+        {
+            GL.Uniform2(location, vector.X, vector.Y);
+        }
+
+        public static void Uniform3(int location, Vector3 vector)
+        {
+            GL.Uniform3(location, vector.X, vector.Y, vector.Z);
+        }
+
+        public static void Uniform4(int location, Vector4 vector)
+        {
+            GL.Uniform4(location, vector.X, vector.Y, vector.Z, vector.W);
+        }
+
+        public static void Uniform4(int location, Color4 color)
+        {
+            GL.Uniform4(location, color.R, color.G, color.B, color.A);
+        }
+
+        public static void Uniform4(int location, Quaternion quaternion)
+        {
+            GL.Uniform4(location, quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
+        }
+
+        public static void UniformMatrix2(int location, bool transpose, ref Matrix2 matrix)
+        {
+            unsafe
+            {
+                fixed (float* matrix_ptr = &matrix.Row0.X)
+                {
+                    GL.UniformMatrix2(location, 1, transpose, matrix_ptr);
+                }
+            }
+        }
+
+        public static void UniformMatrix3(int location, bool transpose, ref Matrix3 matrix)
+        {
+            unsafe
+            {
+                fixed (float* matrix_ptr = &matrix.Row0.X)
+                {
+                    GL.UniformMatrix3(location, 1, transpose, matrix_ptr);
+                }
+            }
+        }
+
+        public static void UniformMatrix4(int location, bool transpose, ref Matrix4 matrix)
+        {
+            unsafe
+            {
+                fixed (float* matrix_ptr = &matrix.Row0.X)
+                {
+                    GL.UniformMatrix4(location, 1, transpose, matrix_ptr);
+                }
+            }
+        }
+
+
+        public static string GetActiveAttrib(int program, int index, out int size, out ActiveAttribType type)
+        {
+            int length;
+            GetProgram(program, ES20.GetProgramParameterName.ActiveAttributeMaxLength, out length);
+            string str;
+
+            GetActiveAttrib(program, index, length == 0 ? 1 : length * 2, out length, out size, out type, out str);
+            return str;
+        }
+
+        public static string GetActiveUniform(int program, int uniformIndex, out int size, out ActiveUniformType type)
+        {
+            int length;
+            GetProgram(program, ES20.GetProgramParameterName.ActiveUniformMaxLength, out length);
+
+            string str;
+            GetActiveUniform(program, uniformIndex, length == 0 ? 1 : length, out length, out size, out type, out str);
+            return str;
+        }
+
+        public static void ShaderSource(Int32 shader, System.String @string)
+        {
+            unsafe
+            {
+                int length = @string.Length;
+                GL.ShaderSource((UInt32)shader, 1, new string[] { @string }, &length);
+            }
+        }
+
+        public static string GetShaderInfoLog(Int32 shader)
+        {
+            string info;
+            GetShaderInfoLog(shader, out info);
+            return info;
+        }
+
+        public static void GetShaderInfoLog(Int32 shader, out string info)
+        {
+            unsafe
+            {
+                int length;
+                GL.GetShader(shader, ShaderParameter.InfoLogLength, out length);
+                if (length == 0)
+                {
+                    info = String.Empty;
+                    return;
+                }
+                GL.GetShaderInfoLog((UInt32)shader, length * 2, &length, out info);
+            }
+        }
+
+        public static string GetProgramInfoLog(Int32 program)
+        {
+            string info;
+            GetProgramInfoLog(program, out info);
+            return info;
+        }
+
+        public static void GetProgramInfoLog(Int32 program, out string info)
+        {
+            unsafe
+            {
+                int length;
+                GL.GetProgram(program, ES20.GetProgramParameterName.InfoLogLength, out length); if (length == 0)
+                {
+                    info = String.Empty;
+                    return;
+                }
+                GL.GetProgramInfoLog((UInt32)program, length * 2, &length, out info);
+            }
+        }
+
+        [CLSCompliant(false)]
+        public static void VertexAttrib2(Int32 index, ref Vector2 v)
+        {
+            GL.VertexAttrib2(index, v.X, v.Y);
+        }
+
+        [CLSCompliant(false)]
+        public static void VertexAttrib3(Int32 index, ref Vector3 v)
+        {
+            GL.VertexAttrib3(index, v.X, v.Y, v.Z);
+        }
+
+        [CLSCompliant(false)]
+        public static void VertexAttrib4(Int32 index, ref Vector4 v)
+        {
+            GL.VertexAttrib4(index, v.X, v.Y, v.Z, v.W);
+        }
+
+        public static void VertexAttrib2(Int32 index, Vector2 v)
+        {
+            GL.VertexAttrib2(index, v.X, v.Y);
+        }
+
+        public static void VertexAttrib3(Int32 index, Vector3 v)
+        {
+            GL.VertexAttrib3(index, v.X, v.Y, v.Z);
+        }
+
+        public static void VertexAttrib4(Int32 index, Vector4 v)
+        {
+            GL.VertexAttrib4(index, v.X, v.Y, v.Z, v.W);
+        }
+
+        public static void VertexAttribPointer(int index, int size, VertexAttribPointerType type, bool normalized, int stride, int offset)
+        {
+            VertexAttribPointer(index, size, type, normalized, stride, (IntPtr)offset);
+        }
+
+
+        public static void VertexAttribPointer(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, int offset)
+        {
+            VertexAttribPointer(index, size, type, normalized, stride, (IntPtr)offset);
+        }
+
+
+        public static void GetFloat(GetPName pname, out Vector2 vector)
+        {
+            unsafe
+            {
+                fixed (Vector2* ptr = &vector)
+                {
+                    GetFloat(pname, (float*)ptr);
+                }
+            }
+        }
+
+        public static void GetFloat(GetPName pname, out Vector3 vector)
+        {
+            unsafe
+            {
+                fixed (Vector3* ptr = &vector)
+                {
+                    GetFloat(pname, (float*)ptr);
+                }
+            }
+        }
+
+        public static void GetFloat(GetPName pname, out Vector4 vector)
+        {
+            unsafe
+            {
+                fixed (Vector4* ptr = &vector)
+                {
+                    GetFloat(pname, (float*)ptr);
+                }
+            }
+        }
+
+        public static void GetFloat(GetPName pname, out Matrix4 matrix)
+        {
+            unsafe
+            {
+                fixed (Matrix4* ptr = &matrix)
+                {
+                    GetFloat(pname, (float*)ptr);
+                }
+            }
+        }
+
+        public static void Viewport(Size size)
+        {
+            GL.Viewport(0, 0, size.Width, size.Height);
+        }
+
+        public static void Viewport(Point location, Size size)
+        {
+            GL.Viewport(location.X, location.Y, size.Width, size.Height);
+        }
+
+        public static void Viewport(Rectangle rectangle)
+        {
+            GL.Viewport(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        }
+
+
+
+
+        //#if MINIMAL
+        //        public static void Viewport(OpenTK.Point location, OpenTK.Size size)
+        //        {
+        //            GL.Viewport(location.X, location.Y, size.Width, size.Height);
+        //        }
+
+        //        public static void Viewport(OpenTK.Rectangle rectangle)
+        //        {
+        //            GL.Viewport(rectangle.X, rectangle.Y, rectangle.Width, rectangle.Height);
+        //        }
+        //#endif
+
+#pragma warning restore 3019
+#pragma warning restore 1591
+#pragma warning restore 1572
+#pragma warning restore 1573
+    }
+
+#pragma warning disable 1574 // XML comment cref attribute could not be resolved, compiler bug in Mono 3.4.0
+
+    /// <summary>
+    /// Defines the signature of a debug callback for
+    /// <see cref="GL.DebugMessageCallback"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="DebugSource"/> for this debug message.</param>
+    /// <param name="type">The <see cref="DebugType"/> for this debug message.</param>
+    /// <param name="id">The id of this debug message.</param>
+    /// <param name="severity">The <see cref="DebugSeverity"/> for this debug message.</param>
+    /// <param name="length">The length of this debug message.</param>
+    /// <param name="message">A pointer to a null-terminated ASCII C string, representing the content of this debug message.</param>
+    /// <param name="userParam">A pointer to a user-specified parameter.</param>
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate void DebugProc(
+        DebugSource source, DebugType type, int id,
+        DebugSeverity severity, int length, IntPtr message,
+        IntPtr userParam);
+
+    /// <summary>
+    /// Defines the signature of a debug callback for
+    /// <see cref="GL.Khr.DebugMessageCallback"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="DebugSource"/> for this debug message.</param>
+    /// <param name="type">The <see cref="DebugType"/> for this debug message.</param>
+    /// <param name="id">The id of this debug message.</param>
+    /// <param name="severity">The <see cref="DebugSeverity"/> for this debug message.</param>
+    /// <param name="length">The length of this debug message.</param>
+    /// <param name="message">A pointer to a null-terminated ASCII C string, representing the content of this debug message.</param>
+    /// <param name="userParam">A pointer to a user-specified parameter.</param>
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate void DebugProcKhr(
+        DebugSource source, DebugType type, int id,
+        DebugSeverity severity, int length, IntPtr message,
+        IntPtr userParam);
+
+#pragma warning restore 1574 // XML comment cref attribute could not be resolved, compiler bug in Mono 3.4.0
+
+
+
+    partial class GL
+    {
+        public static void DrawArrays(BeginMode beginMode, int first, int count)
+        {
+            GL.DrawArrays((PrimitiveType)beginMode, first, count);
+        }
+        public static void DrawElements(BeginMode beginMode, int nelements, DrawElementsType type, int offset)
+        {
+            GL.DrawElements((PrimitiveType)beginMode, nelements, type, (IntPtr)offset);
+        }
+        public static int GenTexture()
+        {
+            GL.GenTextures(1, out int textureId);
+            return textureId;
+        }
+        public static void DeleteTexture(int textureId)
+        {
+            unsafe
+            {
+                int* arr = stackalloc int[1];
+                arr[0] = textureId;
+                GL.DeleteTextures(1, arr);
+            }
+        }
+
+        //-------------
+        public static void VertexAttribPointer(int location,
+            int size, VertexAttribPointerType
+            type, bool normalize,
+            int byteCount, byte[] vertices)
+        {
+            unsafe
+            {
+                fixed (byte* v_ptr = vertices)
+                {
+                    GL.VertexAttribPointer(location,
+                         size,
+                         type,
+                         normalize,
+                         byteCount, //total size
+                        (IntPtr)v_ptr);
+                }
+            }
+        }
+        public static void VertexAttribPointer<T>(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, T[] arr)
+            where T : struct
+        {
+            System.Runtime.InteropServices.GCHandle pAddress_ptr = System.Runtime.InteropServices.GCHandle.Alloc(arr, System.Runtime.InteropServices.GCHandleType.Pinned);
+            try
+            {
+                GL.VertexAttribPointer(index, size, type, normalized, stride, (IntPtr)pAddress_ptr.AddrOfPinnedObject());                 
+            }
+            finally
+            {
+                pAddress_ptr.Free();
+            }
+        }
+        public static void VertexAttribPointer<T>(int index, int size, VertexAttribPointerType type, bool normalized, int stride, T[] arr)
+            where T : struct
+        {
+            System.Runtime.InteropServices.GCHandle pAddress_ptr = System.Runtime.InteropServices.GCHandle.Alloc(arr, System.Runtime.InteropServices.GCHandleType.Pinned);
+            try
+            {
+                GL.VertexAttribPointer(index, size, type, normalized, stride, (IntPtr)pAddress_ptr.AddrOfPinnedObject());
+            }
+            finally
+            {
+                pAddress_ptr.Free();
+            }
+        }
+        public static void VertexAttribPointer(Int32 index, Int32 size,
+            OpenTK.Graphics.ES20.VertexAttribPointerType type,
+            bool normalized,
+            Int32 stride,
+            float[] arr)
+        {
+            unsafe
+            {
+                fixed (float* v_ptr = arr)
+                {
+                    GL.VertexAttribPointer(index,
+                         size,
+                         type,
+                         normalized,
+                         stride, //total size
+                         (IntPtr)v_ptr);
+                }
+            }
+        }
+        public static void DrawElements(OpenTK.Graphics.ES20.PrimitiveType mode, Int32 count, OpenTK.Graphics.ES20.DrawElementsType type, byte[] indices)
+        {
+            unsafe
+            {
+                fixed (byte* v_ptr = indices)
+                {
+                    GL.DrawElements(mode, count, type, (IntPtr)v_ptr);
+                }
+            }
+        }
+        public static void DrawElements(BeginMode mode, int size, DrawElementsType elemType, ushort[] indices)
+        {
+            unsafe
+            {
+                fixed (ushort* v_ptr = indices)
+                {
+                    GL.DrawElements((PrimitiveType)mode, size, elemType, (IntPtr)v_ptr);
+                }
+            }
+        }
+        public static void TexImage2D(TextureTarget textureTarget, int level, PixelInternalFormat format,
+            int width, int height, int b, PixelFormat pixelFormat, PixelType pxtype, byte[] buffer)
+        {
+            unsafe
+            {
+                fixed (byte* buffer_ptr = buffer)
+                {
+                    GL.TexImage2D(
+                       (TextureTarget2d)textureTarget,
+                       level,
+                       (TextureComponentCount)format,
+                       width, height, b, pixelFormat, pxtype,
+                       (System.IntPtr)buffer_ptr);
+                }
+            }
+        }
+
+
+    }
+}
+
+
+
