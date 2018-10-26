7bf439d597f22954a0c5a74a09400d5782a2601f
win <wintercoredev@gmail.com>
2018-10-26 19:41:31 +0700

    1131.1: tiger_whiskers

--- src/Tests/TestSamples/Sample03/T108_LionFill2.cs
+++ src/Tests/TestSamples/Sample03/T108_LionFill2.cs
@@ -23,8 +23,11 @@ namespace OpenTkEssTest
         protected override void OnReadyForInitGLShaderProgram()
         {
 
-            VgRenderVx svgRenderVx = SvgRenderVxLoader.CreateSvgRenderVxFromFile("Samples/lion.svg"); 
-            lionShape = new SpriteShape(svgRenderVx); 
+            //string sampleFile = "Samples/lion.svg";
+            string sampleFile = "Samples/tiger_whiskers.svg";
+
+            VgRenderVx svgRenderVx = SvgRenderVxLoader.CreateSvgRenderVxFromFile(sampleFile);
+            lionShape = new SpriteShape(svgRenderVx);
             //flip this lion vertically before use with openGL
             PixelFarm.CpuBlit.VertexProcessing.Affine aff = PixelFarm.CpuBlit.VertexProcessing.Affine.NewMatix(
                  PixelFarm.CpuBlit.VertexProcessing.AffinePlan.Scale(1, -1),
