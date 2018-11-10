a3a9575e83131eea3850abf0f9380f67010295ae
win <wintercoredev@gmail.com>
2018-11-10 17:23:15 +0700

    187.2: remove code

--- src/PixelFarm/PaintLab.Platforms.WinForms/2_GLES2/3_MyTopWindowBridgeOpenGL.cs
+++ src/PixelFarm/PaintLab.Platforms.WinForms/2_GLES2/3_MyTopWindowBridgeOpenGL.cs
@@ -81,20 +81,14 @@ namespace LayoutFarm.UI.OpenGL
                 //3.
             }
         }
-
-        //bool _disablePaintToOutputWindow;
-        //public bool DisablePaintToOutputWindow
-        //{
-        //    get => _disablePaintToOutputWindow;
-        //    set => _disablePaintToOutputWindow = value;
-        //}
+         
 
 #if DEBUG
         System.Diagnostics.Stopwatch dbugStopWatch = new System.Diagnostics.Stopwatch();
 #endif
         public override void PaintToOutputWindow()
         {
-            if (!isInitGLControl)// || _disablePaintToOutputWindow)
+            if (!isInitGLControl) 
             {
                 return;
             }
