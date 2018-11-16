26571a63f7551a3485d7b7c23485bf5a52663fbc
prepare <wintercoredev@gmail.com>
2018-11-16 23:13:42 +0700

    269.3

--- src/PixelFarm/PaintLab.Platforms.WinForms/4_GdiPlus/1_GdiPlusCanvasViewport.cs
+++ src/PixelFarm/PaintLab.Platforms.WinForms/4_GdiPlus/1_GdiPlusCanvasViewport.cs
@@ -23,7 +23,7 @@ namespace LayoutFarm.UI.GdiPlus
                 _drawBoard.Dispose();
                 _drawBoard = null;
             }
-        }       
+        }
 
 #if DEBUG
         //int dbugCount;
@@ -146,63 +146,64 @@ namespace LayoutFarm.UI.GdiPlus
         }
 
 
-        public void PaintMe(GdiPlusDrawBoard mycanvas)
-        {
-            if (this.IsClosed) { return; }
-            //------------------------------------ 
+        //PRESERVE***
+        //        public void PaintMe(GdiPlusDrawBoard mycanvas)
+        //        {
+        //            if (this.IsClosed) { return; }
+        //            //------------------------------------ 
 
-            this._rootGraphics.PrepareRender();
-            //---------------
-            this._rootGraphics.IsInRenderPhase = true;
-#if DEBUG
-            this._rootGraphics.dbug_rootDrawingMsg.Clear();
-            this._rootGraphics.dbug_drawLevel = 0;
-#endif
+        //            this._rootGraphics.PrepareRender();
+        //            //---------------
+        //            this._rootGraphics.IsInRenderPhase = true;
+        //#if DEBUG
+        //            this._rootGraphics.dbug_rootDrawingMsg.Clear();
+        //            this._rootGraphics.dbug_drawLevel = 0;
+        //#endif
 
-            mycanvas.OffsetCanvasOrigin(-mycanvas.Left, -mycanvas.Top);
-            Rectangle rect = mycanvas.Rect;
-            this._rootGraphics.TopWindowRenderBox.DrawToThisCanvas(mycanvas, rect);
-#if DEBUG
-            this._rootGraphics.TopWindowRenderBox.dbugShowRenderPart(mycanvas, rect);
-#endif
+        //            mycanvas.OffsetCanvasOrigin(-mycanvas.Left, -mycanvas.Top);
+        //            Rectangle rect = mycanvas.Rect;
+        //            this._rootGraphics.TopWindowRenderBox.DrawToThisCanvas(mycanvas, rect);
+        //#if DEBUG
+        //            this._rootGraphics.TopWindowRenderBox.dbugShowRenderPart(mycanvas, rect);
+        //#endif
 
-            mycanvas.IsContentReady = true;
-            mycanvas.OffsetCanvasOrigin(mycanvas.Left, mycanvas.Top);
-            //if (this.FullMode)
-            //{
-            //    quadPages.RenderToOutputWindowFullMode(rootGraphics.TopWindowRenderBox, hdc,
-            //        this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
-            //}
-            //else
-            //{
-            //    //temp to full mode
-            //    //quadPages.RenderToOutputWindowFullMode(rootGraphics.TopWindowRenderBox, hdc, this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
-            //    quadPages.RenderToOutputWindowPartialMode(rootGraphics.TopWindowRenderBox, hdc,
-            //        this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
+        //            mycanvas.IsContentReady = true;
+        //            mycanvas.OffsetCanvasOrigin(mycanvas.Left, mycanvas.Top);
+        //            //if (this.FullMode)
+        //            //{
+        //            //    quadPages.RenderToOutputWindowFullMode(rootGraphics.TopWindowRenderBox, hdc,
+        //            //        this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
+        //            //}
+        //            //else
+        //            //{
+        //            //    //temp to full mode
+        //            //    //quadPages.RenderToOutputWindowFullMode(rootGraphics.TopWindowRenderBox, hdc, this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
+        //            //    quadPages.RenderToOutputWindowPartialMode(rootGraphics.TopWindowRenderBox, hdc,
+        //            //        this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
 
-            //}
-            this._rootGraphics.IsInRenderPhase = false;
-#if DEBUG
+        //            //}
+        //            this._rootGraphics.IsInRenderPhase = false;
+        //#if DEBUG
 
-            RootGraphic visualroot = RootGraphic.dbugCurrentGlobalVRoot;
-            if (visualroot.dbug_RecordDrawingChain)
-            {
-                List<dbugLayoutMsg> outputMsgs = dbugOutputWindow.dbug_rootDocDebugMsgs;
-                outputMsgs.Clear();
-                outputMsgs.Add(new dbugLayoutMsg(null as RenderElement, "[" + debug_render_to_output_count + "]"));
-                visualroot.dbug_DumpRootDrawingMsg(outputMsgs);
-                dbugOutputWindow.dbug_InvokeVisualRootDrawMsg();
-                debug_render_to_output_count++;
-            }
+        //            RootGraphic visualroot = RootGraphic.dbugCurrentGlobalVRoot;
+        //            if (visualroot.dbug_RecordDrawingChain)
+        //            {
+        //                List<dbugLayoutMsg> outputMsgs = dbugOutputWindow.dbug_rootDocDebugMsgs;
+        //                outputMsgs.Clear();
+        //                outputMsgs.Add(new dbugLayoutMsg(null as RenderElement, "[" + debug_render_to_output_count + "]"));
+        //                visualroot.dbug_DumpRootDrawingMsg(outputMsgs);
+        //                dbugOutputWindow.dbug_InvokeVisualRootDrawMsg();
+        //                debug_render_to_output_count++;
+        //            }
 
 
-            if (dbugHelper01.dbugVE_HighlightMe != null)
-            {
-                dbugOutputWindow.dbug_HighlightMeNow(dbugHelper01.dbugVE_HighlightMe.dbugGetGlobalRect());
-            }
-#endif
-        }
-
+        //            if (dbugHelper01.dbugVE_HighlightMe != null)
+        //            {
+        //                dbugOutputWindow.dbug_HighlightMeNow(dbugHelper01.dbugVE_HighlightMe.dbugGetGlobalRect());
+        //            }
+        //#endif
+        //        }
+        //PRESERVE***
 
         public void RenderToOutputWindowFullMode(
             IRenderElement topWindowRenderBox,
