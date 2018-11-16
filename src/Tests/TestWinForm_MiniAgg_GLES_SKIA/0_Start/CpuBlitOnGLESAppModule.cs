81a277587f254c8b33db278a433685fad1207d94
prepare <wintercoredev@gmail.com>
2018-11-16 15:22:15 +0700

    260: update and remove GdiPlusOnGLESAppModule

--- src/Tests/TestWinForm_MiniAgg_GLES_SKIA/0_Start/CpuBlitOnGLESAppModule.cs
+++ src/Tests/TestWinForm_MiniAgg_GLES_SKIA/0_Start/CpuBlitOnGLESAppModule.cs
@@ -25,12 +25,8 @@ namespace Mini
         CpuBlitGLESUIElement _demoUI;
         DemoBase _demoBase;
 
-
         OpenTK.MyGLControl _glControl;
-
         public CpuBlitOnGLESAppModule() { }
-
-
         public void BindSurface(LayoutFarm.UI.UISurfaceViewportControl surfaceViewport)
         {
             _myWidth = 800;
@@ -46,6 +42,9 @@ namespace Mini
             IntPtr hh1 = _glControl.Handle; //ensure that contrl handler is created
             _glControl.MakeCurrent();
         }
+
+        public bool WithGdiPlusDrawBoard { get; set; }
+
         public void LoadExample(DemoBase demoBase)
         {
             _glControl.MakeCurrent();
@@ -53,14 +52,23 @@ namespace Mini
             this._demoBase = demoBase;
             demoBase.Init();
 
-            _demoUI = new CpuBlitGLESUIElement(demoBase, _myWidth, _myHeight);
+            if (WithGdiPlusDrawBoard)
+            {
+                _demoUI = new GdiPlusCpuBlitGLESUIElement(_myWidth, _myHeight);
+            }
+            else
+            {
+                //pure agg's cpu blit 
+                _demoUI = new CpuBlitGLESUIElement(_myWidth, _myHeight);
+            }
+            //
+            _demoUI.SetDemo(demoBase);
             //use existing GLRenderSurface and GLPainter
             //see=>UISurfaceViewportControl.InitRootGraphics()
 
             GLRenderSurface glsx = _surfaceViewport.GetGLRenderSurface();
             GLPainter glPainter = _surfaceViewport.GetGLPainter();
             _demoUI.CreatePrimaryRenderElement(glsx, glPainter, _rootGfx);
-
             //-----------------------------------------------
             demoBase.SetEssentialGLHandlers(
                 () => this._glControl.SwapBuffers(),
@@ -68,12 +76,11 @@ namespace Mini
                 () => this._glControl.GetEglSurface()
             );
             //-----------------------------------------------
-
             DemoBase.InvokeGLContextReady(demoBase, glsx, glPainter);
             //Add to RenderTree
             _rootGfx.TopWindowRenderBox.AddChild(_demoUI.GetPrimaryRenderElement(_rootGfx));
-
             //-----------------------------------------------
+            //***
             GeneralEventListener genEvListener = new GeneralEventListener();
             genEvListener.MouseDown += e =>
             {
@@ -103,176 +110,219 @@ namespace Mini
         {
             _demoBase.CloseDemo();
         }
-
-
-        /// <summary>
-        /// CpuBlit to GLES UIElement
-        /// </summary>
-        class CpuBlitGLESUIElement : UIElement
-        {
-            DemoBase _demoBase;
-            int _width;
-            int _height;
-            CpuBlitGLCanvasRenderElement _canvasRenderE;
-
-            public CpuBlitGLESUIElement(DemoBase demoBase, int width, int height)
-            {
-                _width = width;
-                _height = height;
-                _demoBase = demoBase;
-                SetupAggCanvas();
-                DemoBase.InvokePainterReady(_demoBase, _aggPainter);
-                ContentMayChanged = true;
-            }
-
-            public void CreatePrimaryRenderElement(GLRenderSurface glsx, GLPainter painter, RootGraphic rootgfx)
-            {
-                if (_canvasRenderE == null)
-                {
-                    var glRenderElem = new CpuBlitGLCanvasRenderElement(rootgfx, _width, _height, _lazyImgProvider);
-                    glRenderElem.SetPainter(glsx, painter);
-                    glRenderElem.SetController(this); //connect to event system
-                    glRenderElem.SetOwnerDemoUI(this);
-                    _canvasRenderE = glRenderElem;
-                }
-            }
-            public override RenderElement CurrentPrimaryRenderElement
-            {
-                get
-                {
-                    return _canvasRenderE;
-                }
-            }
-
-            protected override bool HasReadyRenderElement => _canvasRenderE != null;
-
-
-            public override RenderElement GetPrimaryRenderElement(RootGraphic rootgfx)
-            {
-                //for this Elem  => please call CreatePrimaryRenderElement first  
-                return _canvasRenderE;
-            }
-
-            public override void InvalidateGraphics()
-            {
-                if (_canvasRenderE != null)
-                {
-                    _canvasRenderE.InvalidateGraphics();
-                }
-            }
-            public override void Walk(UIVisitor visitor)
-            {
-
-            }
-            //handle event
-            public bool ContentMayChanged { get; set; }
-
-
-            //---------------------
-
-            ActualBitmap _aggBmp;
-            AggPainter _aggPainter;
-            LazyActualBitmapBufferProvider _lazyImgProvider;
-            void SetupAggCanvas()
-            {
-                _aggBmp = new ActualBitmap(800, 600);
-                _aggPainter = AggPainter.Create(_aggBmp);
-
-                //optional if we want to print text on agg surface
-                _aggPainter.CurrentFont = new PixelFarm.Drawing.RequestFont("Tahoma", 10);
-                var aggTextPrinter = new PixelFarm.Drawing.Fonts.FontAtlasTextPrinter(_aggPainter);
-                _aggPainter.TextPrinter = aggTextPrinter;
-                _lazyImgProvider = new LazyActualBitmapBufferProvider(_aggBmp);
-            }
-            internal void UpdateCpuBlitSurface()
-            {
-
-                _aggPainter.Clear(PixelFarm.Drawing.Color.White);
-                //-----------
-                //TODO:
-                //if the content of _aggBmp is not changed
-                //we should not draw again  
-                _demoBase.Draw(_aggPainter);
-                //
-                //test print some text
-                _aggPainter.FillColor = PixelFarm.Drawing.Color.Black; //set font 'fill' color
-                _aggPainter.DrawString("Hello! 12345", 0, 500);
-            }
-
-        }
-
-        class CpuBlitGLCanvasRenderElement : RenderElement, IDisposable
-        {
-
-            CpuBlitGLESUIElement _demoUI;
-            GLRenderSurface _glsx;
-            GLPainter _glPainter;
-            GLBitmap _glBmp;
-
-            LazyActualBitmapBufferProvider _lzBmpProvider;
-            public CpuBlitGLCanvasRenderElement(RootGraphic rootgfx, int w, int h, LazyActualBitmapBufferProvider lzBmpProvider)
-                : base(rootgfx, w, h)
-            {
-                _lzBmpProvider = lzBmpProvider;// 
-            }
-            public void SetOwnerDemoUI(CpuBlitGLESUIElement demoUI)
-            {
-                _demoUI = demoUI;
-            }
-            public void SetPainter(GLRenderSurface glsx, GLPainter canvasPainter)
-            {
-                _glsx = glsx;
-                _glPainter = canvasPainter;
-            }
-
-            public override void CustomDrawToThisCanvas(DrawBoard canvas, Rectangle updateArea)
-            {
-
-                //TODO: 
-                //1. if the content of glBmp is not changed
-                //we should not render again 
-                //2. if we only update some part of texture
-                //may can transfer only that part to the glBmp
-                //-------------------------------------------------------------------------  
-                if (_demoUI.ContentMayChanged)
-                {
-                    _demoUI.UpdateCpuBlitSurface();
-                    _demoUI.ContentMayChanged = false;
-
-                    //load new glBmp 
-                    if (_glBmp != null)
-                    {
-                        _glBmp.Dispose();
-                        _glBmp = null;
-                    }
-                }
-
-                //------------------------------------------------------------------------- 
-                //copy from 
-                if (_glBmp == null)
-                {
-                    _glBmp = new GLBitmap(_lzBmpProvider);
-                    _glBmp.IsYFlipped = false;
-                }
-
-
-                //------------------------------------------------------------------------- 
-                _glsx.DrawImage(_glBmp, 0, 0);
-                //test print text from our GLTextPrinter 
-                _glPainter.FillColor = PixelFarm.Drawing.Color.Black;
-                _glPainter.DrawString("Hello2", 0, 400);
-                //------------------------------------------------------------------------- 
-            }
-            public override void ResetRootGraphics(RootGraphic rootgfx)
-            {
-
-            }
-            public void Dispose()
-            {
-
-            }
-        }
-
     }
 
+
+    /// <summary>
+    /// CpuBlit to GLES UIElement
+    /// </summary>
+    class CpuBlitGLESUIElement : UIElement
+    {
+        DemoBase _demoBase;
+        protected int _width;
+        protected int _height;
+        CpuBlitGLCanvasRenderElement _canvasRenderE;
+
+        public CpuBlitGLESUIElement(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            //
+            SetupAggCanvas();//***
+            ContentMayChanged = true;
+        }
+        public void SetDemo(DemoBase demoBase)
+        {
+            _demoBase = demoBase;
+            DemoBase.InvokePainterReady(_demoBase, _aggPainter);
+        }
+        protected virtual bool HasSomeExtension => false;//class that override 
+
+        public void CreatePrimaryRenderElement(GLRenderSurface glsx, GLPainter painter, RootGraphic rootgfx)
+        {
+            if (_canvasRenderE == null)
+            {
+                var glRenderElem = new CpuBlitGLCanvasRenderElement(rootgfx, _width, _height, _lazyImgProvider);
+                glRenderElem.SetPainter(glsx, painter);
+                glRenderElem.SetController(this); //connect to event system
+                glRenderElem.SetOwnerDemoUI(this);
+                _canvasRenderE = glRenderElem;
+            }
+        }
+        public override RenderElement CurrentPrimaryRenderElement
+        {
+            get
+            {
+                return _canvasRenderE;
+            }
+        }
+
+        protected override bool HasReadyRenderElement => _canvasRenderE != null;
+
+        public override RenderElement GetPrimaryRenderElement(RootGraphic rootgfx)
+        {
+            //for this Elem  => please call CreatePrimaryRenderElement first  
+            return _canvasRenderE;
+        }
+
+        public override void InvalidateGraphics()
+        {
+            if (_canvasRenderE != null)
+            {
+                _canvasRenderE.InvalidateGraphics();
+            }
+        }
+        public override void Walk(UIVisitor visitor)
+        {
+
+        }
+        //handle event
+        public bool ContentMayChanged { get; set; }
+
+
+        //----------------------------------------------
+        protected ActualBitmap _aggBmp;
+        protected AggPainter _aggPainter;
+        protected LazyActualBitmapBufferProvider _lazyImgProvider;
+        //
+        protected virtual void SetupAggCanvas()
+        {
+            //***
+            _aggBmp = new ActualBitmap(_width, _height);
+            _aggPainter = AggPainter.Create(_aggBmp);
+
+            //optional if we want to print text on agg surface
+            _aggPainter.CurrentFont = new PixelFarm.Drawing.RequestFont("Tahoma", 10);
+            _aggPainter.TextPrinter = new PixelFarm.Drawing.Fonts.FontAtlasTextPrinter(_aggPainter);
+            _lazyImgProvider = new LazyActualBitmapBufferProvider(_aggBmp);
+            //
+        }
+        protected virtual void ClearSurface()
+        {
+            _aggPainter.Clear(PixelFarm.Drawing.Color.White);
+        }
+        internal void UpdateCpuBlitSurface()
+        {
+            ClearSurface();
+            //TODO:
+            //if the content of _aggBmp is not changed
+            //we should not draw again  
+            _demoBase.Draw(_aggPainter);
+            //
+            //test print some text
+            _aggPainter.FillColor = PixelFarm.Drawing.Color.Black; //set font 'fill' color
+            _aggPainter.DrawString("Hello! 12345", 0, 500);
+        }
+    }
+
+    /// <summary>
+    /// CpuBlit and Gdi+  Bridge UIElement
+    /// </summary>
+    class GdiPlusCpuBlitGLESUIElement : CpuBlitGLESUIElement
+    {
+
+        PixelFarm.Drawing.WinGdi.GdiPlusDrawBoard _gdiDrawBoard;
+
+        public GdiPlusCpuBlitGLESUIElement(int width, int height)
+            : base(width, height)
+        {
+        }
+        protected override void ClearSurface()
+        {
+            _gdiDrawBoard.RenderSurface.Win32DC.PatBlt(Win32.NativeWin32MemoryDC.PatBltColor.White);
+        }
+        protected override bool HasSomeExtension => true;
+        protected override void SetupAggCanvas()
+        {
+            //don't call base
+            //1. we create gdi plus draw board
+            _gdiDrawBoard = new PixelFarm.Drawing.WinGdi.GdiPlusDrawBoard(0, 0, _width, _height);
+            _gdiDrawBoard.CurrentFont = new RequestFont("Tahoma", 10);
+
+            //2. create actual bitmap that share 'bitmap mem' with gdiPlus Render surface                 
+            _aggBmp = new ActualBitmap(_width, _height, _gdiDrawBoard.RenderSurface.Win32DC.PPVBits);
+            //3. create painter from the agg bmp (then we will copy the 'client' gdi mem surface to the GL)
+            _aggPainter = AggPainter.Create(_aggBmp);
+
+            //...
+            //optional if we want to print text on agg surface
+            _aggPainter.CurrentFont = new PixelFarm.Drawing.RequestFont("Tahoma", 10);
+            var aggTextPrinter = new PixelFarm.Drawing.Fonts.FontAtlasTextPrinter(_aggPainter);
+            _aggPainter.TextPrinter = aggTextPrinter;
+            //
+            _lazyImgProvider = new LazyActualBitmapBufferProvider(_aggBmp);
+        }
+    }
+
+    class CpuBlitGLCanvasRenderElement : RenderElement, IDisposable
+    {
+
+        CpuBlitGLESUIElement _ui;
+        //
+        GLRenderSurface _glsx;
+        GLPainter _glPainter;
+        GLBitmap _glBmp;
+        //
+        LazyActualBitmapBufferProvider _lzBmpProvider;
+        public CpuBlitGLCanvasRenderElement(RootGraphic rootgfx, int w, int h, LazyActualBitmapBufferProvider lzBmpProvider)
+            : base(rootgfx, w, h)
+        {
+            _lzBmpProvider = lzBmpProvider;// 
+        }
+        public void SetOwnerDemoUI(CpuBlitGLESUIElement ui)
+        {
+            _ui = ui;
+        }
+        public void SetPainter(GLRenderSurface glsx, GLPainter canvasPainter)
+        {
+            _glsx = glsx;
+            _glPainter = canvasPainter;
+        }
+        public override void CustomDrawToThisCanvas(DrawBoard canvas, Rectangle updateArea)
+        {
+
+            //TODO: 
+            //1. if the content of glBmp is not changed
+            //we should not render again 
+            //2. if we only update some part of texture
+            //may can transfer only that part to the glBmp
+            //-------------------------------------------------------------------------  
+            if (_ui.ContentMayChanged)
+            {
+                _ui.UpdateCpuBlitSurface();
+                _ui.ContentMayChanged = false;
+
+                //load new glBmp 
+                if (_glBmp != null)
+                {
+                    _glBmp.Dispose();
+                    _glBmp = null;
+                }
+            }
+
+            //------------------------------------------------------------------------- 
+            //copy from 
+            if (_glBmp == null)
+            {
+                _glBmp = new GLBitmap(_lzBmpProvider);
+                _glBmp.IsYFlipped = false;
+            }
+
+
+            //------------------------------------------------------------------------- 
+            _glsx.DrawImage(_glBmp, 0, 0);
+            //test print text from our GLTextPrinter 
+            _glPainter.FillColor = PixelFarm.Drawing.Color.Black;
+            _glPainter.DrawString("Hello2", 0, 400);
+            //------------------------------------------------------------------------- 
+        }
+        public override void ResetRootGraphics(RootGraphic rootgfx)
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+    }
 }
