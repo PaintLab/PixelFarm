4c531831e54c4ce9207ca8d63fa7c1f151082eaa
win <wintercoredev@gmail.com>
2018-12-07 16:11:14 +0700

    1472: Vxs minor refactor, remove unused code

--- src/PixelFarm/PixelFarm.DrawingCore/3_DrawBoard/1_VertexStore.cs
+++ src/PixelFarm/PixelFarm.DrawingCore/3_DrawBoard/1_VertexStore.cs
@@ -18,19 +18,21 @@
 //          http://www.antigrain.com
 //----------------------------------------------------------------------------
 
-#define UNSAFE_VER 
+
 
 using PixelFarm.CpuBlit;
 namespace PixelFarm.Drawing
 {
     public sealed class VertexStore
     {
-        public readonly bool _isTrimed;
+
         int _vertices_count;
         int _allocated_vertices_count;
         double[] _coord_xy;
         byte[] _cmds;
+
 #if DEBUG
+        public readonly bool dbugIsTrim;
         static int dbugTotal = 0;
         public readonly int dbugId = dbugGetNewId();
         public int dbugNote;
@@ -49,10 +51,8 @@ namespace PixelFarm.Drawing
         /// <summary>
         /// num of vertex
         /// </summary>
-        public int Count
-        {
-            get { return _vertices_count; }
-        }
+        public int Count => _vertices_count;
+        //
         public VertexCmd GetLastCommand()
         {
             if (_vertices_count != 0)
@@ -134,7 +134,9 @@ namespace PixelFarm.Drawing
             }
 
         }
-        public void ReplaceVertex(int index, double x, double y)
+
+        //--------
+        internal void ReplaceVertex(int index, double x, double y)
         {
 #if DEBUG
             _dbugIsChanged = true;
@@ -246,31 +248,7 @@ namespace PixelFarm.Drawing
                 _allocated_vertices_count = newSize;
             }
         }
-        //internal use only!
-        public static void UnsafeDirectSetData(
-            VertexStore vstore,
-            int m_allocated_vertices,
-            int m_num_vertices,
-            double[] m_coord_xy,
-            byte[] m_CommandAndFlags)
-        {
-            vstore._vertices_count = m_num_vertices;
-            vstore._allocated_vertices_count = m_allocated_vertices;
-            vstore._coord_xy = m_coord_xy;
-            vstore._cmds = m_CommandAndFlags;
-        }
-        public static void UnsafeDirectGetData(
-            VertexStore vstore,
-            out int m_allocated_vertices,
-            out int m_num_vertices,
-            out double[] m_coord_xy,
-            out byte[] m_CommandAndFlags)
-        {
-            m_num_vertices = vstore._vertices_count;
-            m_allocated_vertices = vstore._allocated_vertices_count;
-            m_coord_xy = vstore._coord_xy;
-            m_CommandAndFlags = vstore._cmds;
-        }
+
         public void AppendVertexStore(VertexStore another)
         {
 
@@ -351,8 +329,9 @@ namespace PixelFarm.Drawing
 
             if (trim)
             {
-
-                _isTrimed = true;
+#if DEBUG
+                dbugIsTrim = true;
+#endif
                 int coord_len = _vertices_count; //+1 for no more cmd
                 int cmds_len = _vertices_count; //+1 for no more cmd
 
@@ -403,8 +382,12 @@ namespace PixelFarm.Drawing
 
             _allocated_vertices_count = src._allocated_vertices_count;
             _vertices_count = src._vertices_count;
-
-            _isTrimed = true;
+            //
+            //
+#if DEBUG
+            dbugIsTrim = true;
+#endif
+            //
             int coord_len = _vertices_count; //+1 for no more cmd
             int cmds_len = _vertices_count; //+1 for no more cmd
 
@@ -443,7 +426,6 @@ namespace PixelFarm.Drawing
         {
             return new VertexStore(src, false);
         }
-
         /// <summary>
         /// trim to new vertex store
         /// </summary>
