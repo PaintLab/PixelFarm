173cf1ea46a2f4c765116be9ba99c0f0e3642165
win <wintercoredev@gmail.com>
2018-12-20 10:01:56 +0700

    1512: add padding to imgbox

--- src/PixelFarm/LayoutFarm.YourBasicCustomWidgets/0_CustomRenderElements/CustomImageRenderBox.cs
+++ src/PixelFarm/LayoutFarm.YourBasicCustomWidgets/0_CustomRenderElements/CustomImageRenderBox.cs
@@ -7,11 +7,53 @@ namespace LayoutFarm.CustomWidgets
     {
 
         ImageBinder _imageBinder;
+        int _paddingLeft, _paddingTop, _paddingRight, _paddingBottom;
+        bool _hasSomePadding;
+        bool _evalPadding;//evaluate padding again
+
         public CustomImageRenderBox(RootGraphic rootgfx, int width, int height)
             : base(rootgfx, width, height)
         {
+            _evalPadding = true;
             this.BackColor = Color.LightGray;
         }
+        public int PaddingLeft
+        {
+            get => _paddingLeft;
+            set
+            {
+                _paddingLeft = value;
+                _evalPadding = true;
+            }
+        }
+        public int PaddingTop
+        {
+            get => _paddingTop;
+            set
+            {
+                _paddingTop = value;
+                _evalPadding = true;
+            }
+        }
+        public int PaddingBottom
+        {
+            get => _paddingBottom;
+            set
+            {
+                _paddingBottom = value;
+                _evalPadding = true;
+            }
+        }
+        public int PaddingRight
+        {
+            get => _paddingRight;
+            set
+            {
+                _paddingRight = value;
+                _evalPadding = true;
+            }
+        }
+
         public override void ClearAllChildren()
         {
         }
@@ -30,8 +72,29 @@ namespace LayoutFarm.CustomWidgets
             {
                 case BinderState.Loaded:
                     {
-                        canvas.DrawImage(_imageBinder,
-                            new RectangleF(0, 0, this.Width, this.Height));
+                        if (_evalPadding)
+                        {
+                            _hasSomePadding = _paddingLeft != 0 || _paddingRight != 0 || _paddingBottom != 0 || _paddingTop != 0;
+                            _evalPadding = false;
+                        }
+                        if (_hasSomePadding)
+                        {
+                            canvas.FillRectangle(this.BackColor, 0, 0, this.Width, this.Height);
+                            canvas.DrawImage(_imageBinder,
+                                new RectangleF(
+                                PaddingLeft, PaddingTop,
+                                this.Width - (PaddingLeft + PaddingRight),
+                                this.Height - (PaddingTop + PaddingBottom)));
+                        }
+                        else
+                        {
+
+                            canvas.DrawImage(_imageBinder,
+                               new RectangleF(
+                               0, 0,
+                               this.Width,
+                               this.Height));
+                        }
                     }
                     break;
                 case BinderState.Unload:
