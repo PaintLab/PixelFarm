8f1c7dd3345ba63c883885b9362f0a34d7e8024b
win <wintercoredev@gmail.com>
2018-12-21 18:01:36 +0700

    1525: Load and Save  CompiledShader)()

--- src/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLShader/ShaderBase.cs
+++ src/PixelFarm/PixelFarm.DrawingGL/DrawingGL/GLShader/ShaderBase.cs
@@ -1,15 +1,19 @@
 ﻿//MIT, 2016-present, WinterDev
 
+using System.IO;
 using OpenTK.Graphics.ES20;
 namespace PixelFarm.DrawingGL
 {
     abstract class ShaderBase
     {
         protected readonly ShaderSharedResource _shareRes;
-        protected readonly MiniShaderProgram _shaderProgram = new MiniShaderProgram();
+        protected readonly MiniShaderProgram _shaderProgram;
         public ShaderBase(ShaderSharedResource shareRes)
         {
             _shareRes = shareRes;
+            _shaderProgram = new MiniShaderProgram();
+
+            EnableProgramBinaryCache = true; //default ???
         }
         /// <summary>
         /// set as current shader
@@ -20,12 +24,133 @@ namespace PixelFarm.DrawingGL
             {
                 _shareRes._currentShader = this;
                 _shaderProgram.UseProgram();
-              
+
                 this.OnSwitchToThisShader();
             }
         }
         protected virtual void OnSwitchToThisShader()
         {
         }
+
+        public bool EnableProgramBinaryCache { get; set; }
+        //this is an optional feature
+        protected bool SaveCompiledShader()
+        {
+            //use default Typename guide
+            return SaveCompiledShader(this.GetType().GUID.ToString() + ".bin_shader");
+        }
+        protected bool LoadCompiledShader()
+        {
+            return LoadCompiledShader(this.GetType().GUID.ToString() + ".bin_shader");
+        }
+        protected bool SaveCompiledShader(string filename)
+        {
+            if (!EnableProgramBinaryCache) return false;
+            //--------------------------------------------
+
+            using (System.IO.Stream s = CachedBinaryShaderIO.InternalGetWriteStream(filename))
+            {
+                if (s != null)
+                {
+                    using (System.IO.BinaryWriter w = new System.IO.BinaryWriter(s))
+                    {
+                        return _shaderProgram.SaveCompiledShader(w);
+                    }
+                }
+            }
+            return false;
+
+        }
+        protected bool LoadCompiledShader(string filename)
+        {
+            if (!EnableProgramBinaryCache) return false;
+            //--------------------------------------------
+            using (System.IO.Stream s = CachedBinaryShaderIO.InternalGetReadStream(filename))
+            {
+                if (s != null)
+                {
+                    using (System.IO.BinaryReader r = new System.IO.BinaryReader(s))
+                    {
+                        return _shaderProgram.LoadCompiledShader(r);
+                    }
+                }
+            }
+            return false;
+        }
     }
+
+
+    public abstract class CachedBinaryShaderIO
+    {
+        static CachedBinaryShaderIO s_defaultLocalFile = new LocalFileCachedBinaryShaderIO("");
+
+        static CachedBinaryShaderIO s_currentImpl = s_defaultLocalFile;
+
+        public static void ClearCurrentImpl()
+        {
+            s_currentImpl = null;
+        }
+        public void SetAsCurrentImplementation()
+        {
+            s_currentImpl = this;
+        }
+
+        public abstract Stream GetReadStream(string shaderName);
+        public abstract Stream GetWriteStream(string shaderName);
+        //--------
+
+        internal static CachedBinaryShaderIO GetBinCacheIO() => s_currentImpl;
+
+        internal static Stream InternalGetReadStream(string shaderName) => s_currentImpl.GetReadStream(shaderName);
+
+        internal static Stream InternalGetWriteStream(string shaderName) => s_currentImpl.GetWriteStream(shaderName);
+
+    }
+
+
+    //example
+    public class LocalFileCachedBinaryShaderIO : CachedBinaryShaderIO
+    {
+        public LocalFileCachedBinaryShaderIO(string baseDir, bool enabledAbsolutePath = false)
+        {
+            BaseDir = baseDir;
+        }
+        public string BaseDir { get; }
+        public bool EnableAbsolutePath { get; }
+        //
+        public override Stream GetReadStream(string shaderName)
+        {
+            if (Path.IsPathRooted(shaderName))
+            {
+                if (!EnableAbsolutePath) return null;
+            }
+            else
+            {
+                shaderName = Path.Combine(BaseDir, shaderName);
+            }
+
+            if (File.Exists(shaderName))
+            {
+                //read from file,
+                //don't forget to delete this stream after use.  
+                return new FileStream(shaderName, FileMode.Open);
+            }
+            return null;
+        }
+        public override Stream GetWriteStream(string shaderName)
+        {
+            if (Path.IsPathRooted(shaderName))
+            {
+                if (!EnableAbsolutePath) return null;
+            }
+            else
+            {
+                shaderName = Path.Combine(BaseDir, shaderName);
+            }
+            //write to
+            return new FileStream(shaderName, FileMode.Create); //***
+        }
+    }
+
+
 }
