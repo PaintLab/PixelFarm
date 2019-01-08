2f32609a02d8f2957faf1299591ae2f9ad1597bd
win <wintercoredev@gmail.com>
2019-01-08 19:51:14 +0700

    1696

--- src/Tests/TestWinForm_MiniAgg_GLES_SKIA/3_Samples/05_AlphaMask2/LionMask3.cs
+++ src/Tests/TestWinForm_MiniAgg_GLES_SKIA/3_Samples/05_AlphaMask2/LionMask3.cs
@@ -26,7 +26,7 @@ namespace PixelFarm.CpuBlit.Sample_LionAlphaMask
         double _angle = 0;
         double _lionScale = 1.0;
         double _skewX = 0;
-        double _skewY = 0;
+        double err_skewY = 0;
         bool _isMaskSliderValueChanged = true;
 
         MemBitmap lionImg;
