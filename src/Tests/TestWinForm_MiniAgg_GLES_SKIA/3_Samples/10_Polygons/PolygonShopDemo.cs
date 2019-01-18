d1a261797806a1d292a0dd8ea17259ec57a6628c
win <wintercoredev@gmail.com>
2019-01-18 17:04:41 +0700

    1758.1

--- src/Tests/TestWinForm_MiniAgg_GLES_SKIA/3_Samples/10_Polygons/PolygonShopDemo.cs
+++ src/Tests/TestWinForm_MiniAgg_GLES_SKIA/3_Samples/10_Polygons/PolygonShopDemo.cs
@@ -14,81 +14,137 @@ namespace PixelFarm
     [Info(DemoCategory.Vector)]
     public class PolygonShopDemo : DemoBase
     {
-        VertexStore _tmpArrow;
-        VertexStore _stem;
+        VertexStore _arrowSolidHead;
+        VertexStore _arrowLineHead;
+        VertexStore _roundRectSolid;
+        VertexStore _roundRectOutline;
 
         public enum PolygonKind
         {
-            Arrow,
+            //
+            ArrowSolidHead,
+            ArrowLineHead,
+            //
+            RoundCornerTriangle,
+            RoundCornerRect, RoundCornerRect_Outline,
+            RoundCornerPolygon
         }
         public PolygonShopDemo()
         {
-            //create simple arrow head 
-            //using (VxsTemp.Borrow(out var v1, out var v2))
-            //using (VxsTemp.Borrow(out var v3))
-            //{
-            //    BuildLine(5, 20, 10, 10, v1);
-            //    BuildLine(10, 10, 15, 20, v2);
-            //    //v1.AddMoveTo(5, 20);
-            //    //v1.AddLineTo(10, 10);
-            //    //v1.AddLineTo(20, 20);
-
-            //    BuildLine(10, 10, 10, 50, v3);
-            //    _tmpArrow = VxsClipper.CombinePaths(v1, v2, VxsClipperType.Union, false, null);
-            //    _tmpArrow = VxsClipper.CombinePaths(_tmpArrow, v3, VxsClipperType.Union, false, null);
-            //}
+            _arrowSolidHead = BuildArrow(true);
+            _arrowLineHead = BuildArrow(false);
+            _roundRectSolid = BuildRoundedRect(false);
+            _roundRectOutline = BuildRoundedRect(true);
+        }
+        static VertexStore BuildArrow(bool solidHead)
+        {
+            VertexStore arrow;
+            VertexStore stem;
             using (VectorToolBox.Borrow(out Stroke stroke))
             using (VxsTemp.Borrow(out var v1, out var v2))
             using (VxsTemp.Borrow(out var v3, out var v4))
             {
+                if (solidHead)
+                {
+                    v1.AddMoveTo(5, 20);
+                    v1.AddLineTo(10, 10);
+                    v1.AddLineTo(15, 20);
+                    v1.AddCloseFigure();
+                    arrow = v1.CreateTrim();
 
-                v1.AddMoveTo(5, 20);
-                v1.AddLineTo(10, 10);
-                v1.AddLineTo(15, 20);
-                stroke.LineCap = LineCap.Round;
-                stroke.Width = 3;
-                stroke.MakeVxs(v1, v2);
-                _tmpArrow = v2.CreateTrim();
+                    BuildLine(10, 20, 10, 50, v3);
+                    stem = v3.CreateTrim();
+                }
+                else
+                {
+                    v1.AddMoveTo(5, 20);
+                    v1.AddLineTo(10, 10);
+                    v1.AddLineTo(15, 20);
+                    stroke.LineJoin = LineJoin.Round;
+                    stroke.LineCap = LineCap.Round;
+                    stroke.Width = 3;
+                    stroke.MakeVxs(v1, v2);
+                    arrow = v2.CreateTrim();
 
-                BuildLine(10, 10, 10, 50, v3);
-                _stem = v3.CreateTrim();
+                    BuildLine(10, 10, 10, 50, v3);
+                    stem = v3.CreateTrim();
+                }
+                arrow = VxsClipper.CombinePaths(arrow, stem, VxsClipperType.Union, false, null);
 
-                _tmpArrow = VxsClipper.CombinePaths(_tmpArrow, _stem, VxsClipperType.Union, false, null);
+                Affine.NewScaling(3).TransformToVxs(arrow, v4);
+                arrow = v4.CreateTrim();
 
-                Affine.NewScaling(3).TransformToVxs(_tmpArrow, v4);
-                _tmpArrow = v4.CreateTrim();
+                return arrow;
             }
         }
-        void BuildLine(float x0, float y0, float x1, float y1, VertexStore output)
+        static void BuildLine(float x0, float y0, float x1, float y1, VertexStore output)
         {
             using (VxsTemp.Borrow(out var v1))
             using (VectorToolBox.Borrow(out Stroke stroke))
             {
-
                 stroke.Width = 3;
                 v1.AddMoveTo(x0, y0);
                 v1.AddLineTo(x1, y1);
-
                 stroke.MakeVxs(v1, output);
             }
         }
 
+        static VertexStore BuildRoundedRect(bool outline)
+        {
+
+            using (VxsTemp.Borrow(out var v1))
+            using (VectorToolBox.Borrow(out RoundedRect roundedRect))
+            {
+                if (outline)
+                {
+
+                    roundedRect.SetRadius(5, 5, 0, 0, 5, 5, 0, 0);
+                    roundedRect.SetRect(10, 10, 30, 30);
+
+                    using (VxsTemp.Borrow(out var v2))
+                    using (VectorToolBox.Borrow(out Stroke stroke))
+                    {
+                        stroke.LineJoin = LineJoin.Bevel;
+                        stroke.Width = 3;
+                        roundedRect.MakeVxs(v1);
+                        return stroke.MakeVxs(v1, v2).CreateTrim();
+                    }
+                }
+                else
+                {
+                    roundedRect.SetRadius(5, 5, 0, 0, 5, 5, 0, 0);
+                    roundedRect.SetRect(10, 10, 30, 30);
+                    return roundedRect.MakeVxs(v1).CreateTrim();
+                }
+            }
+        }
+
+
+
         public override void Draw(Painter p)
         {
             p.Clear(Color.White);
+            p.FillColor = Color.Black;
 
             switch (ReqPolygonKind)
             {
-                case PolygonKind.Arrow:
+                case PolygonKind.ArrowLineHead:
+                    p.Fill(_arrowLineHead);
+                    break;
+                case PolygonKind.ArrowSolidHead:
+                    p.Fill(_arrowSolidHead);
+                    break;
+                case PolygonKind.RoundCornerRect:
+                    p.Fill(_roundRectSolid);
+                    break;
+                case PolygonKind.RoundCornerRect_Outline:
+                    p.Fill(_roundRectOutline);
                     break;
             }
 
-            p.FillColor = Color.Black;
-            p.Fill(_tmpArrow);
-            //p.Fill(_stem);
             base.Draw(p);
         }
-
+        [DemoConfig]
         public PolygonKind ReqPolygonKind
         {
             get;
