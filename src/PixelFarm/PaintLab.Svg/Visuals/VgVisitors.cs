56a3cf5c472ea5e4e8d07f7ebba9e4018c471283
win <wintercoredev@gmail.com>
2019-01-26 06:50:31 +0700

    1799: refactor, move VgPaintArgs

--- src/PixelFarm/PaintLab.Svg/Visuals/VgVisitors.cs
+++ src/PixelFarm/PaintLab.Svg/Visuals/VgVisitors.cs
@@ -40,71 +40,8 @@ namespace PaintLab.Svg
             VgElemenVisitHandler = null;
         }
     }
-    public class VgPaintArgs : VgVisitorBase
-    {
-        float _opacity;
-        bool _maskMode;
-        internal VgPaintArgs()
-        {
-            Opacity = 1;
-        }
-        public Painter P { get; internal set; }
-        public Action<VertexStore, VgPaintArgs> PaintVisitHandler;
-        public bool HasOpacity => _opacity < 1;
-        public float Opacity
-        {
-            get => _opacity;
-            set
-            {
-                if (value < 0)
-                {
-                    _opacity = 0;
-                }
-                else if (value > 1)
-                {
-                    _opacity = 1;//not use this opacity
-                }
-                else
-                {
-                    _opacity = value;
-                }
-            }
-        }
-        internal override void Reset()
-        {
-            base.Reset();//*** reset base class fields too
-            //-------
-            Opacity = 2;
-            P = null;
-            PaintVisitHandler = null;
-        }
-        public bool MaskMode
-        {
-            get => _maskMode;
-            set
-            {
-                _maskMode = value;
-            }
-        }
-    }
 
-    public static class VgPainterArgsPool
-    {
 
-        public static TempContext<VgPaintArgs> Borrow(Painter painter, out VgPaintArgs paintArgs)
-        {
-            if (!Temp<VgPaintArgs>.IsInit())
-            {
-                Temp<VgPaintArgs>.SetNewHandler(
-                    () => new VgPaintArgs(),
-                    p => p.Reset());//when relese back
-            }
-
-            var context = Temp<VgPaintArgs>.Borrow(out paintArgs);
-            paintArgs.P = painter;
-            return context;
-        }
-    }
     public static class VgVistorArgsPool
     {
 
