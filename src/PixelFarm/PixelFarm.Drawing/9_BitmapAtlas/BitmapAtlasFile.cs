749775bc90b2995375fca86bab65cdc7c0d6682f
win <wintercoredev@gmail.com>
2019-03-08 13:16:03 +0700

    1940:  bmp atlas1

---
+++ src/PixelFarm/PixelFarm.Drawing/9_BitmapAtlas/BitmapAtlasFile.cs
@@ -0,0 +1,163 @@
+﻿//MIT, 2018-present, WinterDev
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace PixelFarm.Drawing.BitmapAtlas
+{
+    public class BitmapAtlasFile
+    {
+        SimpleBitmaptAtlas _atlas;
+        enum ObjectKind : ushort
+        {
+            End,
+            TotalImageInfo,
+            GlyphList,
+            OverviewFontInfo,
+        }
+        public SimpleBitmaptAtlas Result => _atlas;
+
+        public void Read(Stream inputStream)
+        {
+            //custom font atlas file             
+            _atlas = new SimpleBitmaptAtlas();
+            using (BinaryReader reader = new BinaryReader(inputStream, System.Text.Encoding.UTF8))
+            {
+                //1. version
+                ushort fileversion = reader.ReadUInt16();
+                bool stop = false;
+                while (!stop)
+                {
+                    //2. read object kind
+                    ObjectKind objKind = (ObjectKind)reader.ReadUInt16();
+                    switch (objKind)
+                    {
+                        default: throw new NotSupportedException();
+                        case ObjectKind.OverviewFontInfo:
+                            ReadOverviewFontInfo(reader);
+                            break;
+                        case ObjectKind.End:
+                            stop = true;
+                            break;
+                        case ObjectKind.GlyphList:
+                            ReadGlyphList(reader);
+                            break;
+                        case ObjectKind.TotalImageInfo:
+                            ReadTotalImageInfo(reader);
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        void ReadTotalImageInfo(BinaryReader reader)
+        {
+            //read total
+            //this version compose of width and height
+            _atlas.Width = reader.ReadUInt16();
+            _atlas.Height = reader.ReadUInt16();
+            byte colorComponent = reader.ReadByte(); //1 or 4
+            _atlas.TextureKind = (TextureKind)reader.ReadByte();
+        }
+        void ReadGlyphList(BinaryReader reader)
+        {
+            ushort glyphCount = reader.ReadUInt16();
+            for (int i = 0; i < glyphCount; ++i)
+            {
+                //read each glyph map info
+                //1. codepoint
+                var glyphMap = new BitmapMapData();
+                ushort glyphIndex = reader.ReadUInt16();
+                //2. area, left,top,width,height
+
+                glyphMap.Left = reader.ReadUInt16();
+                glyphMap.Top = reader.ReadUInt16();
+                glyphMap.Width = reader.ReadUInt16();
+                glyphMap.Height = reader.ReadUInt16();
+                //---------------------------------------
+                //3. texture offset
+                glyphMap.TextureXOffset = reader.ReadInt16();
+                glyphMap.TextureYOffset = reader.ReadInt16();
+
+                //---------------------------------------
+                _atlas.AddGlyph(glyphIndex, glyphMap);
+            }
+        }
+
+        void ReadOverviewFontInfo(BinaryReader reader)
+        {
+            _atlas.FontFilename = reader.ReadString();
+            _atlas.OriginalFontSizePts = reader.ReadSingle();
+        }
+
+        //------------------------------------------------------------
+        BinaryWriter _writer;
+        internal void StartWrite(Stream outputStream)
+        {
+            _writer = new BinaryWriter(outputStream, System.Text.Encoding.UTF8);
+            //version            
+            _writer.Write((ushort)1);
+        }
+        internal void EndWrite()
+        {
+            //write end marker
+            _writer.Write((ushort)ObjectKind.End);
+            //
+            _writer.Flush();
+            _writer = null;
+        }
+
+        internal void WriteOverviewFontInfo(string fontFileName, float sizeInPt)
+        {
+            _writer.Write((ushort)ObjectKind.OverviewFontInfo);
+            if (fontFileName == null)
+            {
+                fontFileName = "";
+            }
+            _writer.Write(fontFileName);
+            _writer.Write(sizeInPt);
+        }
+        internal void WriteTotalImageInfo(ushort width, ushort height, byte colorComponent, TextureKind textureKind)
+        {
+            _writer.Write((ushort)ObjectKind.TotalImageInfo);
+            _writer.Write(width);
+            _writer.Write(height);
+            _writer.Write(colorComponent);
+            _writer.Write((byte)textureKind);
+        }
+        internal void WriteGlyphList(Dictionary<ushort, CacheBmp> glyphs)
+        {
+            _writer.Write((ushort)ObjectKind.GlyphList);
+            //total number
+            int totalNum = glyphs.Count;
+#if DEBUG
+            if (totalNum >= ushort.MaxValue)
+            {
+                throw new NotSupportedException();
+            }
+#endif
+
+            _writer.Write((ushort)totalNum);
+            // 
+            foreach (CacheBmp g in glyphs.Values)
+            {
+                //1. code point
+                _writer.Write((ushort)g.glyphIndex);
+                //2. area, left,top,width,height
+                _writer.Write((ushort)g.area.Left);
+                _writer.Write((ushort)g.area.Top);
+                _writer.Write((ushort)g.area.Width);
+                _writer.Write((ushort)g.area.Height);
+
+
+                //3. texture offset                
+                AtlasItemImage img = g.img;
+                _writer.Write((short)img.TextureOffsetX);//short
+                _writer.Write((short)img.TextureOffsetY);//short
+
+            }
+        }
+    }
+
+}
---
+++ src/PixelFarm/PixelFarm.Drawing/9_BitmapAtlas/SimpleBitmaptAtlas.cs
@@ -0,0 +1,51 @@
+﻿//MIT, 2016-present, WinterDev
+//----------------------------------- 
+
+using System;
+using System.Collections.Generic;
+
+
+namespace PixelFarm.Drawing.BitmapAtlas
+{
+    public enum TextureKind : byte
+    {
+        StencilLcdEffect, //default
+        StencilGreyScale,
+        Msdf,
+        Bitmap
+    }
+    public class SimpleBitmaptAtlas
+    {
+        AtlasItemImage _totalGlyphImage;
+        Dictionary<ushort, BitmapMapData> _glyphLocations = new Dictionary<ushort, BitmapMapData>();
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        /// <summary>
+        /// original font size in point unit
+        /// </summary>
+        public float OriginalFontSizePts { get; set; }
+        public TextureKind TextureKind { get; set; }
+        public string FontFilename { get; set; }
+
+        public void AddGlyph(ushort glyphIndex, BitmapMapData glyphData)
+        {
+            _glyphLocations.Add(glyphIndex, glyphData);
+        }
+        public AtlasItemImage TotalGlyph
+        {
+            get => _totalGlyphImage;
+            set => _totalGlyphImage = value;
+        }
+        public bool TryGetGlyphMapData(ushort glyphIndex, out BitmapMapData glyphdata)
+        {
+            if (!_glyphLocations.TryGetValue(glyphIndex, out glyphdata))
+            {
+                glyphdata = null;
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
