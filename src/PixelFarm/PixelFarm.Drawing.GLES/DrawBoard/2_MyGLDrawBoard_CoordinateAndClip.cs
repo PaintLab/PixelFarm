671496ed06d98c17c407126a8227c2b46873a856
win <wintercoredev@gmail.com>
2019-03-17 13:47:12 +0700

    1952.3: add note

--- src/PixelFarm/PixelFarm.Drawing.GLES/DrawBoard/2_MyGLDrawBoard_CoordinateAndClip.cs
+++ src/PixelFarm/PixelFarm.Drawing.GLES/DrawBoard/2_MyGLDrawBoard_CoordinateAndClip.cs
@@ -18,14 +18,25 @@ namespace PixelFarm.Drawing.GLES2
         //--------------------------------------------------------------------
         public override void SetCanvasOrigin(int x, int y)
         {
-
             _gpuPainter.SetOrigin(x, y);
-            //-----------              
-            //this.gx.TranslateTransform(total_dx, total_dy);
+            //-----------             
+            //current clip rect is relative to current canvas origin
+            //so if move canvas origin we must move current clip rect
+            //in opposite direction 
             //clip rect move to another direction***
             _currentClipRect.Offset(-(x - _canvasOriginX), -(y - _canvasOriginY));
             _canvasOriginX = x;
             _canvasOriginY = y;
+#if DEBUG
+            if (x == 8 || y == 8)
+            {
+
+            }
+            if (_clipRectStack.Count > 0)
+            {
+
+            }
+#endif
         }
 
         public override int OriginX => _canvasOriginX;
@@ -36,9 +47,8 @@ namespace PixelFarm.Drawing.GLES2
         {
             //TODO: reivew clip combine mode
 
-            //_currentClipRect = rect;
-            //return;
             _gpuPainter.SetClipBox(rect.Left, rect.Top, rect.Right, rect.Bottom);
+            _currentClipRect = rect;
         }
         public override bool PushClipAreaRect(int width, int height, ref Rectangle updateArea)
         {
@@ -70,7 +80,9 @@ namespace PixelFarm.Drawing.GLES2
                 _gpuPainter.SetClipBox(_currentClipRect.Left, _currentClipRect.Top, _currentClipRect.Right, _currentClipRect.Bottom);
             }
         }
-
+        /// <summary>
+        /// currrent clip rect relative to current canvas origin
+        /// </summary>
         public override Rectangle CurrentClipRect => _currentClipRect;
 
         public override int Top => _top;
