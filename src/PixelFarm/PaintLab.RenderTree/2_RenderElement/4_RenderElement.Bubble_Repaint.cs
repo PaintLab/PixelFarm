522dbccf497a5a04adfd24da71e2cfe44aee2934
win <wintercoredev@gmail.com>
2019-03-17 20:38:56 +0700

    1955.2: OnInvalidateGraphicsNoti()

--- src/PixelFarm/PaintLab.RenderTree/2_RenderElement/4_RenderElement.Bubble_Repaint.cs
+++ src/PixelFarm/PaintLab.RenderTree/2_RenderElement/4_RenderElement.Bubble_Repaint.cs
@@ -28,7 +28,7 @@ namespace LayoutFarm
             //RELATIVE to its parent
             this.InvalidateParentGraphics(this.RectBounds);
         }
-        protected virtual void OnInvalidateGraphicsNoti(Rectangle totalBounds) { }
+        protected virtual void OnInvalidateGraphicsNoti(bool fromMe, Rectangle totalBounds) { }
 
         public void InvalidateParentGraphics(Rectangle totalBounds)
         {
@@ -39,7 +39,7 @@ namespace LayoutFarm
             //--------------------------------------- 
             if ((_uiLayoutFlags & RenderElementConst.LY_REQ_INVALIDATE_RECT_EVENT) != 0)
             {
-                OnInvalidateGraphicsNoti(totalBounds);
+                OnInvalidateGraphicsNoti(false, totalBounds);
             }
             //
             if (parent != null)
@@ -51,9 +51,9 @@ namespace LayoutFarm
         {
             return (re._uiLayoutFlags & RenderElementConst.LY_REQ_INVALIDATE_RECT_EVENT) != 0;
         }
-        internal static void InvokeInvalidateGraphicsNoti(RenderElement re, Rectangle totalBounds)
+        internal static void InvokeInvalidateGraphicsNoti(RenderElement re, bool fromMe, Rectangle totalBounds)
         {
-            re.OnInvalidateGraphicsNoti(totalBounds);
+            re.OnInvalidateGraphicsNoti(fromMe, totalBounds);
         }
         static void RootInvalidateGraphicArea(RenderElement re, ref Rectangle rect)
         {
