e54660b1b773a1e800cbf39c9ba995cf394ce654
win <wintercoredev@gmail.com>
2019-05-11 08:46:20 +0700

    2021: TextBox.SetFont()

--- src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/2_Basic/2_TextBox.cs
+++ src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/2_Basic/2_TextBox.cs
@@ -16,6 +16,8 @@ namespace LayoutFarm.CustomWidgets
         protected bool _multiline;
         protected TextSpanStyle _defaultSpanStyle;
         protected Color _backgroundColor = Color.White;
+
+
         internal TextBoxBase(int width, int height)
             : base(width, height)
         {
@@ -29,7 +31,21 @@ namespace LayoutFarm.CustomWidgets
                 if (_textEditRenderElement != null) _textEditRenderElement.BackgroundColor = value;
             }
         }
-
+        public override void SetFont(RequestFont font)
+        {
+            if (font == null) return;
+            if (_textEditRenderElement == null)
+            {
+                _defaultSpanStyle.FontColor = Color.Black;
+                _defaultSpanStyle.ReqFont = font;
+            }
+            else
+            {
+                _defaultSpanStyle.ReqFont = font;
+                _defaultSpanStyle.FontColor = Color.Black;
+                _textEditRenderElement.CurrentTextSpanStyle = _defaultSpanStyle;
+            }
+        }
         public TextSpanStyle DefaultSpanStyle
         {
             get => _defaultSpanStyle;
@@ -206,12 +222,14 @@ namespace LayoutFarm.CustomWidgets
         bool _isEditable;
         List<string> _userTextContent2;
 
+
         public TextBox(int width, int height, bool multiline, bool isEditable = true)
             : base(width, height)
         {
             _isEditable = isEditable;
             _multiline = multiline;
         }
+
         /// <summary>
         /// write all lines into stbuilder
         /// </summary>
@@ -421,9 +439,18 @@ namespace LayoutFarm.CustomWidgets
                 }
                 tbox.BackgroundColor = _backgroundColor;
                 tbox.SetController(this);
-                tbox.ViewportChanged += (s, e) => RaiseViewportChanged();
+                tbox.ViewportChanged += (s, e) =>
+                {
+                    RaiseViewportChanged();
+                };
+
                 tbox.ContentSizeChanged += (s, e) =>
                 {
+                    if (Height < tbox.InnerContentSize.Height)
+                    {
+                        this.SetHeight(tbox.InnerContentSize.Height + 2);
+                    }
+
                     RaiseLayoutFinished();
                 };
 
