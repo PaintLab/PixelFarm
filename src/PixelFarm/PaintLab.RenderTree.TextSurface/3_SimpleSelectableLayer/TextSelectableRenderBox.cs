bb6d5f0b2303059dcae5debe4d0e398db30a8637
win <wintercoredev@gmail.com>
2019-06-25 05:30:29 +0700

    2091: TextSelectableRenderBox

---
+++ src/PixelFarm/PaintLab.RenderTree.TextSurface/3_SimpleSelectableLayer/TextSelectableRenderBox.cs
@@ -0,0 +1,24 @@
+﻿//Apache2, 2014-present, WinterDev
+
+using System;
+using PixelFarm.Drawing;
+using LayoutFarm.UI;
+namespace LayoutFarm.TextEditing
+{
+    public class TextSelectableRenderBox : RenderElement
+    {
+        public TextSelectableRenderBox(RootGraphic root, int w, int h) : base(root, w, h)
+        {
+
+        }
+        public override void CustomDrawToThisCanvas(DrawBoard d, Rectangle updateArea)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ResetRootGraphics(RootGraphic rootgfx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
