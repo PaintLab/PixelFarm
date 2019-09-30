7302f9c1bbd914eab6a45e1b764575ab6551d8fe
win <wintercoredev@gmail.com>
2019-09-30 20:38:39 +0700

    2210.1

--- src/PixelFarm/PixelFarm.Painter/0_Base/ForNet20.cs
+++ src/PixelFarm/PixelFarm.Painter/0_Base/ForNet20.cs
@@ -6,6 +6,7 @@ namespace System
     //public delegate R Func<T, R>(T t1);
     //public delegate R Func<T1, T2, R>(T1 t1, T2 t2);
     //public delegate R Func<T1, T2, T3, R>(T1 t1, T2 t2, T3 t3);
+#if !NETSTANDARD
     public delegate void Action<in T1, in T2>(T1 arg1, T2 arg2);
     public delegate void Action<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
     public delegate void Action<in T1, in T2, in T3, in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
@@ -14,7 +15,7 @@ namespace System
     public delegate void Action<in T1, in T2, in T3, in T4, in T5, in T6, in T7>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
     public delegate void Action<in T1, in T2, in T3, in T4, in T5, in T6, in T7, in T8>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8);
 
-#if !NETSTANDARD
+
     public delegate TResult Func<out TResult>();
     public delegate TResult Func<in T, out TResult>(T arg);
     public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
