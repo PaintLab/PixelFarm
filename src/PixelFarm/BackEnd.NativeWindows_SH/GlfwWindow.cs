b88357af385d99587d003ab81958c3812d140a64
win <wintercoredev@gmail.com>
2019-12-30 16:12:23 +0700

    2144.1

--- src/PixelFarm/BackEnd.NativeWindows_SH/GlfwWindow.cs
+++ src/PixelFarm/BackEnd.NativeWindows_SH/GlfwWindow.cs
@@ -269,8 +269,8 @@ namespace PixelFarm.Forms
                 /* Render here */
                 /* Swap front and back buffers */
 
-                s_mainMsgWin.RenderDel?.Invoke();
-                s_mainMsgWin.SwapBuffers();
+                //s_mainMsgWin.RenderDel?.Invoke();
+                //s_mainMsgWin.SwapBuffers();
 
                 /* Poll for and process events */
                 Glfw3.glfwPollEvents();
