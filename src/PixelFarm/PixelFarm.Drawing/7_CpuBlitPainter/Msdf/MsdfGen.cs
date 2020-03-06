adb8a2b8fc8ea76db4d96d0ce96280b6dec955ed
win <wintercoredev@gmail.com>
2020-03-06 17:59:38 +0700

    2294.2: update autogen pro

--- src/PixelFarm/PixelFarm.Drawing/7_CpuBlitPainter/Msdf/MsdfGen.cs
+++ src/PixelFarm/PixelFarm.Drawing/7_CpuBlitPainter/Msdf/MsdfGen.cs
@@ -6,6 +6,32 @@ using System.Collections.Generic;
 
 namespace ExtMsdfGen
 {
+    /// <summary>
+    /// parameter for msdf generation
+    /// </summary>
+    public class MsdfGenParams
+    {
+        public float scaleX = 1;
+        public float scaleY = 1;
+        public float shapeScale = 1;
+        public int minImgWidth = 5;
+        public int minImgHeight = 5;
+
+        public double angleThreshold = 3; //default
+        public double pxRange = 4; //default
+        public double edgeThreshold = 1.00000001;//default,(from original code)
+
+
+        public MsdfGenParams()
+        {
+
+        }
+        public void SetScale(float scaleX, float scaleY)
+        {
+            this.scaleX = scaleX;
+            this.scaleY = scaleY;
+        }
+    }
 
     public static class SdfGenerator
     {
