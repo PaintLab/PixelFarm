485981f334b2cdc87cbb5554125a78538e2fdb6f
win <wintercoredev@gmail.com>
2020-03-14 18:44:17 +0700

    2407: move code

--- src/Tests/TestWinForm_MiniAgg_GLES/FormTestBitmapAtlas.cs
+++ src/Tests/TestWinForm_MiniAgg_GLES/FormTestBitmapAtlas.cs
@@ -1,5 +1,6 @@
 ﻿//MIT, 2020, WinterDev
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
@@ -76,7 +77,7 @@ namespace Mini
         {
 
             string atlas_file = "test1_atlas";
-            OpenTkEssTest.TestBitmapAtlasBuilder.Test(_srcDir, LoadBmp, atlas_file);
+            BuildBitmapAtlas(_srcDir, LoadBmp, atlas_file);
 
             DisposeExistingPictureBoxImage(pictureBox2);
 
@@ -178,5 +179,92 @@ namespace Mini
 
             }
         }
+
+
+
+
+        public static void BuildBitmapAtlas(string imgdir, Func<string, MemBitmap> imgLoader, string outputFilename, bool test_extract = false)
+        {
+
+            //demonstrate how to build a bitmap atlas
+
+            //1. create builder
+            SimpleBitmapAtlasBuilder bmpAtlasBuilder = new SimpleBitmapAtlasBuilder();
+
+            //2. collect all image-files
+            int imgdirNameLen = imgdir.Length;
+            string[] filenames = System.IO.Directory.GetFiles(imgdir, "*.png");
+            ushort index = 0;
+
+            Dictionary<string, ushort> imgDic = new Dictionary<string, ushort>();
+            foreach (string f in filenames)
+            {
+                //3. load a bitmap
+                MemBitmap itemBmp = imgLoader(f);
+                //4. get information about it
+                AtlasItemImage atlasItem = new AtlasItemImage(itemBmp.Width, itemBmp.Height);
+                atlasItem.OriginalBounds = new PixelFarm.Drawing.RectangleF(0, 0, itemBmp.Width, itemBmp.Height);
+                atlasItem.SetBitmap(itemBmp, false);
+                //5. add to builder
+                bmpAtlasBuilder.AddAtlasItemImage(index, atlasItem);
+                string imgPath = f.Substring(imgdirNameLen);
+                imgDic.Add(imgPath, index);
+                index++;
+
+                //------------
+#if DEBUG
+                if (index >= ushort.MaxValue)
+                {
+                    throw new NotSupportedException();
+                }
+#endif
+                //------------
+            }
+
+
+            string atlasInfoFile = outputFilename + ".info";
+            string totalImgFile = outputFilename + ".png";
+
+            //5. merge all small images into a bigone 
+            AtlasItemImage totalImg = bmpAtlasBuilder.BuildSingleImage();
+            bmpAtlasBuilder.ImgUrlDict = imgDic;
+            bmpAtlasBuilder.SetAtlasInfo(TextureKind.Bitmap);
+            //6. save atlas info and total-img (.png file)
+            bmpAtlasBuilder.SaveAtlasInfo(atlasInfoFile);
+            totalImg.Bitmap.SaveImage(totalImgFile);
+
+            //----------------------
+            //test, read data back
+            //----------------------
+            if (test_extract)
+            {
+                bmpAtlasBuilder = new SimpleBitmapAtlasBuilder();
+                SimpleBitmaptAtlas bitmapAtlas = bmpAtlasBuilder.LoadAtlasInfo(atlasInfoFile);
+                //
+                MemBitmap totalAtlasImg = imgLoader(totalImgFile);
+                AtlasItemImage atlasImg = new AtlasItemImage(totalAtlasImg.Width, totalAtlasImg.Height);
+                bitmapAtlas.TotalImg = atlasImg;
+
+                //-----
+                for (int i = 0; i < index; ++i)
+                {
+                    if (bitmapAtlas.TryGetBitmapMapData((ushort)i, out BitmapMapData bmpMapData))
+                    {
+                        //test copy data from bitmap
+                        MemBitmap itemImg = totalAtlasImg.CopyImgBuffer(bmpMapData.Left, bmpMapData.Top, bmpMapData.Width, bmpMapData.Height);
+                        itemImg.SaveImage("test1_atlas_item" + i + ".png");
+                    }
+                }
+                //test,
+                {
+                    if (bitmapAtlas.TryGetBitmapMapData(@"\chk_checked.png", out BitmapMapData bmpMapData))
+                    {
+                        MemBitmap itemImg = totalAtlasImg.CopyImgBuffer(bmpMapData.Left, bmpMapData.Top, bmpMapData.Width, bmpMapData.Height);
+                        itemImg.SaveImage("test1_atlas_item_a.png");
+                    }
+                }
+            }
+        }
+
     }
 }
