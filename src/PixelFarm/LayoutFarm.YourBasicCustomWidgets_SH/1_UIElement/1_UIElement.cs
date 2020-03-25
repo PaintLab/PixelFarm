d3bec23f8ef8799a4cfeadf24a8a0b148d33dde7
win <wintercoredev@gmail.com>
2020-03-25 20:15:11 +0700

    2473.1: minor

--- src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/1_UIElement/1_UIElement.cs
+++ src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/1_UIElement/1_UIElement.cs
@@ -94,11 +94,7 @@ namespace LayoutFarm.UI
         protected virtual bool HasReadyRenderElement => CurrentPrimaryRenderElement != null;
         public abstract void InvalidateGraphics();
 
-        public bool AcceptKeyboardFocus
-        {
-            get;
-            set;
-        }
+        public bool AcceptKeyboardFocus { get; set; }
 
         public virtual object Tag
         {
