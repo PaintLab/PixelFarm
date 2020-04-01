afc4a45c99cd894754619973eec5a97ad592d403
win <wintercoredev@gmail.com>
2020-04-01 16:23:48 +0700

    2534: test dragging beh

--- src/Tests/Test_BasicPixelFarm/Demo1/1.4_Demo_Drag.cs
+++ src/Tests/Test_BasicPixelFarm/Demo1/1.4_Demo_Drag.cs
@@ -2,6 +2,7 @@
 
 using PixelFarm.Drawing;
 using LayoutFarm.UI;
+using LayoutFarm.CustomWidgets;
 namespace LayoutFarm
 {
     [DemoNote("1.4 DemoDrag")]
@@ -9,12 +10,39 @@ namespace LayoutFarm
     {
         protected override void OnStart(AppHost host)
         {
+
+            var box_beh = new UIMouseBehaviour<Box, bool>();
+            {
+                box_beh.MouseDown += (s, e) =>
+                {
+                    e.MouseCursorStyle = MouseCursorStyle.Pointer;
+                };
+                box_beh.MouseUp += (s, e) =>
+                {
+                    e.MouseCursorStyle = MouseCursorStyle.Default;
+                    //box.BackColor = Color.LightGray;
+                    s.Source.BackColor = Color.FromArgb(50, KnownColors.FromKnownColor(KnownColor.DeepSkyBlue));
+                };
+                box_beh.MouseMove += (s, e) =>
+                {
+                    if (e.IsDragging)
+                    {
+                        Box box = s.Source;
+                        box.BackColor = Color.FromArgb(180, KnownColors.FromKnownColor(KnownColor.GreenYellow));
+                        Point pos = box.Position;
+                        box.SetLocation(pos.X + e.XDiff, pos.Y + e.YDiff);
+                        e.MouseCursorStyle = MouseCursorStyle.Pointer;
+                        e.CancelBubbling = true;
+                    }
+                };
+            }
+
+            //-------------
             {
                 var box1 = new LayoutFarm.CustomWidgets.Box(50, 50);
                 box1.BackColor = Color.Red;
-                box1.SetLocation(10, 10);
-                //box1.dbugTag = 1;
-                SetupActiveBoxProperties(box1);
+                box1.SetLocation(10, 10); 
+                box_beh.AttachSharedBehaviorTo(box1); 
                 host.AddChild(box1);
             }
             //--------------------------------
@@ -22,33 +50,9 @@ namespace LayoutFarm
                 var box2 = new LayoutFarm.CustomWidgets.Box(30, 30);
                 box2.SetLocation(50, 50);
                 //box2.dbugTag = 2;
-                SetupActiveBoxProperties(box2);
+                box_beh.AttachSharedBehaviorTo(box2);
                 host.AddChild(box2);
             }
-        }
-        static void SetupActiveBoxProperties(LayoutFarm.CustomWidgets.Box box)
-        {
-            //1. mouse down         
-            box.MouseDown += (s, e) =>
-            {
-                //box.BackColor = KnownColors.FromKnownColor(KnownColor.DeepSkyBlue);
-                e.MouseCursorStyle = MouseCursorStyle.Pointer;
-            };
-            //2. mouse up
-            box.MouseUp += (s, e) =>
-            {
-                e.MouseCursorStyle = MouseCursorStyle.Default;
-                //box.BackColor = Color.LightGray;
-                box.BackColor = Color.FromArgb(50, KnownColors.FromKnownColor(KnownColor.DeepSkyBlue));
-            };
-            box.MouseDrag += (s, e) =>
-            {
-                box.BackColor = Color.FromArgb(180, KnownColors.FromKnownColor(KnownColor.GreenYellow));
-                Point pos = box.Position;
-                box.SetLocation(pos.X + e.XDiff, pos.Y + e.YDiff);
-                e.MouseCursorStyle = MouseCursorStyle.Pointer;
-                e.CancelBubbling = true;
-            };
-        }
+        } 
     }
 }
