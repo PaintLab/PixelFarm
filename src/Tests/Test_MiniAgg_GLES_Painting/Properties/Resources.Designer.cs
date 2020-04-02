83c4ff85d4026ae4e54bc09affeef7db21a23db8
win <wintercoredev@gmail.com>
2020-04-02 10:00:40 +0700

    2537: rename some test folder

--- src/Tests/Test_MiniAgg_GLES_Painting/Properties/Resources.Designer.cs
+++ src/Tests/Test_MiniAgg_GLES_Painting/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TestWinForm_MiniAgg_GLES.Properties {
+namespace Test_MiniAgg_GLES_Painting.Properties {
     using System;
     
     
@@ -19,7 +19,7 @@ namespace TestWinForm_MiniAgg_GLES.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -39,7 +39,7 @@ namespace TestWinForm_MiniAgg_GLES.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestWinForm_MiniAgg_GLES.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Test_MiniAgg_GLES_Painting.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
