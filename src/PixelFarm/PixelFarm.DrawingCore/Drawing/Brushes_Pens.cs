c1081521ddff8926e39c775cb112719c0499ba4b
win <wintercoredev@gmail.com>
2020-04-08 14:02:45 +0700

    2581.3

--- src/PixelFarm/PixelFarm.DrawingCore/Drawing/Brushes_Pens.cs
+++ src/PixelFarm/PixelFarm.DrawingCore/Drawing/Brushes_Pens.cs
@@ -243,11 +243,14 @@ namespace PixelFarm.Drawing
 
     public sealed class PolygonGradientBrush : GeometryGradientBrush
     {
+        /// <summary>
+        /// vertex contains (x,y and color)
+        /// </summary>
         public struct ColorVertex2d
         {
-            public float X;
-            public float Y;
-            public Color C;
+            public readonly float X;
+            public readonly float Y;
+            public readonly Color C;
             public ColorVertex2d(float x, float y, Color c)
             {
                 X = x;
