cae6a1f1166603a64e55773cb60c59aa50f613bb
win <wintercoredev@gmail.com>
2020-04-30 21:20:38 +0700

    406: move LineBox

--- src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/2_Basic/0_AbstractBox.cs
+++ src/PixelFarm/LayoutFarm.YourBasicCustomWidgets_SH/2_Basic/0_AbstractBox.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using PixelFarm.Drawing;
 using LayoutFarm.UI;
-using PixelFarm.CpuBlit;
+using LayoutFarm.RenderBoxes;
+
 
 namespace LayoutFarm.CustomWidgets
 {
@@ -191,7 +192,197 @@ namespace LayoutFarm.CustomWidgets
         }
     }
 
+    class LineBox : IParentLink
+    {
+        LinkedList<RenderElement> _linkList = new LinkedList<RenderElement>();
 
+#if DEBUG
+        static int dbugTotalId;
+        public readonly int dbugId;
+#endif
+        public LineBox()
+        {
+#if DEBUG
+            dbugId = dbugTotalId++;
+#endif
+
+        }
+        public int LineTop { get; set; }
+        public int LineHeight { get; set; }
+        public int LineBottom => LineTop + LineHeight;
+        public RenderElement ParentRenderElement { get; internal set; }
+
+        public void AdjustLocation(ref int px, ref int py)
+        {
+            //TODO: line left ?
+            py += LineTop;
+        }
+#if DEBUG
+        public string dbugGetLinkInfo()
+        {
+            return "linkbox";
+        }
+#endif
+        public RenderElement FindOverlapedChildElementAtPoint(RenderElement afterThisChild, Point point)
+        {
+            return null;
+        }
+
+        public bool IsIntersect(int minY, int maxY) => !((maxY < LineTop) || (minY > (LineTop + LineHeight)));
+
+        //TODO: need to clear all parent link? 
+        public void Clear() => _linkList.Clear();
+
+        public void Add(RenderElement renderE)
+        {
+            _linkList.AddLast(renderE);
+            if (ParentRenderElement != null)
+            {
+                RenderElement.SetParentLink(renderE, this);
+            }
+        }
+
+        public int Count => _linkList.Count;
+
+        public bool HitTestCore(HitChain hitChain)
+        {
+
+            if (LineTop <= hitChain.TestPointY &&
+               (LineTop + LineHeight) > hitChain.TestPointY)
+            {
+                LinkedListNode<RenderElement> node = _linkList.First;
+                hitChain.OffsetTestPoint(0, -LineTop);
+                bool found = false;
+                while (node != null)
+                {
+                    if (node.Value.HitTestCore(hitChain))
+                    {
+                        found = true;
+                        break;
+                    }
+                    node = node.Next;
+                }
+                hitChain.OffsetTestPoint(0, LineTop);
+                return found;
+            }
+            return false;
+        }
+        public void Render(DrawBoard d, UpdateArea updateArea)
+        {
+            LinkedListNode<RenderElement> renderNode = _linkList.First;
+            Rectangle backup = updateArea.CurrentRect;
+            int enter_canvas_x = d.OriginX;
+            int enter_canvas_y = d.OriginY;
+
+            while (renderNode != null)
+            {
+                //---------------------------
+                //TODO: review here again
+                RenderElement renderE = renderNode.Value;
+                if (renderE.IntersectsWith(updateArea))
+                {
+                    int x = renderE.X;
+                    int y = renderE.Y;
+
+                    d.SetCanvasOrigin(enter_canvas_x + x, enter_canvas_y + y);
+                    updateArea.Offset(-x, -y);
+                    RenderElement.Render(renderE, d, updateArea);
+                    updateArea.Offset(x, y);
+                }
+
+                renderNode = renderNode.Next;
+            }
+            updateArea.CurrentRect = backup;//restore  
+            d.SetCanvasOrigin(enter_canvas_x, enter_canvas_y);//restore
+        }
+
+        public void AdjustHorizontalAlignment(RectUIAlignment alignment)
+        {
+            //line, and spread data inside this line
+
+
+        }
+        public void AdjustVerticalAlignment(VerticalAlignment vertAlignment)
+        {
+            LinkedListNode<RenderElement> node = _linkList.First;
+
+            switch (vertAlignment)
+            {
+                case VerticalAlignment.Bottom:
+                    while (node != null)
+                    {
+                        RenderElement r = node.Value;
+                        if (r is CustomRenderBox box)
+                        {
+                            int diff = LineHeight - r.Height;
+                            if (diff > 0)
+                            {
+                                //change location
+                                r.SetLocation(r.Left, box.ContentTop + diff);
+                            }
+                            else
+                            {
+                                //
+                            }
+                        }
+                        else
+                        {
+                            r.SetLocation(r.Left, 0); //***
+                        }
+
+                        node = node.Next;//**
+                    }
+                    break;
+                case VerticalAlignment.Middle:
+                    {
+                        //middle height of this line                        
+
+                        while (node != null)
+                        {
+                            RenderElement r = node.Value;
+                            if (r is CustomRenderBox box)
+                            {
+                                int diff = LineHeight - r.Height;
+                                if (diff > 0)
+                                {
+                                    //change location
+                                    r.SetLocation(r.Left, box.ContentTop + (diff / 2));
+                                }
+                                else
+                                {
+                                    //
+                                }
+                            }
+                            else
+                            {
+                                r.SetLocation(r.Left, 0); //***
+                            }
+
+                            node = node.Next;//**
+                        }
+                    }
+                    break;
+                case VerticalAlignment.Top:
+                    while (node != null)
+                    {
+                        RenderElement r = node.Value;
+                        if (r is CustomRenderBox box)
+                        {
+                            r.SetLocation(r.Left, box.ContentTop);
+                        }
+                        else
+                        {
+                            r.SetLocation(r.Left, 0); //***
+                        }
+
+                        node = node.Next;//**
+                    }
+                    break;
+                case VerticalAlignment.UserSpecific://TODO
+                    break;
+            }
+        }
+    }
     /// <summary>
     /// abstract box ui element.
     /// this control provides 'primary-render-element', 
@@ -620,7 +811,7 @@ namespace LayoutFarm.CustomWidgets
             set
             {
                 _preserveLineBoxes = value;
-                if (!value && _primElement != null && _primElement.Lines != null)
+                if (!value && _primElement != null)
                 {
                     _primElement.Lines = null;
                 }
@@ -864,6 +1055,8 @@ namespace LayoutFarm.CustomWidgets
                                 {
                                     if (ui is AbstractRectUI rect)
                                     {
+                                        //1. measure content=> get 'default' size, minimum or specific size
+                                        //
                                         rect.PerformContentLayout();
 
                                         int new_x = xpos + rect.Width + rect.MarginLeftRight;
@@ -913,6 +1106,10 @@ namespace LayoutFarm.CustomWidgets
                                             //start 
                                             maxBottom = tmp_bottom;
                                         }
+                                    }
+                                    else
+                                    {
+
                                     }
 
                                     linebox.Add(ui.GetPrimaryRenderElement());
