274551b9f42939367ec3e2bd1fcb0bd44ada638a
win <wintercoredev@gmail.com>
2020-07-26 08:58:22 +0700

    907: test

--- src/Tests/Test_RenderTreeFocus/0_Start/Program.cs
+++ src/Tests/Test_RenderTreeFocus/0_Start/Program.cs
@@ -1,6 +1,11 @@
 ﻿//Apache2, 2014-present, WinterDev
 
+using PixelFarm.Drawing;
 using System;
+using System.Collections.Generic;
+using System.Text;
+using Typography.Text;
+using Typography.TextBreak;
 namespace TestGraphicPackage2
 {
     static class Program
@@ -8,7 +13,7 @@ namespace TestGraphicPackage2
 
         [STAThread]
         static void Main(string[] args)
-        { 
+        {
 
             PixelFarm.Platforms.StorageService.RegisterProvider(new YourImplementation.LocalFileStorageProvider(""));
 
@@ -19,7 +24,7 @@ namespace TestGraphicPackage2
             //string typographyDir = @"brkitr_src/dictionaries";
             //string icu_datadir = YourImplementation.RelativePathBuilder.SearchBackAndBuildFolderPath(System.IO.Directory.GetCurrentDirectory(), "PixelFarm", @"..\Typography\Typography.TextBreak\icu62\brkitr");
 
-
+            dbugTestBreak();
             string icu_datadir = "brkitr"; //see brkitr folder, we link data from Typography project and copy to output if newer
             if (!System.IO.Directory.Exists(icu_datadir))
             {
@@ -36,5 +41,109 @@ namespace TestGraphicPackage2
             //load demo list from specific asm             
             YourImplementation.TestBedStartup.RunDemoList(typeof(LayoutFarm.Demo_SingleButton).Assembly);
         }
+
+#if DEBUG
+        static void dbugTestBreak()
+        {
+
+            string msg = "heこん😁";
+            TextBufferSpan buff;
+            char[] msg_buffer = msg.ToCharArray();
+
+            char[] x1 = "😁".ToCharArray();
+            int utf32_x1 = char.ConvertToUtf32(x1[0], x1[1]);
+
+            //https://www.unicode.org/faq/utf_bom.html#utf16-1
+            //the first snippet calculates the high (or leading) surrogate from a character code C.
+
+            //    const UTF16 HI_SURROGATE_START = 0xD800
+
+            //    UTF16 X = (UTF16) C;
+            //    UTF32 U = (C >> 16) & ((1 << 5) - 1);
+            //    UTF16 W = (UTF16) U - 1;
+            //    UTF16 HiSurrogate = HI_SURROGATE_START | (W << 6) | X >> 10;
+
+            //where X, U and W correspond to the labels used in Table 3-5 UTF-16 Bit Distribution. The next snippet does the same for the low surrogate.
+
+            //    const UTF16 LO_SURROGATE_START = 0xDC00
+
+            //    UTF16 X = (UTF16) C;
+            //    UTF16 LoSurrogate = (UTF16) (LO_SURROGATE_START | X & ((1 << 10) - 1));
+
+            //Finally, the reverse, where hi and lo are the high and low surrogate, and C the resulting character
+
+            //    UTF32 X = (hi & ((1 << 6) -1)) << 10 | lo & ((1 << 10) -1);
+            //    UTF32 W = (hi >> 6) & ((1 << 5) - 1);
+            //    UTF32 U = W + 1;
+
+            //    UTF32 C = U << 16 | X;
+
+            //------------------
+            // constants
+            const int LEAD_OFFSET = 0xD800 - (0x10000 >> 10);
+            const int SURROGATE_OFFSET = 0x10000 - (0xD800 << 10) - 0xDC00;
+
+            //from codepoint to upper and lower 
+            ushort lead = (ushort)(LEAD_OFFSET + (utf32_x1 >> 10));
+            ushort trail = (ushort)(0xDC00 + (utf32_x1 & 0x3FF));
+
+            //compute back
+            int codepoint_x= (lead << 10) + trail + SURROGATE_OFFSET;
+            //UTF32 codepoint = (lead << 10) + trail + SURROGATE_OFFSET;
+
+
+
+            char x1_0 = (char)(utf32_x1 >> 16);
+            char x1_1 = (char)(utf32_x1);
+
+
+            int[] utf32 = GetUtf32Buffer(msg, out int utf32Len);
+
+            buff = new TextBufferSpan(utf32, 0, utf32Len);
+            //buff = new TextBufferSpan(msg_buffer);
+            Typography.TextLayout.LayoutWordVisitor visitor = new Typography.TextLayout.LayoutWordVisitor();
+
+            var line_segs = new Typography.TextLayout.LineSegmentList<Typography.TextLayout.LineSegment>();
+            visitor.SetLineSegmentList(line_segs);
+
+            //mew text service
+            var openFontTextService = new Typography.Text.OpenFontTextService();
+            Typography.Text.GlobalTextService.TxtClient = openFontTextService.CreateNewServiceClient();
+            Typography.Text.GlobalTextService.TxtClient.BreakToLineSegments(buff, visitor);
+
+        }
+        static int[] GetUtf32Buffer(string str, out int output_i)
+        {
+            char[] buffer = str.ToCharArray();
+            int[] buffer2 = new int[buffer.Length];
+
+
+            output_i = 0;
+            for (int i = 0; i < buffer.Length; ++i)
+            {
+                char c = buffer[i];
+                if (char.IsHighSurrogate(c))
+                {
+                    if (i < buffer.Length - 1 && char.IsLowSurrogate(buffer[i + 1]))
+                    {
+                        buffer2[output_i] = char.ConvertToUtf32(c, buffer[i + 1]);
+                        output_i++;
+                        i++;
+                    }
+                    else
+                    {
+                        //error
+                    }
+                }
+                else
+                {
+                    buffer2[output_i] = c;
+                    output_i++;
+                }
+            }
+
+            return buffer2;
+        }
+#endif
     }
 }
